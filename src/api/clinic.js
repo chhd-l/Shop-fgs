9fbfdd82151cee4a0c16c0db2b957884debefaf9
唐井儿儿儿 <mytang11@163.com>
2020-04-26 11:23:30 +0800

    联调首页clinics接口

--- src/api/clinic.js
+++ src/api/clinic.js
@@ -2,7 +2,8 @@ import axios from '@/utils/request'
 
 const api = {
   queryPrescription: '/clinics/prescription',
-  allClinics:'/clinics/queryPrescriptionNonsort'
+  prescriptionById: '/clinics/prescriptionById',
+  allClinics: '/clinics/queryPrescriptionNonsort'
 }
 
 export default api
@@ -11,13 +12,21 @@ export function getPrescription (parameter) {
   return axios({
     url: `${api.queryPrescription}`,
     method: 'post',
-    data:parameter
+    data: parameter
   })
 }
 export function getAllPrescription (parameter) {
   return axios({
     url: `${api.allClinics}`,
     method: 'post',
-    data:parameter
+    data: parameter
+  })
+}
+
+export function getPrescriptionById (parameter) {
+  return axios({
+    url: `${api.prescriptionById}`,
+    method: 'post',
+    data: parameter
   })
 }
