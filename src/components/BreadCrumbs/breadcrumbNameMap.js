c25469fdb5f31b46e0e74c5d8e6b208f142cc09c
唐井儿儿儿 <mytang11@163.com>
2020-05-07 21:24:29 +0800

    bug fix

--- src/components/BreadCrumbs/breadcrumbNameMap.js
+++ src/components/BreadCrumbs/breadcrumbNameMap.js
@@ -1,14 +1,14 @@
 const breadcrumbNameMap = {
-  '/list/dogs': [{ name: 'home.catogery1' }],
-  '/list/cats': [{ name: 'home.catogery2' }],
+  '/list/dogs': [{ name: 'home.catogery3' }],
+  '/list/cats': [{ name: 'home.catogery4' }],
   '/list/vcn': [
-    { name: 'home.catogery3' }
+    { name: 'home.catogery1' }
     // { name: 'dogs2', href: '/list/dogs' },
     // { name: 'age', href: '/list/dogs' },
     // { name: 'puppy' }
   ],
   '/list/vd': [
-    { name: 'home.catogery4' }
+    { name: 'home.catogery2' }
     // { name: 'cats2', href: '/list/cats' },
     // { name: 'age', href: '/list/cats' },
     // { name: 'kitten' }
