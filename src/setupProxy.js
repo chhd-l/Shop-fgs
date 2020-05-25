1e48c1cb9214d7faaf75558308cd59e7ac7f48b4
yuqi zhu <yuqizhu@yuqideMacBook-Pro.local>
2020-05-25 09:52:40 +0800

    更改端口

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware')
 module.exports = function (app) {
   app.use(proxy('/api', {
     // target: process.env.VUE_APP_URL,
-    target: "http://121.37.129.70:8090",
+    target: "https://121.37.129.70:8090",
     secure: false,
     changeOrigin: true,
     pathRewrite: {
