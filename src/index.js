97718eb177b1e0c13df87583349ee18af3428676
yuqi zhu <yuqizhu@yuqideMacBook-Pro.local>
2020-05-25 12:12:20 +0800

    集成okta

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 import '@/assets/css/checkout.css'
-import 'semantic-ui-css/semantic.min.css';
+// import 'semantic-ui-css/semantic.min.css';
 import './polyfills';
 import registerServiceWorker from './registerServiceWorker';
 
