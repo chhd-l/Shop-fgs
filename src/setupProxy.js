3f952672faf05a00e361060a6c719704f8ccaebf
唐井儿儿儿 <mytang11@163.com>
2020-05-25 10:44:24 +0800

    测试环境接口地址修改

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware')
 module.exports = function (app) {
   app.use(proxy('/api', {
     // target: process.env.VUE_APP_URL,
-    target: "https://121.37.129.70:8090",
+    target: "https://shopuat.466920.com/api",
     secure: false,
     changeOrigin: true,
     pathRewrite: {
