b1509035391951d6a4be50c9ceab8951468b83a3
chuali <chuali@deioitte.com.cn>
2020-07-22 11:40:01 +0800

    oxxo bug

--- src/views/Confirmation/modules/OxxoModal.js
+++ src/views/Confirmation/modules/OxxoModal.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import "./OxxoModal.css";
-import OxxoModalBody from "./OxxoModalBody";
-import ReactToPrint from "react-to-print";
 import { FormattedMessage } from "react-intl";
 
 export default class OxxoModal extends Component {
@@ -39,15 +37,6 @@ export default class OxxoModal extends Component {
           >
             <div className="modal-content mt-0">
               <div id="mainBody" style={{ maxHeight: '80vh',minHeight: '80vh', overflowY: 'auto' }}>
-                {/* <OxxoModalBody ref={(el) => (this.componentRef = el)} />
-                <ReactToPrint
-                  trigger={() => {
-                    // NOTE: could just as easily return <SomeComponent />. Do NOT pass an `onClick` prop
-                    // to the root node of the returned component as it will be overwritten.
-                    return <div className="printPanel"> <button className="print" href="#"><FormattedMessage id="print" /></button></div>;
-                  }}
-                  content={() => this.componentRef}
-                /> */}
                   <iframe src={this.props.oxxoPayUrl} width="100%" style={{ height: '78vh' }}>
                   </iframe>
               </div>
