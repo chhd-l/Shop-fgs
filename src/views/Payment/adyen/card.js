94a6187c8181039d665e9f6b1706f7f744de16ae
kevin <qhx717@qq.com>
2020-07-30 11:24:10 +0800

    adyen支付demo测试成功

--- src/views/Payment/adyen/card.js
+++ src/views/Payment/adyen/card.js
@@ -1,52 +1,56 @@
 import {
     makePayment,
-    getOriginKey
+    //getOriginKey
   } from "./utils"
 
 export const initAdyenPay = function(){
-    // getOriginKey().then(originKey => {
-    //         const AdyenCheckout = window.AdyenCheckout
-    //         // 1. Create an instance of AdyenCheckout
-    //         const checkout = new AdyenCheckout({
-    //             environment: 'test',
-    //             originKey: originKey // Mandatory. originKey from Customer Area
-    //         });
-    //         // 2. Create and mount the Component
-    //         const card = checkout
-    //             .create('card', {
-    //                 // Optional Configuration
-    //                 // hasHolderName: true,
+    //先获取OriginKey
+    /* getOriginKey().then(originKey => {
+            const AdyenCheckout = window.AdyenCheckout
+            // 1. Create an instance of AdyenCheckout
+            const checkout = new AdyenCheckout({
+                environment: 'test',
+                originKey: originKey // Mandatory. originKey from Customer Area
+            });
+            // 2. Create and mount the Component
+            const card = checkout
+                .create('card', {
+                    // Optional Configuration
+                    // hasHolderName: true,
         
-    //                 // Optional. Customize the look and feel of the payment form
-    //                 // https://docs.adyen.com/developers/checkout/api-integration/configure-secured-fields/styling-secured-fields
-    //                 styles: {},
+                    // Optional. Customize the look and feel of the payment form
+                    // https://docs.adyen.com/developers/checkout/api-integration/configure-secured-fields/styling-secured-fields
+                    styles: {},
         
-    //                 // Optional. Define custom placeholders for the Card fields
-    //                 // https://docs.adyen.com/developers/checkout/api-integration/configure-secured-fields/styling-secured-fields
-    //                 placeholders: {
-    //                     // encryptedCardNumber: '9999 9999 9999 9999',
-    //                     // encryptedExpiryDate: '01/22',
-    //                     // encryptedSecurityCode : '123'
-    //                 },
+                    // Optional. Define custom placeholders for the Card fields
+                    // https://docs.adyen.com/developers/checkout/api-integration/configure-secured-fields/styling-secured-fields
+                    placeholders: {
+                        // encryptedCardNumber: '9999 9999 9999 9999',
+                        // encryptedExpiryDate: '01/22',
+                        // encryptedSecurityCode : '123'
+                    },
         
-    //                 // Optionally show a Pay Button
-    //                 showPayButton: true,
+                    // Optionally show a Pay Button
+                    showPayButton: true,
         
-    //                 // Events
-    //                 onSubmit: (state, component) => {
-    //                     if (state.isValid) {
-    //                         makePayment(card.data);
-    //                     }
-    //                 },
+                    // Events
+                    onSubmit: (state, component) => {
+                        if (state.isValid) {
+                            makePayment(card.data);
+                        }
+                    },
         
-    //                 onChange: (state, component) => {
-    //                     // state.data;
-    //                     // state.isValid;
-    //                 }
-    //             })
-    //             .mount('#card-container');
-    //     });
-    const AdyenCheckout = window.AdyenCheckout
+                    onChange: (state, component) => {
+                        // state.data;
+                        // state.isValid;
+                    }
+                })
+                .mount('#card-container');
+        }); */
+
+
+        //去掉getOriginKey方法,originKey写死
+        const AdyenCheckout = window.AdyenCheckout
         // 1. Create an instance of AdyenCheckout
         const checkout = new AdyenCheckout({
             environment: 'test',
