fc953dbf4ade308a91f72e06acefab733926c143
minyatang <mytang11@163.com>
2020-08-04 17:55:14 +0800

    容错

--- src/views/Cart/modules/loginCart.js
+++ src/views/Cart/modules/loginCart.js
@@ -327,7 +327,7 @@ class LoginCart extends React.Component {
                               <i></i>
                             </span>
                           </div> */}
-                          {pitem.goodsSpecs.map((sItem, i) => (
+                          {(pitem.goodsSpecs || []).map((sItem, i) => (
                             <div key={i} className="overflow-hidden">
                               <div className="text-left ml-1">{sItem.specName}:</div>
                               {sItem.chidren.map((sdItem, i2) => (
