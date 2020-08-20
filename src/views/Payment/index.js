c972238c1b0baedd8c924a1e92a93552de2562d6
knifecome <qhx717@qq.com>
2020-08-20 14:32:14 +0800

    sofort添加邮箱

--- src/views/Payment/index.js
+++ src/views/Payment/index.js
@@ -322,7 +322,7 @@ class Payment extends React.Component {
       ? dict.filter(c => c.id === cityId)[0].cityName
       : cityId
   }
-  showErrorMsg (msg) {
+  showErrorMsg = (msg)=> {
     this.setState({
       errorMsg: msg
     });
@@ -362,8 +362,8 @@ class Payment extends React.Component {
       // (1) Create an instance of AdyenCheckout
       const checkout = new AdyenCheckout({
         environment: "test",
-        originKey: process.env.REACT_APP_AdyenOriginKEY,
-        // originKey: 'pub.v2.8015632026961356.aHR0cDovL2xvY2FsaG9zdDozMDAw.zvqpQJn9QpSEFqojja-ij4Wkuk7HojZp5rlJOhJ2fY4',
+        //originKey: process.env.REACT_APP_AdyenOriginKEY,
+         originKey: 'pub.v2.8015632026961356.aHR0cDovL2xvY2FsaG9zdDozMDAw.zvqpQJn9QpSEFqojja-ij4Wkuk7HojZp5rlJOhJ2fY4',
         locale: "de-DE",
       });
 
@@ -1077,7 +1077,7 @@ class Payment extends React.Component {
       </div>
       {/* Sofort */}
       <div className={`${this.state.paymentTypeVal === "directEbanking" ? "" : "hidden"}`}>
-        <Sofort clickPay={this.initSofort} />
+        <Sofort clickPay={this.initSofort} showErrorMsg={this.showErrorMsg} />
       </div>
       {/* ***********************支付选项卡的内容end******************************* */}
     </>
