5b90bc1e6756050fec543ec9de3f70441fb5bd6f
knifecome <qhx717@qq.com>
2020-08-24 18:27:42 +0800

    优化footer组件

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,6 +5,7 @@ import { inject, observer } from 'mobx-react';
 import {cookieSettingsBtn} from './cookieSettingsBtn'
 import {requestInvoiceJSX} from './requestInvoiceJSX'
 import MarsFooterMap from './MarsFooterMap'
+import {menubar} from "./menubar"
 import './index.css'
 
 @inject("configStore")
@@ -12,6 +13,9 @@ import './index.css'
 class Footer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      cur_menubar:menubar[process.env.REACT_APP_LANG]
+    }
   }
   async componentDidMount () {
     this.props.configStore.queryConfig()
@@ -37,135 +41,36 @@ class Footer extends React.Component {
             <div className="rc-column rc-padding-x--xs--desktop rc-padding-x--none--mobile">
               <nav data-toggle-group="mobile" data-toggle-effect="rc-expand--vertical" className="rc-padding-x--xs--desktop rc-padding-x--none--mobile">
                 <ul className="rc-list rc-list--blank rc-list--align rc-list--inverse d-flex justify-content-between flex-wrap flex-md-nowrap" role="menubar">
-                  <li className="rc-list__item rc-list__item--group rc-full-width">
-                    <div className="rc-list__header" id="footer-head-158504765613564650" data-toggle="footer-list-158504765613564650" role="menuitem">
-                      <h3 className="rc-padding-left--md--mobile">
-                        <FormattedMessage id="footer.aboutRoyalCanin" />
-                      </h3>
-                    </div>
-                    <ul className="list list--blank list--align" id="footer-list-158504765613564650" aria-labelledby="footer-head-158504765613564650" role="menu">
-                      <li className="rc-list__item">
-                        <a className="rc-list__link text-decoration-none color-f6f6f6" href={this.props.configStore.contactUsUrl} role="menuitem">
-                          <FormattedMessage id="aboutUs2" />
-                        </a>
-                      </li>
-                      <li className="rc-list__item">
-                        <a
-                          className="rc-list__link text-decoration-none color-f6f6f6"
-                          href={this.props.configStore.ourValues || 'https://www.royalcanin.com/mx/about-us/our-values'}
-                          role="menuitem">
-                          <FormattedMessage id="footer.ourValues" />
-                        </a>
-                      </li>
-                      <li className="rc-list__item">
-                        <a
-                          className="rc-list__link text-decoration-none color-f6f6f6"
-                          href={this.props.configStore.qualityAndSafety || 'https://www.royalcanin.com/mx/about-us/quality-and-food-safety'}
-                          role="menuitem">
-                          <FormattedMessage id="footer.qualityAndSafety" />
-                        </a>
-                      </li>
-                      <li className="rc-list__item">
-                        <a
-                          className="rc-list__link text-decoration-none color-f6f6f6"
-                          href={this.props.configStore.specificNutrition || 'https://www.royalcanin.com/mx/tailored-nutrition'}
-                          role="menuitem">
-                          <FormattedMessage id="footer.healthAndNutrition" />
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className="rc-list__item rc-list__item--group rc-full-width">
-                    <div className="rc-list__header" id="footer-head-158504765613561849" data-toggle="footer-list-158504765613561849" role="menuitem">
-                      <h3 className="rc-padding-left--md--mobile">
-                        <FormattedMessage id="footer.products" />
-                      </h3>
-                    </div>
-                    <ul className="list list--blank list--align" id="footer-list-158504765613561849" aria-labelledby="footer-head-158504765613561849" role="menu">
-                      <li className="rc-list__item">
-                        <Link className="rc-list__link text-decoration-none color-f6f6f6" to="/list/cats" role="menuitem">
-                          <FormattedMessage id="cats" />
-                        </Link>
-                      </li>
-                      <li className="rc-list__item">
-                        <Link className="rc-list__link text-decoration-none color-f6f6f6" to="/list/dogs" role="menuitem">
-                          <FormattedMessage id="dogs" />
-                        </Link>
-                      </li>
-                    </ul>
-                  </li>
-                  <li className="rc-list__item rc-list__item--group rc-full-width">
-                    <div className="rc-list__header" id="footer-head-158504765613551093" data-toggle="footer-list-158504765613551093" role="menuitem">
-                      <h3 className="rc-padding-left--md--mobile">
-                        <FormattedMessage id="footer.help" />
-                      </h3>
-                    </div>
-                    <ul className="list list--blank list--align" id="footer-list-158504765613551093" aria-labelledby="footer-head-158504765613551093" role="menu">
-                      <li className="rc-list__item">
-                        <Link className="rc-list__link text-decoration-none color-f6f6f6" to="/help" role="menuitem">
-                          <FormattedMessage id="footer.contacts" />
-                        </Link>
-                      </li>
-                      <li className="rc-list__item">
-                        <Link className="rc-list__link text-decoration-none color-f6f6f6" to="/FAQ" role="menuitem">
-                          <FormattedMessage id="footer.FAQ" />
-                        </Link>
-                      </li>
-                      {requestInvoiceJSX[process.env.REACT_APP_LANG]}
-                    </ul>
-                  </li>
-                  <li className="rc-list__item rc-list__item--group rc-full-width">
-                    <div className="rc-list__header" id="footer-head-158504765613549553" data-toggle="footer-list-158504765613549553" role="menuitem">
-                      <h3 className="rc-padding-left--md--mobile">
-                        <FormattedMessage id="footer.Additionally" />
-                      </h3>
-                    </div>
-                    <ul className="list list--blank list--align" id="footer-list-158504765613549553" aria-labelledby="footer-head-158504765613549553" role="menu">
-                      {/* 德国特殊链接，stone portal没配置，后期最好在stone portal配置*/}
-                      <li className="rc-list__item">
-                        {
-                          process.env.REACT_APP_LANG=='de'?
-                          <a
-                            className="rc-list__link text-decoration-none color-f6f6f6"
-                            href='https://www.royalcanin.com/de/about-us/data-protection' role="menuitem">
-                            <FormattedMessage id="footer.privacyPolicy" />
-                          </a>
-                          : <Link className="rc-list__link text-decoration-none color-f6f6f6" to="/privacypolicy" role="menuitem">
-                          <FormattedMessage id="footer.privacyPolicy" />
-                        </Link>
-                        }
-                      </li>
-                      <li className="rc-list__item">
-                        <a
-                          className="rc-list__link text-decoration-none color-f6f6f6"
-                          href={this.props.configStore.informationForParents} role="menuitem">
-                          <FormattedMessage id="footer.informationForParents" />
-                        </a>
-                      </li>
-                      <li className="rc-list__item">
-                         {/* 德国特殊链接，stone portal没配置，后期最好在stone portal配置*/}
-                        {
-                          process.env.REACT_APP_LANG=='de'
-                          ?<a
-                            className="rc-list__link text-decoration-none color-f6f6f6"
-                            href='https://www.mars.com/legal-germany' role="menuitem">
-                            <FormattedMessage id="footer.websiteTermsOfUse" />
-                          </a>
-                          :<Link className="rc-list__link text-decoration-none color-f6f6f6" to="/termuse" role="menuitem">
-                          <FormattedMessage id="footer.websiteTermsOfUse" />
-                        </Link>
-                        }
-                      </li>
-                      <li className="rc-list__item">
-                        <a
-                          className="rc-list__link text-decoration-none color-f6f6f6"
-                          href={this.props.configStore.cookiesUrl || 'https://www.mars.com/cookies-spain'}
-                          role="menuitem">
-                          <FormattedMessage id="footer.cookieCollectionPolicy" />
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
+                  {
+                    this.state.cur_menubar.map(item=>item.map(item=>{
+                      return (
+                        <li className="rc-list__item rc-list__item--group rc-full-width">
+                          <div className="rc-list__header" id="footer-head-158504765613564650" data-toggle="footer-list-158504765613564650" role="menuitem">
+                            <h3 className="rc-padding-left--md--mobile">
+                              <FormattedMessage id={item.titleId} />
+                            </h3>
+                          </div>
+                          <ul className="list list--blank list--align" id="footer-list-158504765613564650" aria-labelledby="footer-head-158504765613564650" role="menu">
+                            {
+                              item.list.map(listItem=>{
+                                return (
+                                  <li className="rc-list__item">
+                                    {!!listItem.link?
+                                    <Link className="rc-list__link text-decoration-none color-f6f6f6" to={listItem.link} role="menuitem">
+                                      <FormattedMessage id={listItem.messageId} />
+                                    </Link>:
+                                    <a className="rc-list__link text-decoration-none color-f6f6f6" href={(!!listItem.prop)&&this.props.configStore[listItem.prop]||listItem.url} role="menuitem">
+                                      <FormattedMessage id={listItem.messageId} />
+                                    </a>}                               
+                                  </li>
+                                )
+                              })
+                            }
+                          </ul>
+                        </li>
+                      )
+                    }))
+                  }
                 </ul>
               </nav>
 
