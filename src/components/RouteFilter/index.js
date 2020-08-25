21ba585f40a56244de2faf349fc199fb6369376f
minyatang <mytang11@163.com>
2020-08-25 22:21:05 +0800

    1.paymentsos(三方)的js不能同時加載 2.游客時加載https://js.paymentsos.com/v2/latest/secure-fields.min.js，會員時加載https://js.paymentsos.com/v2/0.0.1/token.min.js

--- src/components/RouteFilter/index.js
+++ src/components/RouteFilter/index.js
@@ -66,57 +66,60 @@ class RouteFilter extends Component {
     if (window.location.href.indexOf('/#/') !== -1) {
       window.location.href = window.location.href.split('/#/').join('/');
     }
-    // if (this.props.location.pathname === '/payment/payment') {
-    //   loadJS(
-    //     'https://js.paymentsos.com/v2/latest/secure-fields.min.js',
-    //     function () {
-    //       window.POS.setPublicKey(process.env.REACT_APP_PaymentKEY);
-    //       window.POS.setEnvironment(process.env.REACT_APP_PaymentENV);
-    //       const style = {
-    //         base: {
-    //           secureFields: {
-    //             width: 'calc(100% - 45px)'
-    //           },
-    //           pan: {
-    //             display: 'inline-block',
-    //             width: '50%'
-    //           },
-    //           expirationDate: {
-    //             display: 'inline-block',
-    //             width: '30%'
-    //           },
-    //           cvv: {
-    //             display: 'inline-block',
-    //             width: '20%'
-    //           }
-    //         }
-    //       };
-    //       window.POS.setStyle(style);
-    //       window.POS.initSecureFields('card-secure-fields');
-    //       try {
-    //         document
-    //           .getElementById('zoozIframe')
-    //           .setAttribute('scrolling', 'no');
-    //       } catch (e) {}
-    //       if (document.getElementById('payment-form')) {
-    //         document
-    //           .getElementById('payment-form')
-    //           .addEventListener('submit', function (event) {
-    //             console.log(document.getElementById('cardholder-name'));
-    //             event.preventDefault();
-    //             const additionalData = {
-    //               holder_name: document.getElementById('cardholder-name').value // This field is mandatory
-    //             };
-    //             window.POS.createToken(additionalData, function (result) {
-    //               console.log(result, 'result');
-    //               // Grab the token here
-    //               sessionStorage.setItem('payosdata', result);
-    //             });
-    //           });
-    //       }
-    //     }
-    //   );
-    // }
+    if (
+      this.props.location.pathname === '/payment/payment' &&
+      !localStorage.getItem('rc-token')
+    ) {
+      loadJS(
+        'https://js.paymentsos.com/v2/latest/secure-fields.min.js',
+        function () {
+          window.POS.setPublicKey(process.env.REACT_APP_PaymentKEY);
+          window.POS.setEnvironment(process.env.REACT_APP_PaymentENV);
+          const style = {
+            base: {
+              secureFields: {
+                width: 'calc(100% - 45px)'
+              },
+              pan: {
+                display: 'inline-block',
+                width: '50%'
+              },
+              expirationDate: {
+                display: 'inline-block',
+                width: '30%'
+              },
+              cvv: {
+                display: 'inline-block',
+                width: '20%'
+              }
+            }
+          };
+          window.POS.setStyle(style);
+          window.POS.initSecureFields('card-secure-fields');
+          try {
+            document
+              .getElementById('zoozIframe')
+              .setAttribute('scrolling', 'no');
+          } catch (e) {}
+          if (document.getElementById('payment-form')) {
+            document
+              .getElementById('payment-form')
+              .addEventListener('submit', function (event) {
+                console.log(document.getElementById('cardholder-name'));
+                event.preventDefault();
+                const additionalData = {
+                  holder_name: document.getElementById('cardholder-name').value // This field is mandatory
+                };
+                window.POS.createToken(additionalData, function (result) {
+                  console.log(result, 'result');
+                  // Grab the token here
+                  sessionStorage.setItem('payosdata', result);
+                });
+              });
+          }
+        }
+      );
+    }
     // loadJS('https://js.paymentsos.com/v2/0.0.1/token.min.js', function () {
     //   window.POS.setPublicKey(process.env.REACT_APP_PaymentKEY);
     //   window.POS.setEnvironment(process.env.REACT_APP_PaymentENV);
