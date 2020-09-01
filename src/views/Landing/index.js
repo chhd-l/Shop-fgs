cf51eb192aa56e810690e5e4ce294289edd2e1eb
knifecome <qhx717@qq.com>
2020-09-01 16:46:46 +0800

    更改landingpage input有value值时的样式

--- src/views/Landing/index.js
+++ src/views/Landing/index.js
@@ -19,9 +19,17 @@ class Landing extends Component {
             ]
         };
     }
+    changeEvent=(e)=>{
+        let copyArr = [...this.state.inputType]
+        copyArr[e.target.name].value = e.target.value
+        this.setState({
+            inputType: copyArr
+        })
+    }
     render() {
         return (
             <div className="landing-wrap">
+                {/* logo */}
                 <Link to="/" className="header__nav__brand logo-home pt-2">
                     <span className="rc-screen-reader-text"></span>
                     <img
@@ -31,14 +39,17 @@ class Landing extends Component {
                     />
                 </Link>
                 <div style={{ marginTop: '100px' }}>
+                    {/* banger视频 */}
                     <Link to="/" style={{display:'flex',justifyContent:'center',height:'300px'}}>
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/Yiktt8SkL3A?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </Link>
 
+                    {/* 介绍 */}
                     <p style={{ textAlign: 'justify', color: '#000', fontSize: '14px', marginTop: '30px' }}>Te presentamos la innovación de Royal Canin México, eVet. La manera más fácil para tus pacientes de adquirir las dietas veterinarias y de prescripción de Royal Canin por medio de una plataforma exclusiva para veterinarios. Todo esto por medio de tu prescripción, ya que para nosotros lo más importante es el bienestar de los gatos y los perros, y tu Médico Veterinario tienes la batuta en el alimento a recomendar, por favor ponte en contacto con nosotros para saber cómo integrar tu clínica en esta nueva forma de estar en contacto con tus clientes y paciente.</p>
 
+                    {/* form */}
                     {
-                        this.state.inputType.map(item => {
+                        this.state.inputType.map((item,index) => {
                             return (
                                 <div class="rc-two-column" style={{ alignItems: 'baseline' }}>
                                     <div class="rc-column" style={{ padding: 0 }}>
@@ -49,8 +60,8 @@ class Landing extends Component {
                                     <div class="rc-column" style={{ padding: '0 0 0 20px' }}>
                                         <div class="rc-content-v-left">
                                             <span class="rc-input rc-input--inline rc-input--label">
-                                                <input class="rc-input__control" id="id-textsuffix" type="text" name="text" style={{ fontSize: '12px',paddingBottom:0 }} />
-                                                <label class="rc-input__label" for="id-textsuffix" style={{ fontSize: '12px' }}>
+                                                <input class="rc-input__control" id="id-textsuffix" type="text" name="text" style={{ fontSize: '12px',paddingBottom:0 }} name={index} onChange={this.changeEvent} />
+                                                <label class="rc-input__label" for="id-textsuffix" style={{ fontSize: '12px'}}>
                                                     <span class="rc-input__label-text">Text input</span>
                                                 </label>
                                             </span>
@@ -62,7 +73,7 @@ class Landing extends Component {
                     }
 
 
-                    {/* 按钮 */}
+                    {/* submit按钮 */}
                     <div style={{ textAlign: 'center', margin: '30px 0 ' }}>
                         <button className="rc-btn rc-btn rc-btn--one px-5">Submit</button>
                     </div>
