7a2f539a51361831adbaf48ee83b4bae1d1484ae
knifecome <qhx717@qq.com>
2020-09-03 14:57:24 +0800

    完成footer链接

--- src/components/Footer/menubar.js
+++ src/components/Footer/menubar.js
@@ -45,7 +45,7 @@ export const menubar = {
             titleId:'footer.help2',list: [{url:"https://shopstg.royalcanin.com/help",messageId:'footer.contacts'},{link:'/FAQVersand',messageId:'footer.FAQVersand'},{link:'/FAQAllgemein',messageId:'footer.FAQAllgemeines'}]
         }],
         [{
-            titleId:'footer.Additionally2',list: [{url:"https://www.mars.com/privacy-policy-germany",messageId:'footer.privacyPolicy2'},{url:'',messageId:'footer.termsOfService'},{link:'/Widerrufsbelehrung',messageId:'footer.withdrawal'}]
+            titleId:'footer.Additionally2',list: [{url:"https://www.mars.com/privacy-policy-germany",messageId:'footer.privacyPolicy2'},{url:'https://www.royalcanin.com/de/about-us/sales-and-delivery-conditions',messageId:'footer.termsOfService'},{link:'/Widerrufsbelehrung',messageId:'footer.withdrawal'}]
         }],
     ]
 }
