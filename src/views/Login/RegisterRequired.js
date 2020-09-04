a969d279b3f4448d7e3aa210ef7ee9e5252aee6a
knifecome <qhx717@qq.com>
2020-09-04 17:56:23 +0800

    update

--- src/views/Login/RegisterRequired.js
+++ src/views/Login/RegisterRequired.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { injectIntl, FormattedMessage } from "react-intl";
 import logoAnimatedPng from "@/assets/images/logo--animated2.png";
 import "./index.css"
-import { findUserConsentList,consentListDetail } from "@/api/consent"
+import { findUserConsentList, consentListDetail } from "@/api/consent"
 // import { confirmAndCommit } from "@/api/payment";
 // import {  Link } from 'react-router-dom'
 // import store from "storejs";
@@ -12,23 +12,43 @@ class RegisterRequired extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isReadPrivacyPolicy: false,
-            isShipTracking: false,
-            isNewsLetter: false
+            list: [],
         };
     }
     async componentDidMount() {
         try {
             const result = await findUserConsentList({})
-            console.log(result)
-            
+            const optioalList = result.context.optioalList.map(item => {
+                return {
+                    id: item.id,
+                    consentTitle: item.consentTitle,
+                    isChecked: false,
+                    isRequired: true
+                }
+            })
+
+            const requiredList = result.context.requiredList.map(item => {
+                return {
+                    id: item.id,
+                    consentTitle: item.consentTitle,
+                    isChecked: false,
+                    isRequired: false
+                }
+            })
+
+            //把非必填和必填的项目组装成一个数组list，用于渲染
+            let list = this.state.list
+            list = [...optioalList, ...requiredList]
+            this.setState({
+                list
+            })
+
             const result2 = await consentListDetail(
                 {
-                    "consentKey":18,
-                    "contentTitle":"privacy policy"
+                    "consentKey": 18,
+                    "contentTitle": "privacy policy"
                 }
             )
-            console.log(result2)
             console.log(sessionStorage.getItem('okta-redirectUrl'))
         } catch (err) {
             console.log(err.message)
@@ -49,69 +69,43 @@ class RegisterRequired extends Component {
                 <p style={{ textAlign: 'center', color: '#5F5F5F', fontSize: '20px' }}>Complete registeration</p>
                 {/* checkbox */}
                 <div className="required-checkbox" style={{ marginTop: '80px' }}>
-                    <div className="footerCheckbox rc-margin-top--sm mt-3">
-                        <input
-                            className="form-check-input ui-cursor-pointer-pure"
-                            id="id-checkbox-cat-2"
-                            value=""
-                            type="checkbox"
-                            name="checkbox-2"
-                            onChange={() => {
-                                this.setState({
-                                    isReadPrivacyPolicy: !this.state.isReadPrivacyPolicy,
-                                });
-                            }}
-                            checked={this.state.isReadPrivacyPolicy}
-                        />
-                        <label
-                            htmlFor="id-checkbox-cat-2"
-                            className="rc-input__label--inline ui-cursor-pointer-pure"
-                        >
-                            I accept <a className="terms-link" href="https://www.mars.com/privacy-policy-germany">privacy policy</a> and <a className="terms-link" href="https://www.mars.com/privacy-policy-germany">terms of use.</a><span className="pl-2 rc-text-colour--brand1">*</span>
-                        </label>
-                    </div>
-                    <div className="footerCheckbox">
-                        <input
-                            className="form-check-input ui-cursor-pointer-pure"
-                            id="id-checkbox-cat-1"
-                            value="Cat"
-                            type="checkbox"
-                            name="checkbox-2"
-                            onChange={() => {
-                                this.setState({
-                                    isShipTracking: !this.state.isShipTracking,
-                                });
-                            }}
-                            checked={this.state.isShipTracking}
-                        />
-                        <label
-                            htmlFor="id-checkbox-cat-1"
-                            className="rc-input__label--inline ui-cursor-pointer-pure"
-                        >
-                            I am over 18 years old.<span className="pl-2 rc-text-colour--brand1">*</span>
-                        </label>
-                    </div>
-                    <div className="footerCheckbox">
-                        <input
-                            className="form-check-input ui-cursor-pointer-pure"
-                            id="id-checkbox-cat-3"
-                            value="Cat"
-                            type="checkbox"
-                            name="checkbox-2"
-                            onChange={() => {
-                                this.setState({
-                                    isNewsLetter: !this.state.isNewsLetter,
-                                });
-                            }}
-                            checked={this.state.isNewsLetter}
-                        />
-                        <label
-                            htmlFor="id-checkbox-cat-3"
-                            className="rc-input__label--inline ui-cursor-pointer-pure"
-                        >
-                            I agree to accept the marketing email.
-                    </label>
-                    </div>
+                    {
+                        this.state.list.map((item,index) => {
+                            return (
+                                <div className="footerCheckbox" key={index}>
+                                    <input
+                                        className="form-check-input ui-cursor-pointer-pure"
+                                        id="id-checkbox-cat-2"
+                                        value=""
+                                        type="checkbox"
+                                        name="checkbox-2"
+                                        onChange={() => {
+                                            //替换属性start
+                                            let itemObj = Object.assign(item,{
+                                                isChecked:!item.isChecked
+                                            })
+                                            let list = [...this.state.list]
+                                            list.splice(index,1,itemObj) 
+                                            this.setState({
+                                                list
+                                            });
+                                            //替换属性end
+                                        }}
+                                        checked={item.isChecked}
+                                    />
+                                    {/* <label
+                                        htmlFor="id-checkbox-cat-2"
+                                        className="rc-input__label--inline ui-cursor-pointer-pure"
+                                    >
+                                        I accept <a className="terms-link" href="https://www.mars.com/privacy-policy-germany">privacy policy</a> and <a className="terms-link" href="https://www.mars.com/privacy-policy-germany">terms of use.</a>{item.isRequired?<span className="pl-2 rc-text-colour--brand1">*</span>:null}
+                                    </label> */}
+                                    
+                                    {item.consentTitle}
+                                </div>
+                            )
+                        })
+                    }
+
                 </div>
                 {/* 注释 */}
                 <p className='pizhu'><span className="pl-2 pr-2 rc-text-colour--brand1">*</span>Required fields</p>
