bfda7bc6c1700be67f5b28c05dc6aa9ad692ddd2
yuqi zhu <yuqizhu@yuqideMacBook-Pro.local>
2020-09-07 16:27:39 +0800

    修改okta本地配置

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export default {
     clientId: CLIENT_ID,
     issuer: ISSUER,
     // redirectUri: 'https://shopuat.466920.com/implicit/callback',
-    redirectUri: process.env.NODE_ENV === 'development'? 'http://localhost:3000/mx/implicit/callback': process.env.REACT_APP_RedirectURL,
+    redirectUri: process.env.NODE_ENV === 'development'? 'http://localhost:3000/implicit/callback': process.env.REACT_APP_RedirectURL,
     // redirectUri: 'http://localhost:3000/implicit/callback',
     scopes: ['openid', 'profile', 'email','user.consent:read','user.profile:write','user.consent:delete','user.consent:collect'],
     pkce: true,
