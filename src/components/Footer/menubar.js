393c514a6a34c71038bcb6d56984c10ba818d73a
minyatang <mytang11@163.com>
2020-09-11 15:49:39 +0800

    FAQ增加分类联调

--- src/components/Footer/menubar.js
+++ src/components/Footer/menubar.js
@@ -1,54 +1,202 @@
 export const menubar = {
-    // titleId:列头部名称，list：每列的数据 {prop：stone portal配置的数据，messageId：列数据名称，url：前端自定义超链接，link：前端路由}
-    en:[
-        [{
-          titleId:'footer.aboutRoyalCanin',list: [{prop:'contactUsUrl',messageId:'aboutUs2'},{prop:'ourValues',url:'https://www.royalcanin.com/mx/about-us/our-values',messageId:'footer.ourValues'},{prop:'qualityAndSafety',url:'https://www.royalcanin.com/mx/about-us/quality-and-food-safety',messageId:'footer.qualityAndSafety'},{prop:'specificNutrition',url:'https://www.royalcanin.com/mx/tailored-nutrition',messageId:'footer.healthAndNutrition'}]
-        }],
-        [{
-            titleId:'footer.products',list: [{link:"/list/cats",messageId:'cats'},{link:'/list/dogs',messageId:'dogs'}]
-        }],
-        [{
-            titleId:'footer.help',list: [{link:"/help",messageId:'footer.contacts'},{link:'/FAQ',messageId:'footer.FAQ'},{link:'/requestinvoice',messageId:'footer.RequestInvoice'}]
-        }],
-        [{
-            titleId:'footer.Additionally',list: [{link:"/privacypolicy",messageId:'footer.privacyPolicy'},{prop:'informationForParents',messageId:'footer.informationForParents'},{link:'/termuse',messageId:'footer.websiteTermsOfUse'},{prop:'cookiesUrl',messageId:'footer.cookieCollectionPolicy'}]
-        }],
+  // titleId:列头部名称，list：每列的数据 {prop：stone portal配置的数据，messageId：列数据名称，url：前端自定义超链接，link：前端路由}
+  en: [
+    [
+      {
+        titleId: 'footer.aboutRoyalCanin',
+        list: [
+          { prop: 'contactUsUrl', messageId: 'aboutUs2' },
+          {
+            prop: 'ourValues',
+            url: 'https://www.royalcanin.com/mx/about-us/our-values',
+            messageId: 'footer.ourValues'
+          },
+          {
+            prop: 'qualityAndSafety',
+            url:
+              'https://www.royalcanin.com/mx/about-us/quality-and-food-safety',
+            messageId: 'footer.qualityAndSafety'
+          },
+          {
+            prop: 'specificNutrition',
+            url: 'https://www.royalcanin.com/mx/tailored-nutrition',
+            messageId: 'footer.healthAndNutrition'
+          }
+        ]
+      }
     ],
-    es:[
-        [{
-          titleId:'footer.aboutRoyalCanin',list: [{prop:'contactUsUrl',messageId:'aboutUs2'},{prop:'ourValues',url:'https://www.royalcanin.com/mx/about-us/our-values',messageId:'footer.ourValues'},{prop:'qualityAndSafety',url:'https://www.royalcanin.com/mx/about-us/quality-and-food-safety',messageId:'footer.qualityAndSafety'},{prop:'specificNutrition',url:'https://www.royalcanin.com/mx/tailored-nutrition',messageId:'footer.healthAndNutrition'}]
-        }],
-        [{
-            titleId:'footer.products',list: [{link:"/list/cats",messageId:'cats'},{link:'/list/dogs',messageId:'dogs'}]
-        }],
-        [{
-            titleId:'footer.help',list: [{link:"/help",messageId:'footer.contacts'},{link:'/FAQ',messageId:'footer.FAQ'},{link:'/requestinvoice',messageId:'footer.RequestInvoice'}]
-        }],
-        [{
-            titleId:'footer.Additionally',list: [{link:"/privacypolicy",messageId:'footer.privacyPolicy'},{prop:'informationForParents',messageId:'footer.informationForParents'},{link:'/termuse',messageId:'footer.websiteTermsOfUse'},{prop:'cookiesUrl',messageId:'footer.cookieCollectionPolicy'}]
-        }],
+    [
+      {
+        titleId: 'footer.products',
+        list: [
+          { link: '/list/cats', messageId: 'cats' },
+          { link: '/list/dogs', messageId: 'dogs' }
+        ]
+      }
     ],
-    de:[
-        [{
-            titleId:'aboutUs',list: [
-                {url:'https://www.royalcanin.com/de/about-us/our-history#Our%20history',messageId:'footer.history'},
-                {props:'ourValues',url:'https://www.royalcanin.com/de/about-us/our-values#Our%20values',messageId:'footer.ourValues'},{url:'https://www.royalcanin.com/de/about-us/sustainability#Sustainability',messageId:'footer.consistence'},
-                {url:'https://www.royalcanin.com/de/about-us/qualitat-und-futtermittelsicherheit',messageId:'footer.qualityAndSafety'},{url:'https://www.royalcanin.com/de/about-us/news',messageId:'footer.news'},
-                {url:'https://www.royalcanin.com/de/about-us/acceptance-guarantee',messageId:'footer.acceptance'},
-                {url:'https://www.royalcanin.com/de/about-us/newsletter',messageId:'footer.newsletter'},
-                {url:'https://www.royalcanin.com/de/about-us/promotions',messageId:'footer.action'}]
-        }],
-        [{
-            titleId:'footer.products',list: [{link:"/list/cats",messageId:'de_cats'},{link:'/list/dogs',messageId:'de_dogs'}]
-        }],
-        [{
-            titleId:'footer.help2',list: [{url:"https://shopstg.royalcanin.com/help",messageId:'footer.contacts'},{link:'/FAQVersand',messageId:'footer.FAQVersand'},{link:'/FAQAllgemein',messageId:'footer.FAQAllgemeines'}]
-        }],
-        [{
-            titleId:'footer.Additionally2',list: 
-            [{url:"https://www.mars.com/privacy-policy-germany",messageId:'footer.privacyPolicy2'},
-            {link:'/Terms-And-Conditions',messageId:'footer.termsOfService'},
-            {link:'/Widerrufsbelehrung',messageId:'footer.withdrawal'}]
-        }],
+    [
+      {
+        titleId: 'footer.help',
+        list: [
+          { link: '/help', messageId: 'footer.contacts' },
+          { link: '/FAQ/all', messageId: 'footer.FAQ' },
+          { link: '/requestinvoice', messageId: 'footer.RequestInvoice' }
+        ]
+      }
+    ],
+    [
+      {
+        titleId: 'footer.Additionally',
+        list: [
+          { link: '/privacypolicy', messageId: 'footer.privacyPolicy' },
+          {
+            prop: 'informationForParents',
+            messageId: 'footer.informationForParents'
+          },
+          { link: '/termuse', messageId: 'footer.websiteTermsOfUse' },
+          { prop: 'cookiesUrl', messageId: 'footer.cookieCollectionPolicy' }
+        ]
+      }
     ]
-}
+  ],
+  es: [
+    [
+      {
+        titleId: 'footer.aboutRoyalCanin',
+        list: [
+          { prop: 'contactUsUrl', messageId: 'aboutUs2' },
+          {
+            prop: 'ourValues',
+            url: 'https://www.royalcanin.com/mx/about-us/our-values',
+            messageId: 'footer.ourValues'
+          },
+          {
+            prop: 'qualityAndSafety',
+            url:
+              'https://www.royalcanin.com/mx/about-us/quality-and-food-safety',
+            messageId: 'footer.qualityAndSafety'
+          },
+          {
+            prop: 'specificNutrition',
+            url: 'https://www.royalcanin.com/mx/tailored-nutrition',
+            messageId: 'footer.healthAndNutrition'
+          }
+        ]
+      }
+    ],
+    [
+      {
+        titleId: 'footer.products',
+        list: [
+          { link: '/list/cats', messageId: 'cats' },
+          { link: '/list/dogs', messageId: 'dogs' }
+        ]
+      }
+    ],
+    [
+      {
+        titleId: 'footer.help',
+        list: [
+          { link: '/help', messageId: 'footer.contacts' },
+          { link: '/FAQ/all', messageId: 'footer.FAQ' },
+          { link: '/requestinvoice', messageId: 'footer.RequestInvoice' }
+        ]
+      }
+    ],
+    [
+      {
+        titleId: 'footer.Additionally',
+        list: [
+          { link: '/privacypolicy', messageId: 'footer.privacyPolicy' },
+          {
+            prop: 'informationForParents',
+            messageId: 'footer.informationForParents'
+          },
+          { link: '/termuse', messageId: 'footer.websiteTermsOfUse' },
+          { prop: 'cookiesUrl', messageId: 'footer.cookieCollectionPolicy' }
+        ]
+      }
+    ]
+  ],
+  de: [
+    [
+      {
+        titleId: 'aboutUs',
+        list: [
+          {
+            url:
+              'https://www.royalcanin.com/de/about-us/our-history#Our%20history',
+            messageId: 'footer.history'
+          },
+          {
+            props: 'ourValues',
+            url:
+              'https://www.royalcanin.com/de/about-us/our-values#Our%20values',
+            messageId: 'footer.ourValues'
+          },
+          {
+            url:
+              'https://www.royalcanin.com/de/about-us/sustainability#Sustainability',
+            messageId: 'footer.consistence'
+          },
+          {
+            url:
+              'https://www.royalcanin.com/de/about-us/qualitat-und-futtermittelsicherheit',
+            messageId: 'footer.qualityAndSafety'
+          },
+          {
+            url: 'https://www.royalcanin.com/de/about-us/news',
+            messageId: 'footer.news'
+          },
+          {
+            url: 'https://www.royalcanin.com/de/about-us/acceptance-guarantee',
+            messageId: 'footer.acceptance'
+          },
+          {
+            url: 'https://www.royalcanin.com/de/about-us/newsletter',
+            messageId: 'footer.newsletter'
+          },
+          {
+            url: 'https://www.royalcanin.com/de/about-us/promotions',
+            messageId: 'footer.action'
+          }
+        ]
+      }
+    ],
+    [
+      {
+        titleId: 'footer.products',
+        list: [
+          { link: '/list/cats', messageId: 'de_cats' },
+          { link: '/list/dogs', messageId: 'de_dogs' }
+        ]
+      }
+    ],
+    [
+      {
+        titleId: 'footer.help2',
+        list: [
+          {
+            url: 'https://shopstg.royalcanin.com/help',
+            messageId: 'footer.contacts'
+          },
+          { link: '/FAQ/catogery-0', messageId: 'footer.FAQVersand' },
+          { link: '/FAQ/catogery-1', messageId: 'footer.FAQAllgemeines' }
+        ]
+      }
+    ],
+    [
+      {
+        titleId: 'footer.Additionally2',
+        list: [
+          {
+            url: 'https://www.mars.com/privacy-policy-germany',
+            messageId: 'footer.privacyPolicy2'
+          },
+          { link: '/Terms-And-Conditions', messageId: 'footer.termsOfService' },
+          { link: '/Widerrufsbelehrung', messageId: 'footer.withdrawal' }
+        ]
+      }
+    ]
+  ]
+};
