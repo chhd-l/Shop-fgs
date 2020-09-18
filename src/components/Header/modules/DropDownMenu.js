cfb6cf4da9afbe14c9fca6215d63fa0c9eddf6c7
minyatang <mytang11@163.com>
2020-09-18 09:42:32 +0800

    容错

--- src/components/Header/modules/DropDownMenu.js
+++ src/components/Header/modules/DropDownMenu.js
@@ -139,36 +139,36 @@ export default class DropDownMenu extends React.Component {
     const { visibleType } = this.props;
     return (
       <div
-        class={`dropdown-nav d-flex ${visibleType === item.key ? 'show' : ''}`}
+        className={`dropdown-nav d-flex ${visibleType === item.key ? 'show' : ''}`}
         aria-hidden={visibleType === item.key}
         onMouseOver={() => this.hanldeListItemMouseOver(item.key)}
         onMouseOut={this.hanldeListItemMouseOut}
         key={i}
       >
-        <div class="flex-grow-1 rc-padding-y--xs rc-padding-left--sm--desktop">
+        <div className="flex-grow-1 rc-padding-y--xs rc-padding-left--sm--desktop">
           <ul
-            class="d-flex justify-content-center rc-padding--none rc-margin--none fullHeight"
+            className="d-flex justify-content-center rc-padding--none rc-margin--none fullHeight"
             role="menu"
           >
             {item.menus.map((mitem, mIndx) => (
               <li
-                class="dropdown-nav__item rc-padding-top--xs relative"
+                className="dropdown-nav__item rc-padding-top--xs relative"
                 role="menuitem"
                 key={mIndx}
               >
-                <div class="dropdown-nav__title rc-margin-bottom--xs">
-                  <span id="dog-age" class="dropdown-nav__item">
+                <div className="dropdown-nav__title rc-margin-bottom--xs">
+                  <span id="dog-age" className="dropdown-nav__item">
                     <small></small>
                     <b>{mitem.name}</b>
                   </span>
                 </div>
-                <ul class="rc-padding--none" role="menu" aria-hidden="true">
+                <ul className="rc-padding--none" role="menu" aria-hidden="true">
                   {mitem.children.map((citem, cIndex) => (
-                    <li class="dropdown-nav__item" role="menuitem" key={cIndex}>
+                    <li className="dropdown-nav__item" role="menuitem" key={cIndex}>
                       <Link
                         to={citem.linkObj}
                         role="button"
-                        class="dropdown-nav__link"
+                        className="dropdown-nav__link"
                       >
                         {citem.name}
                       </Link>
@@ -176,8 +176,8 @@ export default class DropDownMenu extends React.Component {
                   ))}
                 </ul>
                 {mIndx === 0 && (
-                  <div class="dropdown-nav__cat-link rc-padding-bottom--xs">
-                    <Link class="rc-styled-link" to="/list/dogs">
+                  <div className="dropdown-nav__cat-link rc-padding-bottom--xs">
+                    <Link className="rc-styled-link" to="/list/dogs">
                       Voir tous les produits
                     </Link>
                   </div>
@@ -186,26 +186,26 @@ export default class DropDownMenu extends React.Component {
             ))}
           </ul>
         </div>
-        <div class="content-asset">
-          <div class="dropdown-nav__banner rc-bg-colour--brand4 flex-column flex-sm-row">
-            <div class="align-self-center rc-padding-left--md rc-padding-right--xs rc-padding-y--lg--mobile">
-              <div class="rc-large-intro rc-margin-bottom--sm inherit-fontsize">
+        <div className="content-asset">
+          <div className="dropdown-nav__banner rc-bg-colour--brand4 flex-column flex-sm-row">
+            <div className="align-self-center rc-padding-left--md rc-padding-right--xs rc-padding-y--lg--mobile">
+              <div className="rc-large-intro rc-margin-bottom--sm inherit-fontsize">
                 <p>{item.desc && item.desc.text}</p>
               </div>
               <Link to={item.mainLink}>
-                <button class="rc-btn rc-btn--one">
+                <button className="rc-btn rc-btn--one">
                   Trouver l'alimentation adaptée
                 </button>
               </Link>
             </div>
-            <div class="mt-auto">
+            <div className="mt-auto">
               <img
-                class="pull-right rc-lg-up ls-is-cached lazyloaded"
+                className="pull-right rc-lg-up ls-is-cached lazyloaded"
                 data-src={item.desc && item.desc.img}
                 src={item.desc && item.desc.img}
               />
               <img
-                class="pull-right rc-md-down lazyload"
+                className="pull-right rc-md-down lazyload"
                 data-src={item.desc && item.desc.img}
               />
             </div>
@@ -218,107 +218,108 @@ export default class DropDownMenu extends React.Component {
     const { visibleType } = this.props;
     return (
       <div
-        class={`dropdown-nav d-flex full-width-asset justify-content-center ${
+        className={`dropdown-nav d-flex full-width-asset justify-content-center ${
           visibleType === item.key ? 'show' : ''
         }`}
         aria-hidden={visibleType === item.key}
         onMouseOver={() => this.hanldeListItemMouseOver(item.key)}
         onMouseOut={this.hanldeListItemMouseOut}
+        key={i}
       >
-        <div class="content-asset">
-          <div class="dropdown-nav__help d-md-flex">
+        <div className="content-asset">
+          <div className="dropdown-nav__help d-md-flex">
             {' '}
-            <div class="dropdown-nav__help__text align-self-center">
+            <div className="dropdown-nav__help__text align-self-center">
               {' '}
-              <h4 class="title rc-delta">Une question ?</h4>{' '}
-              <div class="desc children-nomargin">
+              <h4 className="title rc-delta">Une question ?</h4>{' '}
+              <div className="desc children-nomargin">
                 {' '}
                 <p>Vous avez besoin d'aide?</p>
                 <p>N'hésitez pas à nous contacter : </p>{' '}
               </div>{' '}
             </div>{' '}
-            <div class="dropdown-nav__help__card call-us rc-border-all rc-border-colour--interface d-flex align-items-center">
+            <div className="dropdown-nav__help__card call-us rc-border-all rc-border-colour--interface d-flex align-items-center">
               {' '}
-              <div class="rc-margin-right--xs flex-grow-1">
+              <div className="rc-margin-right--xs flex-grow-1">
                 {' '}
                 <b>Par téléphone</b>{' '}
-                <div class="children-nomargin">
+                <div className="children-nomargin">
                   {' '}
                   <p>De 8h00 à 20h00</p>{' '}
                 </div>{' '}
                 <div>
-                  <a href="tel:+0 800 005 360" class="rc-large-body tel">
+                  <a href="tel:+0 800 005 360" className="rc-large-body tel">
                     0 800 005 360
                   </a>
                 </div>{' '}
               </div>{' '}
-              <div class="rc-padding-left--xs rc-lg-up">
+              <div className="rc-padding-left--xs rc-lg-up">
                 {' '}
                 <img
-                  class=" ls-is-cached lazyloaded"
+                  className=" ls-is-cached lazyloaded"
                   data-src="https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-FR-Library/default/dw77342d81/subscription/icon callus@2x.png?sw=65&amp;sh=65&amp;sm=fit&amp;cx=4&amp;cy=0&amp;cw=180&amp;ch=180&amp;sfrm=png"
                   alt="Par téléphone icon"
                   src="https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-FR-Library/default/dw77342d81/subscription/icon callus@2x.png?sw=65&amp;sh=65&amp;sm=fit&amp;cx=4&amp;cy=0&amp;cw=180&amp;ch=180&amp;sfrm=png"
                 />{' '}
               </div>{' '}
-              <div class="rc-padding-left--xs rc-md-down">
+              <div className="rc-padding-left--xs rc-md-down">
                 {' '}
                 <img
-                  class="lazyload"
+                  className="lazyload"
                   data-src="https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-FR-Library/default/dw77342d81/subscription/icon callus@2x.png?sw=65&amp;sh=65&amp;sm=fit&amp;cx=4&amp;cy=0&amp;cw=180&amp;ch=180&amp;sfrm=png"
                   alt="Par téléphone icon"
                 />{' '}
               </div>{' '}
             </div>{' '}
             <Link
-              class="dropdown-nav__help__card email-us rc-border-all rc-border-colour--interface d-flex align-items-center"
+              className="dropdown-nav__help__card email-us rc-border-all rc-border-colour--interface d-flex align-items-center"
               to="/help"
             >
               {' '}
-              <div class="rc-margin-right--xs flex-grow-1">
+              <div className="rc-margin-right--xs flex-grow-1">
                 {' '}
-                <b>Par e-mail</b> <div class="children-nomargin"> </div>{' '}
+                <b>Par e-mail</b> <div className="children-nomargin"> </div>{' '}
               </div>{' '}
-              <div class="rc-padding-left--xs rc-lg-up">
+              <div className="rc-padding-left--xs rc-lg-up">
                 {' '}
                 <img
-                  class=" ls-is-cached lazyloaded"
+                  className=" ls-is-cached lazyloaded"
                   data-src="https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-FR-Library/default/dw72994029/subscription/Emailus_icon@2x.png?sw=65&amp;sh=65&amp;sm=fit&amp;cx=0&amp;cy=0&amp;cw=180&amp;ch=180&amp;sfrm=png"
                   alt="Par e-mail icon"
                   src="https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-FR-Library/default/dw72994029/subscription/Emailus_icon@2x.png?sw=65&amp;sh=65&amp;sm=fit&amp;cx=0&amp;cy=0&amp;cw=180&amp;ch=180&amp;sfrm=png"
                 />{' '}
               </div>{' '}
-              <div class="rc-padding-left--xs rc-md-down">
+              <div className="rc-padding-left--xs rc-md-down">
                 {' '}
                 <img
-                  class="lazyload"
+                  className="lazyload"
                   data-src="https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-FR-Library/default/dw72994029/subscription/Emailus_icon@2x.png?sw=65&amp;sh=65&amp;sm=fit&amp;cx=0&amp;cy=0&amp;cw=180&amp;ch=180&amp;sfrm=png"
                   alt="Par e-mail icon"
                 />{' '}
               </div>{' '}
             </Link>{' '}
             <Link
-              class="dropdown-nav__help__card faq rc-border-all rc-border-colour--interface d-flex align-items-center"
+              className="dropdown-nav__help__card faq rc-border-all rc-border-colour--interface d-flex align-items-center"
               to="/FAQ/all"
             >
               {' '}
-              <div class="rc-margin-right--xs flex-grow-1">
+              <div className="rc-margin-right--xs flex-grow-1">
                 {' '}
-                <b>FAQ</b> <div class="children-nomargin"> </div>{' '}
+                <b>FAQ</b> <div className="children-nomargin"> </div>{' '}
               </div>{' '}
-              <div class="rc-padding-left--xs rc-lg-up">
+              <div className="rc-padding-left--xs rc-lg-up">
                 {' '}
                 <img
-                  class=" ls-is-cached lazyloaded"
+                  className=" ls-is-cached lazyloaded"
                   data-src="https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-FR-Library/default/dw4893a52e/subscription/FAQ_icon@2x.png?sw=65&amp;sh=65&amp;sm=fit&amp;cx=0&amp;cy=0&amp;cw=180&amp;ch=180&amp;sfrm=png"
                   alt="FAQ icon"
                   src="https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-FR-Library/default/dw4893a52e/subscription/FAQ_icon@2x.png?sw=65&amp;sh=65&amp;sm=fit&amp;cx=0&amp;cy=0&amp;cw=180&amp;ch=180&amp;sfrm=png"
                 />{' '}
               </div>{' '}
-              <div class="rc-padding-left--xs rc-md-down">
+              <div className="rc-padding-left--xs rc-md-down">
                 {' '}
                 <img
-                  class="lazyload"
+                  className="lazyload"
                   data-src="https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-FR-Library/default/dw4893a52e/subscription/FAQ_icon@2x.png?sw=65&amp;sh=65&amp;sm=fit&amp;cx=0&amp;cy=0&amp;cw=180&amp;ch=180&amp;sfrm=png"
                   alt="FAQ icon"
                 />{' '}
@@ -331,7 +332,7 @@ export default class DropDownMenu extends React.Component {
   };
   render() {
     return (
-      <div class="rc-md-up">
+      <div className="rc-md-up">
         {subMenuCfg.map((item, i) =>
           item.key !== 'help'
             ? this._renderDogOrCatMenu(item, i)
