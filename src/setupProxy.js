3e24bfce16f986d2aa85adae1e254f081168e1f8
knifecome <qhx717@qq.com>
2020-09-28 19:33:15 +0800

    Update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
   app.use(proxy('/api', {
     // target: process.env.VUE_APP_URL,
     // target:'http://192.168.2.20:8090/api',
-    // target: process.env.REACT_APP_BASEURL, 
-    target: 'https://shopstg.royalcanin.com/api', 
+     target: process.env.REACT_APP_BASEURL, 
+    //target: 'https://shopstg.royalcanin.com/api', 
     secure: false,
     changeOrigin: true,
     pathRewrite: {
