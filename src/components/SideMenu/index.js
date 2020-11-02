f33d7284186d2e34129aed6e5b407d5599cce8e4
minyatang <mytang11@163.com>
2020-11-02 18:30:00 +0800

    1. modify styles

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -1,121 +1,83 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { FormattedMessage } from 'react-intl'
-import { inject, observer } from 'mobx-react';
+import { FormattedMessage } from 'react-intl';
 
-@inject("configStore")
-@observer
+const menuList = [
+  {
+    catogery: 'Home',
+    icon: <span className="rc-icon rc-grid-view--xs rc-iconography" />,
+    langKey: 'home',
+    url: '/account'
+  },
+  {
+    catogery: 'Profile',
+    icon: <span className="rc-icon rc-user--xs rc-iconography" />,
+    langKey: 'account.profile',
+    url: '/account/information'
+  },
+  {
+    catogery: 'Pets',
+    icon: <span className="rc-icon rc-user--xs rc-iconography" />,
+    langKey: 'pets',
+    url: '/account/pets'
+  },
+  {
+    catogery: 'Orders',
+    icon: <span className="rc-icon rc-cart--xs rc-iconography" />,
+    langKey: 'orders',
+    url: '/account/orders'
+  },
+  {
+    catogery: 'Subscription',
+    icon: <span className="rc-icon rc-cart--xs rc-iconography" />,
+    langKey: 'subscription',
+    url: '/account/subscription'
+  },
+  {
+    catogery: 'ShippingAddress',
+    icon: <span className="rc-icon rc-cart--xs rc-iconography" />,
+    langKey: 'shippingAddress',
+    url: '/account/shippingAddress'
+  },
+  {
+    catogery: 'PaymentMethod',
+    icon: <span className="rc-icon rc-cart--xs rc-iconography" />,
+    langKey: 'paymentMethod',
+    url: '/account/paymentMethod'
+  },
+  {
+    catogery: 'Faq',
+    icon: <span className="rc-icon rc-cart--xs rc-iconography" />,
+    langKey: 'footer.FAQ',
+    url: '/FAQ/all'
+  }
+];
+
 class SideMenu extends React.Component {
-  render () {
-    const { type } = this.props
+  render() {
+    const { type } = this.props;
     return (
       <div className="my__account-navigation rc-column rc-padding-top--xs--desktop rc-padding-bottom--none">
-        <h1 className="my__account-header rc-alpha">
-          <FormattedMessage id="account.personalArea">
-            {txt => (
-              <Link
-                to="/account"
-                title={txt}
-                alt={txt}>
-                {txt}
-              </Link>
-            )}
-          </FormattedMessage>
-        </h1>
-        <div>
-          <h2 className={['nav_item', 'medium', type === 'Profile' ? 'active' : ''].join(' ')}>
-            <FormattedMessage id="account.profile">
-              {txt => (
-                <Link
-                  to="/account/information"
-                  title={txt}
-                  alt={txt}>
+        {menuList.map((item, i) => (
+          <h2
+            key={i}
+            className={`nav_item medium ${
+              type === item.catogery ? 'active' : ''
+            }`}
+          >
+            {item.icon}
+            <FormattedMessage id={item.langKey}>
+              {(txt) => (
+                <Link to={item.url} title={txt} alt={txt}>
                   {txt}
                 </Link>
               )}
             </FormattedMessage>
           </h2>
-          <h2 className={['nav_item', 'medium', type === 'Pets' ? 'active' : ''].join(' ')}>
-            <FormattedMessage id="pets">
-              {txt => (
-                <Link
-                  to="/account/pets"
-                  title={txt}
-                  alt={txt}>
-                  {txt}
-                </Link>
-              )}
-            </FormattedMessage>
-          </h2>
-          <h2 className={['nav_item', 'medium', type === 'Subscription' ? 'active' : ''].join(' ')}>
-            <FormattedMessage id="subscription">
-              {txt => (
-                <Link
-                  to="/account/subscription"
-                  title={txt}
-                  alt={txt}>
-                  {txt}
-                </Link>
-              )}
-            </FormattedMessage>
-          </h2>
-          <h2 className={['nav_item', 'medium', type === 'Orders' ? 'active' : ''].join(' ')}>
-            <FormattedMessage id="orders">
-              {txt => (
-                <Link to="/account/orders" title={txt} alt={txt}>
-                  {txt}
-                </Link>
-              )}
-            </FormattedMessage>
-          </h2>
-          {/* <h2 className={['nav_item', 'medium', type === 'ReturnOrder' ? 'active' : ''].join(' ')}>
-            <FormattedMessage id="order.returnOrder">
-              {txt => (
-                <Link to="/account/return-order" title={txt} alt={txt}>
-                  {txt}
-                </Link>
-              )}
-            </FormattedMessage>
-          </h2> */}
-          <h2 className={['nav_item', 'medium', type === 'ShippingAddress' ? 'active' : ''].join(' ')}>
-            <FormattedMessage id="shippingAddress">
-              {txt => (
-                <Link
-                  to="/account/shippingAddress"
-                  title={txt}
-                  alt={txt}>
-                  {txt}
-                </Link>
-              )}
-            </FormattedMessage>
-          </h2>
-          {this.props.configStore.profilePaymentMethod
-            && <h2 className={['nav_item', 'medium', type === 'PaymentMethod' ? 'active' : ''].join(' ')}>
-              <FormattedMessage id="paymentMethod">
-                {txt => (
-                  <Link
-                    to="/account/paymentMethod"
-                    title={txt}
-                    alt={txt}>
-                    {txt}
-                  </Link>
-                )}
-              </FormattedMessage>
-            </h2>}
-          <h2 className={['nav_item', 'medium', type === 'Help' ? 'active' : ''].join(' ')}>
-            <FormattedMessage id="help">
-              {txt => (
-                <Link to="/help" title={txt} alt={txt}>
-                  {txt}
-                </Link>
-              )}
-            </FormattedMessage>
-          </h2>
-          <hr />
-        </div>
+        ))}
       </div>
-    )
+    );
   }
 }
 
-export default SideMenu
+export default SideMenu;
