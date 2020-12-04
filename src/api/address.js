e509c94c16bed114fd3cb715ecbc4783d26fbd93
minyatang <mytang11@163.com>
2020-12-04 17:27:59 +0800

    添加设置默认卡功能

--- src/api/address.js
+++ src/api/address.js
@@ -1,52 +1,52 @@
-import axios from '@/utils/request'
+import axios from '@/utils/request';
 
 const api = {
   getAddressList: '/customer/addressList',
   addressInfo: '/customer/addressInfo',
-  setDefaltAddress:'/customer/defaultAddress',
-  saveAddress:'/customer/address'
-}
+  setDefaltAddress: '/customer/defaultAddress',
+  saveAddress: '/customer/address'
+};
 
-export default api
+export default api;
 
-export function getAddressList (parameter) {
+export function getAddressList(parameter) {
   return axios({
     url: `${api.getAddressList}`,
     method: 'get',
     data: parameter
-  })
+  });
 }
-export function getAddressById (parameter) {
+export function getAddressById(parameter) {
   return axios({
     url: `${api.saveAddress}/${parameter.id}`,
     method: 'get'
-  })
+  });
 }
-export function saveAddress (parameter) {
+export function saveAddress(parameter) {
   return axios({
     url: `${api.saveAddress}`,
     method: 'post',
     data: parameter
-  })
+  });
 }
 
-export function setDefaltAddress (parameter) {
+export function setDefaltAddress(parameter) {
   return axios({
     url: `${api.setDefaltAddress}/${parameter.deliveryAddressId}`,
     method: 'post',
     data: parameter
-  })
+  });
 }
-export function deleteAddress (parameter) {
+export function deleteAddress(parameter) {
   return axios({
     url: `${api.addressInfo}/${parameter.id}`,
     method: 'delete'
-  })
+  });
 }
-export function editAddress (parameter) {
+export function editAddress(parameter) {
   return axios({
     url: `${api.addressInfo}`,
     method: 'put',
     data: parameter
-  })
+  });
 }
--- src/api/payment.js
+++ src/api/payment.js
@@ -1,4 +1,4 @@
-import axios from '@/utils/request'
+import axios from '@/utils/request';
 
 const api = {
   visitorRegisterAndLogin: '/visitorRegisterAndLogin',
@@ -7,120 +7,127 @@ const api = {
   addOrUpdatePaymentMethod: '/payment-method/updata',
   getPaymentMethod: '/payment-method/query-by-customer-id',
   deleteCard: '/payment-method/delete-by-id',
+  setDefaltCard: '/payment-method/default',
   // confirmAndCommit: '/tradeCustom/confirmcommitAndPaySync'
 
   customerCommitAndPay: '/tradeCustom/customerCommitAndPay',
   rePay: '/tradeCustom/rePay',
   customerCommitAndPayMix: '/tradeCustom/customerCommitAndPayMix',
   getMarketingDiscount: '/marketing/getMarketingDiscount',
-  getWays:'/PayGateway/gateways',
-  adyenPaymentsDetails:'/adyenPay/payments/details',
+  getWays: '/PayGateway/gateways',
+  adyenPaymentsDetails: '/adyenPay/payments/details',
   getProductPetConfig: '/order/config/findPet'
-}
+};
 
-export default api
+export default api;
 
-export function postVisitorRegisterAndLogin (parameter) {
+export function postVisitorRegisterAndLogin(parameter) {
   return axios({
     url: api.visitorRegisterAndLogin,
     method: 'post',
     data: parameter
-  })
+  });
 }
 
-export function batchAdd (parameter) {
+export function batchAdd(parameter) {
   return axios({
     url: api.batchAdd,
     method: 'post',
     data: parameter
-  })
+  });
 }
 
-export function confirmAndCommit (parameter) {
+export function confirmAndCommit(parameter) {
   return axios({
     url: api.confirmAndCommit,
     method: 'post',
     data: parameter
-  })
+  });
 }
 
-export function addOrUpdatePaymentMethod (parameter) {
+export function addOrUpdatePaymentMethod(parameter) {
   return axios({
     url: api.addOrUpdatePaymentMethod,
     method: 'post',
     data: parameter
-  })
+  });
 }
 
-export function getPaymentMethod (parameter) {
+export function getPaymentMethod(parameter) {
   return axios({
     url: api.getPaymentMethod,
     method: 'post',
     data: parameter
-  })
+  });
 }
 
-export function deleteCard (para) {
+export function deleteCard(para) {
   return axios({
     url: api.deleteCard,
     method: 'post',
     data: para
-  })
+  });
 }
 
-export function customerCommitAndPay (parameter) {
+export function customerCommitAndPay(parameter) {
   return axios({
     url: api.customerCommitAndPay,
     method: 'post',
     data: parameter
-  })
+  });
 }
 
-export function customerCommitAndPayMix (parameter) {
+export function customerCommitAndPayMix(parameter) {
   return axios({
     url: api.customerCommitAndPayMix,
     method: 'post',
     data: parameter
-  })
+  });
 }
 
-export function rePay (parameter) {
+export function rePay(parameter) {
   return axios({
     url: api.rePay,
     method: 'post',
     data: parameter
-  })
+  });
 }
 
-export function getMarketingDiscount (parameter) {
+export function getMarketingDiscount(parameter) {
   return axios({
     url: api.getMarketingDiscount,
     method: 'post',
     data: parameter
-  })
+  });
 }
 
-export function getWays (parameter) {
+export function getWays(parameter) {
   return axios({
     url: api.getWays,
     method: 'get',
     data: parameter
-  })
+  });
 }
 
-
-export function adyenPaymentsDetails (parameter) {
+export function adyenPaymentsDetails(parameter) {
   return axios({
     url: api.adyenPaymentsDetails,
     method: 'post',
     data: parameter
-  })
+  });
 }
 
-export function getProductPetConfig (parameter) {
+export function getProductPetConfig(parameter) {
   return axios({
     url: api.getProductPetConfig,
     method: 'post',
     data: parameter
-  })
+  });
+}
+export function setDefaltCard(parameter) {
+  return axios({
+    url: `${api.setDefaltCard}/${parameter}`,
+    method: 'post',
+    data: parameter
+  });
 }
