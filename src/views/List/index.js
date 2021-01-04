cb181397f08ca7fc130ba69f39c82f9f5066ba1f
knifecome <qhx717@qq.com>
2021-01-04 18:43:35 +0800

    update

--- src/views/List/index.js
+++ src/views/List/index.js
@@ -566,15 +566,12 @@ class List extends React.Component {
           actionField: { list: this.state.GAListParam }, //?list's name where the product was clicked from (Catalogue, Homepage, Search Results)
           products: [
             {
-              name: item.goodsName, //?
+              name: item.goodsName,
               id: item.goodsNo,
               club: 'no',
               brand: item.goodsBrand.brandName,
-              // category: item.goodsCateName
-              //   ? JSON.parse(item.goodsCateName)[0]
-              //   : '',
               category: item.goodsCateName,
-              list: this.state.GAListParam, //?list's name where the product was clicked from (Catalogue, Homepage, Search Results)
+              list: this.state.GAListParam,
               position: index,
               sku: item.goodsInfos.length && item.goodsInfos[0].goodsInfoNo
             }
@@ -588,19 +585,15 @@ class List extends React.Component {
   GAProductImpression(productList, totalElements, keywords) {
     const impressions = productList.map((item, index) => {
       return {
-        name: item.goodsName, //
+        name: item.goodsName,
         id: item.goodsNo,
         brand: item.goodsBrand.brandName,
         price: item.minMarketPrice,
         club: 'no',
-        //category: !!item.goodsCateName ? JSON.parse(item.goodsCateName)[0] : '',
         category: item.goodsCateName,
-        list: this.state.GAListParam, //list's name where the product was clicked from (Catalogue, Homepage, Search Results)
+        list: this.state.GAListParam,
         position: index,
         sku: item.goodsInfos.length && item.goodsInfos[0].goodsInfoNo,
-        // flag: !!item.taggingForImage
-        //   ? JSON.parse(item.taggingForImage).taggingName
-        //   : ''
         flag: ''
       };
     });
