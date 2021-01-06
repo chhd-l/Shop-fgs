3c2062261d391ad8c33d2cf93eabe4a2b4f1dc63
knifecome <qhx717@qq.com>
2021-01-06 17:37:15 +0800

    update

--- src/views/FAQ/index.js
+++ src/views/FAQ/index.js
@@ -38,7 +38,7 @@ class FAQ extends React.Component {
     setSeoConfig({
       pageName: 'FAQ page'
     }).then(res => {
-      this.setState({seoConfig: res})
+      this.setState({ seoConfig: res })
     });
     // if (localItemRoyal.get('isRefresh')) {
     //   localItemRoyal.remove('isRefresh');
@@ -109,8 +109,8 @@ class FAQ extends React.Component {
         <GoogleTagManager additionalEvents={event} />
         <Helmet>
           <title>{this.state.seoConfig.title}</title>
-          <meta name="description" content={this.state.seoConfig.metaDescription}/>
-          <meta name="keywords" content={this.state.seoConfig.metaKeywords}/>
+          <meta name="description" content={this.state.seoConfig.metaDescription} />
+          <meta name="keywords" content={this.state.seoConfig.metaKeywords} />
         </Helmet>
         <Header
           showMiniIcons={true}
@@ -158,68 +158,117 @@ class FAQ extends React.Component {
                 </div>
               </div>
             </div>
-            {this.state.loading ? (
+            {/* {this.state.loading ? (
               <div className="pb-4">
                 <Skeleton color="#f5f5f5" width="100%" height="50%" count={5} />
               </div>
             ) : (
-              this.state.dataFAQ.map((pitem, index) => (
-                <>
-                  <div className="rc-bg-colour--brand3" key={'p-' + index}>
-                    <h2
-                      name={`catogery-${index}`}
-                      id={`catogery-${index}`}
-                      className="text-center"
-                    >
-                      {pitem.faqType}
-                    </h2>
-                  </div>
-                  <dl
-                    data-toggle-group=""
-                    data-toggle-effect="rc-expand--vertical"
-                    className="rc-max-width--xl rc-padding-x--sm rc-padding-x--xl--mobile rc-margin-y--sm rc-margin-y--lg--mobile"
-                  >
-                    {pitem.storeFaqVo.map((item, index) => (
-                      <div
-                        key={item.id}
-                        className={`rc-list__accordion-item test-color 
-                  ${this.state.showCur === index ? 'showItem' : 'hiddenItem'}`}
+                this.state.dataFAQ.map((pitem, index) => (
+                  <>
+                    <div className="rc-bg-colour--brand3" key={'p-' + index}>
+                      <h2
+                        name={`catogery-${index}`}
+                        id={`catogery-${index}`}
+                        className="text-center"
                       >
+                        {pitem.faqType}
+                      </h2>
+                    </div>
+                    <dl
+                      data-toggle-group=""
+                      data-toggle-effect="rc-expand--vertical"
+                      className="rc-max-width--xl rc-padding-x--sm rc-padding-x--xl--mobile rc-margin-y--sm rc-margin-y--lg--mobile"
+                    >
+                      {pitem.storeFaqVo.map((item, index2) => (
                         <div
-                          className="rc-list__header"
-                          onClick={() => this.handleSelect(index)}
-                          style={{
-                            display: 'flex',
-                            justifyContent: 'space-between'
-                          }}
+                          key={item.id}
+                          className={`rc-list__accordion-item test-color 
+                  ${this.state.showCur === index2 ? 'showItem' : 'hiddenItem'}`}
                         >
                           <div
-                            dangerouslySetInnerHTML={{ __html: item.question }}
-                          ></div>
+                            className="rc-list__header"
+                            onClick={() => this.handleSelect(index2)}
+                            style={{
+                              display: 'flex',
+                              justifyContent: 'space-between'
+                            }}
+                          >
+                            <div
+                              dangerouslySetInnerHTML={{ __html: item.question }}
+                            ></div>
 
-                          <span
-                            className={`icon-change ${
-                              this.state.showCur === index
+                            <span
+                              className={`icon-change ${this.state.showCur === index2
                                 ? 'rc-icon rc-up rc-brand1'
                                 : 'rc-icon rc-down rc-iconography'
-                            }`}
-                          ></span>
+                                }`}
+                            ></span>
+                          </div>
+                          <div className={`rc-list__content `}>
+                            <p
+                              dangerouslySetInnerHTML={{ __html: item.answer }}
+                            ></p>
+                            <LazyLoad>
+                              <img src={item.imgUl} alt="" />
+                            </LazyLoad>
+                          </div>
                         </div>
-                        <div className={`rc-list__content `}>
-                          <p
-                            dangerouslySetInnerHTML={{ __html: item.answer }}
-                          ></p>
-                          <LazyLoad>
-                            <img src={item.imgUl} alt="" />
-                          </LazyLoad>
-                        </div>
-                      </div>
-                    ))}
-                  </dl>
-                </>
-              ))
-            )}
+                      ))}
+                    </dl>
+                  </>
+                ))
+              )} */}
           </div>
+          {
+            this.state.loading
+              ? (
+                <div className="rc-bg-colour--brand3 rc-bottom-spacing data-checkout-stage rc-max-width--lg">
+                    <div className="pb-4">
+                    <Skeleton color="#f5f5f5" width="100%" height="50%" count={5} />
+                  </div>
+                </div>
+              ) : (
+                <div className="rc-bg-colour--brand3 rc-bottom-spacing data-checkout-stage rc-max-width--lg" style={{marginBottom: '100px'}}>
+                  {
+                    this.state.dataFAQ.map((pitem,index) => {
+                      return (
+                        <div id={`catogery-${index}`}>
+                          <div className="rc-bg-colour--brand3">
+                            <div className="rc-padding--sm rc-padding-left--none">
+                              <div className="rc-padding-y--md rc-md-down"></div>
+                              <div className="rc-one-column">
+                                <div className="rc-column rc-padding-left--none">
+                                  <div className="rc-full-width rc-text--left rc-padding-x--sm rc- padding-left--none">
+                                    <h1 style={{ textAlign: "center" }}>{pitem.faqType}</h1>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          {
+                            pitem.storeFaqVo.map((item, idx) => {
+                              return (
+                                <dl data-toggle-group data-toggle-effect="rc-expand--vertical" style={{marginBottom:'10px'}}>
+                                  <div className="rc-list__accordion-item" style={{borderBottom:0}}>
+                                    <dt>
+                                      <button className="rc-list__header FAQ_header" id={`heading-${item.id}`} data-toggle={`content-${item.id}`} data-js-open="false" data-depth="1" aria-haspopup="true" aria-selected="false" dangerouslySetInnerHTML={{ __html: item.question }}></button>
+                                    </dt>
+                                    <dd className="rc-list__content rc-expand--vertical" id={`content-${item.id}`} aria-labelledby={`heading-${item.id}`} aria-expanded="false" aria-hidden="true" style={{ maxHeight: 0 }}>
+                                      <p dangerouslySetInnerHTML={{ __html: item.answer }}></p>
+                                    </dd>
+                                  </div>
+                                </dl>
+                              )
+                            })
+                          }
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+              )
+          }
+
         </main>
         <Footer />
       </div>
