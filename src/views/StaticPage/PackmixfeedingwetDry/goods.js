014ed839ae1bcd86cfa66095eb32cdcd3a240514
knifecome <qhx717@qq.com>
2021-01-07 16:37:26 +0800

    update

---
+++ src/views/StaticPage/PackmixfeedingwetDry/goods.js
@@ -0,0 +1,63 @@
+const list1 = [
+    {
+        imageUrl: 'https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-royal_canin_catalog_fr/default/dw9e6ef833/mkt/00001/Main-Coon-Adult-1-bis.jpg?sw=150&sfrm=jpg',
+        linkUrl:'/pack-maine-coon-adulte-MKT00001',
+        title: 'Pack Maine Coon Adulte',
+        price: '62.98',
+        marketPrice: '',
+        subTitle: 'À partir de 15 mois',
+    },
+    {
+        imageUrl: 'https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-royal_canin_catalog_fr/default/dw17f78f13/mkt/10001/Persan-1-bis.jpg?sw=150&sfrm=jpg',
+        linkUrl:'/pack-persan-adulte-MKT10001',
+        title: 'Pack Persan Adulte',
+        price: '64.98',
+        marketPrice: '',
+        subTitle: 'À partir de 12 mois',
+    },
+    {
+        imageUrl: 'https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-royal_canin_catalog_fr/default/dwdffc9720/mkt/20001/British-Shortair1-bis.jpg?sw=150&sfrm=jpg',
+        linkUrl:'/pack-british-shorthair-adulte-MKT20001',
+        title: 'Pack British Shorthair Adulte',
+        price: '62.98',
+        marketPrice: '',
+        subTitle: 'À partir de 15 mois',
+    },
+    {
+        imageUrl: 'https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-royal_canin_catalog_fr/default/dw1dd60c89/mkt/30001/Appetite-control-1.jpg?sw=150&sfrm=jpg',
+        linkUrl:'/pack-appetite-control-care-MKT30001',
+        title: 'Pack Appetite Control Care',
+        price: '58.98',
+        marketPrice: '',
+        subTitle: 'Chats adultes stérilisés de 1 à 7 ans – Tendance à quémander',
+    }
+]
+
+const list2 = [
+    {
+        imageUrl: 'https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-royal_canin_catalog_fr/default/dwe8f17201/mkt/40001/Mini-Puppy-1-bis.jpg?sw=150&sfrm=jpg',
+        linkUrl:'/pack-mini-puppy-MKT40001',
+        title: 'Pack Mini Puppy',
+        price: '41.98',
+        marketPrice: '',
+        subTitle: 'Chiots jusqu’à 10 mois',
+    },
+    {
+        imageUrl: 'https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-royal_canin_catalog_fr/default/dw454e4e2f/mkt/50001/Mini-Adult-1-bis.jpg?sw=150&sfrm=jpg',
+        linkUrl:'/pack-mini-adult-MKT50001',
+        title: 'Pack Mini Adult',
+        price: '37.48',
+        marketPrice: '',
+        subTitle: 'À partir de 10 mois',
+    },
+    {
+        imageUrl: 'https://shop.royalcanin.fr/dw/image/v2/BCMK_PRD/on/demandware.static/-/Sites-royal_canin_catalog_fr/default/dw95e71446/7gkua2qbg95xk-rbk7sg/v66/nutritional-supplement-educ-packshots-pro-educ-as-packshot.jpg?sw=150&sfrm=png',
+        linkUrl:'/educ-3100',
+        title: 'Educ',
+        price: '36.99',
+        marketPrice: '',
+        subTitle: 'Supplément nutritionnel chiot (à partir de 2 mois) et chien adulte',
+    }
+]
+
+export {list1,list2}
