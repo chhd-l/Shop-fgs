9ef71f7c5dfa2f9ba64c20ff636c46571b2a2fe2
lizongji <kensyang@deloitte.com.cn>
2021-01-08 16:19:52 +0800

    update

--- src/views/StaticPage/Help/fr/frFaq.js
+++ src/views/StaticPage/Help/fr/frFaq.js
@@ -50,7 +50,8 @@ class FrFaq extends Component {
                                 <div className="rc-border-all rc-border-colour--interface fullHeight">
                                     <div className="rc-layout-container rc-three-column rc-margin--none rc-content-h-middle rc-reverse-layout-mobile fullHeight">
                                         <div className="rc-column rc-double-width rc-padding-top--md--mobile">
-                                            <p>Vous pouvez également consulter notre rubrique
+                                            <p>
+                                                {`Vous pouvez également consulter notre rubrique `}
                                                 <a style={{textDecoration:'underline',color:'rgb(236,0,26)',backgroundColor:'rgb(255,255,255)',padding:'0 3px'}} href={`${process.env.REACT_APP_ACCESS_PATH}FAQ/all`}>FAQ</a> 
                                                 qui vous apportera de nombreuses réponses. 
                                             </p>
