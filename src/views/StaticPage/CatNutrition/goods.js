6016b00e9af70cd6786afcb1a9b1dccce9a29d57
lizongji <kensyang@deloitte.com.cn>
2021-01-11 11:44:53 +0800

    update

---
+++ src/views/StaticPage/CatNutrition/goods.js
@@ -0,0 +1,38 @@
+
+const list1 = [
+    {
+      imageUrl: `${process.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/16-sterilised-37-b1-ne.jpg`,
+      linkUrl: '/sterilised-37-2537_FR',
+      title: 'Sterilised 37',
+      price: '80.99',
+      marketPrice: '24.99',
+      subTitle: 'Chat adulte stérilisé'
+    },
+    {
+      imageUrl: `${process.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/16-indoor-27-b1-ne.jpg`,
+      linkUrl: '/indoor-27-2529_FR',
+      title: 'Indoor 27',
+      price: '78.99',
+      marketPrice: '26.99',
+      subTitle:
+        "Chat d’intérieur adulte"
+    },
+    {
+      imageUrl: `${process.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/fcn20-appetite-control-packshot-b1-ne-med-res-basic-389826.jpg`,
+      linkUrl: '/appetite-control-care-2563_FR',
+      title: 'Appetite Control Care',
+      price: '79.99',
+      marketPrice: '27.99',
+      subTitle:
+        "Chats adultes stérilisés de 1 à 7 ans - Tendance à quémander"
+    },
+    {
+      imageUrl: `${process.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/16-appetite-control-sterilised-7-b1-in.jpg`,
+      linkUrl: '/appetite-control-sterilised--7--2564_FR',
+      title: 'Appetite Control Sterilised 7+ ',
+      price: '40.99',
+      marketPrice: '',
+      subTitle: 'Chat stérilisé de plus de 7 ans - Tendance à quémander'
+    }
+  ];
+  export {list1}
