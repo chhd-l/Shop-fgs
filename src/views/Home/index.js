ae71a0e4e6d9b512aa929981929d43c24dc37ec1
minyatang <mytang11@163.com>
2021-01-18 21:35:41 +0800

    related products返回多条数据，只显示了一个

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -6,7 +6,6 @@ import BannerTip from '@/components/BannerTip';
 import GoogleTagManager from '@/components/GoogleTagManager';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import HeroCarousel from '@/components/HeroCarousel3';
 import HeroCarouselMobile from '@/components/HeroCarouselMobile2';
 import FooterImage from './modules/FooterImage';
 import { Ads } from './ad';
