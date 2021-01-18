cba9615ecbb8adc85ff3db6002d710afb1851e05
knifecome <qhx717@qq.com>
2021-01-18 21:31:51 +0800

    update

--- src/views/Payment/modules/Adyen3DSFail.js
+++ src/views/Payment/modules/Adyen3DSFail.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { injectIntl } from 'react-intl';
 import { inject, observer } from 'mobx-react';
 import url from 'url'
+const sessionItemRoyal = window.__.sessionItemRoyal;
 
 @inject('paymentStore')
 @observer
@@ -17,6 +18,13 @@ class Adyen3DSResult extends Component {
     try {
       const param = url.parse(this.props.location.search)
       console.log(param)
+      // sessionItemRoyal.set('rc-tid', 111);
+      // sessionItemRoyal.set('rc-rePaySubscribeId', 222);
+      // sessionItemRoyal.set(
+      //     'rc-tidList',
+      //     JSON.stringify(err.errorData.tidList)
+      //   );
+      //this.props.history.push('/checkout');
     } catch (err) {
       console.log(err);
     }
