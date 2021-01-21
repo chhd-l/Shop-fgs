8857120d2059351cbc0f1bae7191c66fb4528600
knifecome <qhx717@qq.com>
2021-01-21 16:10:55 +0800

    update

--- src/views/Payment/index.js
+++ src/views/Payment/index.js
@@ -179,6 +179,7 @@ class Payment extends React.Component {
       if (this.isLogin) {
         // 登录情况下，无需显示email panel
         paymentStore.setStsToCompleted({ key: 'email', isFirstLoad: true });
+        console.log('################tid###############',tid)
         if (tid) {
           paymentStore.setStsToCompleted({
             key: 'deliveryAddr',
@@ -544,6 +545,7 @@ class Payment extends React.Component {
     }
   };
   queryOrderDetails() {
+    console.log("$$$$tidList$$$$$$$",this.state.tidList[0])
     getOrderDetails(this.state.tidList[0]).then(async (res) => {
       let resContext = res.context;
       let cityRes = await queryCityNameById({
