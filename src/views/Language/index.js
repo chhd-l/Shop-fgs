5d44ccf12844925dc2b5e5d363d3af989a4a32d9
knifecome <qhx717@qq.com>
2021-02-03 09:57:31 +0800

    update

--- src/views/Language/index.js
+++ src/views/Language/index.js
@@ -1,43 +1,46 @@
 import React, { Component } from "react"
 import axios from 'axios';
+import Loading from '@/components/Loading';
 import "./css/index.less"
 
 export default class LanguagePage extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            allData:[]
+            allData: [],
+            loading:true
         }
     }
-    placeCurrentCountryToFirst(){
+    placeCurrentCountryToFirst() {
         let newAllData = [...this.state.allData]
-        let currentCountryData = newAllData.find(element=>{
+        let currentCountryData = newAllData.find(element => {
             return element.IsCurrent == true
         })
-        let currentCountryDataIndex = newAllData.findIndex(element=>{
+        let currentCountryDataIndex = newAllData.findIndex(element => {
             return element.IsCurrent == true
         })
 
-        newAllData.splice(currentCountryDataIndex,1)
+        newAllData.splice(currentCountryDataIndex, 1)
         newAllData.unshift(currentCountryData)
         this.setState({
             allData: newAllData
         })
-}
-    getAllData(){
-        axios.get('/languagepicker/getcountries')
-        .then((response)=>{
-            this.setState({allData:response.data},()=>{
-                this.placeCurrentCountryToFirst()
-            })
-        });
     }
-    selectCountry(){
+    getAllData() {
+        axios.get('/languagepicker/getcountries')
+            .then((response) => {
+                this.setState({ allData: response.data }, () => {
+                    this.placeCurrentCountryToFirst()
+                })
+            });
+    }
+    selectCountry() {
         alert(123)
     }
     render() {
         return (
             <div className="languagePage">
+                {this.state.loading ? <Loading bgColor={'#fff'} /> : null}
                 <aside className="language-picker-modal rc-modal rc-modal--full" data-modal-target="country-lang-selector" data-rc-feature- modal-setup="true" data-rc-modal-active="true" >
                     <div className="rc-modal__container" data-control="filter-panel">
                         <header className="rc-modal__header">
@@ -47,17 +50,17 @@ export default class LanguagePage extends Component {
                         </header>
                         <section className="rc-modal__content rc-max-width--xl">
                             <div data-component="languagepicker">
-                                <div className="rc-alpha rc-modal__title rc-text--center">
+                                <div className="rc-alpha rc-modal__title rc-text--center" id="title">
                                     Seleccioná tu ubicación
-                            </div>
+                                </div>
                                 <div className="modal-select">
                                     <span className="rc-select rc-input--label rc-margin-bottom--md--mobile rc-margin-bottom--sm--desktop rc-select-processed">
                                         <label className="rc-select__label" htmlFor="id-country-select">Country</label>
                                         <select data-js-select="" id="id-country-select">
                                             {
-                                                this.state.allData.map((item,index)=>{
+                                                this.state.allData.map((item, index) => {
                                                     return (
-                                                        <option disabled={item.IsCurrent} selected={item.IsCurrent} value={item.UrlCode} key={`country-${index}`} onSelect={this.selectCountry}>{item.Name}</option>
+                                                        <option disabled={item.IsCurrent} selected={item.IsCurrent} value={item.Name} key={`country-${index}`}>{item.Name}</option>
                                                     )
                                                 })
                                             }
@@ -70,11 +73,11 @@ export default class LanguagePage extends Component {
                                         <label className="rc-select__label" htmlFor="id-language-select">Language</label>
                                         <select data-js-select="" id="id-language-select">
                                             {
-                                                this.state.allData.filter((item)=>{
+                                                this.state.allData.filter((item) => {
                                                     return item.IsCurrent == true
-                                                }).map((item2,index2)=>{
+                                                }).map((item2, index2) => {
                                                     return (
-                                                        <option disabled={item2.IsCurrent} selected={item2.IsCurrent} value={item2.UrlCode} key={`lang-${index2}`}>{item2.Name}</option>
+                                                        <option disabled={item2.IsCurrent} selected={item2.IsCurrent} value={item2.Name} key={`lang-${index2}`}>{item2.Name}</option>
                                                     )
                                                 })
                                             }
@@ -95,7 +98,22 @@ export default class LanguagePage extends Component {
 
         )
     }
-    componentDidMount(){
-       this.getAllData()
+    componentDidMount() {
+        this.getAllData()
+
+        window.onload = ()=> {
+            this.setState({loading:false})
+            const choiceDomArr = document.querySelectorAll('.choices__item')
+            for (let i=0;i<choiceDomArr.length;i++){
+                console.log(choiceDomArr[i])
+                choiceDomArr[i].onclick = function(e){
+                    e.stopPropagation()
+                    e.preventDefault()
+                    alert(i)
+                    console.log(123)
+                }
+            }
+        }
+
     }
 }
