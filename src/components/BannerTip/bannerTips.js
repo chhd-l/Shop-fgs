ae70f2db37761eee6f7c4c3b6efea449713f1945
knifecome <qhx717@qq.com>
2021-02-23 11:00:45 +0800

    update banner

--- src/components/BannerTip/bannerTips.js
+++ src/components/BannerTip/bannerTips.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 import { LinkJSX } from './link';
+import './index.less';
 
 export const bannerTips = () => {
   return (
     <div
       // id="bannerTip"
-      className="red font-weight-normal p-1 position-relative text-center pl-2 pr-2 pr-md-4 pl-md-4 rc-bg-colour--brand4"
+      className="red font-weight-normal p-1 position-relative text-center pl-2 pr-2 pr-md-4 pl-md-4 rc-bg-colour--brand4 rc-bannertip-containner"
     >
       {process.env.REACT_APP_IS_PROMOTION === 'true' && (
         <div className="rc-bg-colour--brand4 text-center">
@@ -15,7 +16,7 @@ export const bannerTips = () => {
             <div className="rc-column rc-content-v-middle rc-zeta rc-margin--none rc-padding--xs">
               <span className="rc-icon rc-refresh rc-brand1 rc-iconography" />
               <div className="d-flex align-items-center">
-                <span className="rc-margin-right--xs rc-margin-left--xs">
+                <span className="rc-margin-right--xs rc-margin-left--xs rc-bannertip-text">
                   <FormattedMessage id="home.promotionTip" />
                 </span>
                 <div>
