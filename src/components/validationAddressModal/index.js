8caf772696c75cc7cdbe2479df371b7375c72ff2
ZengKun <kensyang@deloitte.com.cn>
2021-02-26 15:40:09 +0800

    add components/validationAddressModal

---
+++ src/components/validationAddressModal/index.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import './index.css'
+
+class ValidationAddressModal extends React.Component {
+  static defaultProps = {
+    modalTitle: <FormattedMessage id="information" />,
+    modalVisible: false,
+    address: [],
+    validationAddress: [],
+    selectValidationOption: 'suggestedAddress'
+  };
+  close() {
+    this.props.close();
+  }
+  hanldeClickConfirm() {
+    this.props.hanldeClickConfirm();
+  }
+  handleChooseValidationAddress(e) {
+    this.props.handleChooseValidationAddress(e);
+  }
+  render() {
+    const { address, validationAddress, modalVisible, selectValidationOption } = this.props;
+    return (
+      <div className={`modal validation_modal fade ${modalVisible == true ? 'show' : ''}`} id="shippingAddressValidationModal" tabindex="-1" role="dialog" aria-labelledby="shippingAddressValidationModal" aria-modal="true" style={{ paddingRight: '16px' }}>
+        <div className="modal-dialog" role="document" style={{ transform: 'none' }}>
+          <div className="modal-content rc-padding-x--lg rc-padding-y--sm rc-padding-y--lg--mobile" style={{ marginTop: '0' }}>
+            <div className="modal-header text-center rc-padding-bottom--xs rc-padding-top--sm rc-padding-y--lg--mobile rc-margin-top--lg--mobile">
+              <h4 className="modal-title sav-modal-title rc-full-width rc-gamma rc-margin-bottom--none">Verify your address</h4>
+            </div>
+            <div className="modal-body rc-padding-top--xs--mobile">
+              <div className="text-center rc-margin-bottom--sm rc-meta">
+                <span className="d-none d-md-block">
+                  We could not verify the address you provided
+                  <br />
+                  Please confirm or edit your address to ensure prompt delivery.
+                </span>
+                <span className="d-block d-md-none">We could not verify the address you providedPlease confirm or edit your address to ensure prompt delivery.</span>
+              </div>
+              <div className="d-flex rc-margin-bottom--sm rc-padding-top--xs row addressSuggestionModal">
+                {/* originalAddress */}
+                <div className={`originalAddress col-12 col-md-6 rc-column rc-padding-top--sm--mobile ${selectValidationOption == 'originalAddress' ? 'addressBorder' : ''}`}>
+                  <div className="rc-input rc-padding-left--xs rc-margin-top--sm--mobile text-break" data-rc-feature-forms-setup="true">
+                    <input type="radio" className="rc-input__radio" name="addressChoice" id="originalAddress" value="originalAddress" defaultChecked={selectValidationOption == 'originalAddress' ? true : false} onChange={(e) => this.handleChooseValidationAddress(e)} />
+                    <label className="rc-input__label--inline rc-margin-bottom--none text-left" for="originalAddress">
+                      <b>Original Address</b>
+                      <br />
+                      <span className="name"></span>
+                      <span className="address-one">{address.address1},</span>
+                      <span className="address-two">{address.address2}</span>
+                      <span className="city">{address.city},</span>
+                      <span className="state">{address.provinceName},</span>
+                      <span className="postalCode">{address.postCode},</span>
+                      <span className="countryCode">{address.countryName}</span>
+                      <br />
+                      <a className="styled-link" data-dismiss="modal" onClick={() => this.close()}><b>Edit</b></a>
+                    </label>
+                  </div>
+                </div>
+                {/* suggestedAddress */}
+                <div className={`suggestedAddress col-12 col-md-6 rc-column rc-padding-top--sm--mobile rc-margin-top--sm--mobile ${selectValidationOption == 'suggestedAddress' ? 'addressBorder' : ''}`}>
+                  <div className="rc-input rc-margin-top--sm--mobile text-break" data-rc-feature-forms-setup="true">
+                    <input type="radio" className="rc-input__radio" name="addressChoice" id="suggestedAddress" value="suggestedAddress" defaultChecked={selectValidationOption == 'suggestedAddress' ? true : false} onChange={(e) => this.handleChooseValidationAddress(e)} />
+                    <label className="rc-input__label--inline rc-margin-bottom--none text-left" for="suggestedAddress">
+                      <b>Suggested Address</b>
+                      <br />
+                      <span className="name"></span>
+                      <span className="address-one">{validationAddress.address1},</span>
+                      <span className="address-two">{validationAddress.address2}</span>
+                      <span className="city">{validationAddress.city},</span>
+                      <span className="state">{validationAddress.provinceCode},</span>
+                      <span className="postalCode">{validationAddress.postalCode},</span>
+                      <span className="countryCode">{validationAddress.countryCode}</span>
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div className="sav-buttons-div text-center rc-padding-top--lg--mobile rc-padding-top--sm">
+                <button className="rc-btn rc-btn--one confirmAddress" onClick={(e) => this.hanldeClickConfirm(e)}>Confirm</button>
+              </div>
+              <input type="hidden" name="addressClassification" id="addressClassification" />
+              <input type="hidden" name="fedexValidated" id="fedexValidated" />
+            </div>
+          </div>
+        </div>
+      </div >
+    );
+  }
+}
+export default ValidationAddressModal
--- src/views/Payment/Address/VisitorAddress.js
+++ src/views/Payment/Address/VisitorAddress.js
@@ -3,6 +3,7 @@ import { FormattedMessage, injectIntl } from 'react-intl';
 import { inject, observer } from 'mobx-react';
 import { toJS } from 'mobx';
 import Loading from '@/components/Loading';
+import ValidationAddressModal from '@/components/validationAddressModal';
 import EditForm from './EditForm';
 import { ADDRESS_RULE } from '@/utils/constant';
 import { validData } from '@/utils/utils';
@@ -44,7 +45,7 @@ class VisitorAddress extends React.Component {
         provinceCode: null
       },
       billingChecked: true,
-      modalFlag: false,
+      modalVisible: false,
       selectValidationOption: 'suggestedAddress'
     };
     this.confirmValidationAddress = this.confirmValidationAddress.bind(this);
@@ -167,7 +168,7 @@ class VisitorAddress extends React.Component {
         });
       }
       this.setState({
-        modalFlag: true
+        modalVisible: true
       });
     } catch (err) {
       console.log(err);
@@ -194,7 +195,7 @@ class VisitorAddress extends React.Component {
     }
 
     this.setState({
-      modalFlag: false
+      modalVisible: false
     });
 
     this.props.updateData(form);
@@ -217,82 +218,11 @@ class VisitorAddress extends React.Component {
       });
     }
   }
-  modalJSX = () => {
-    const { form, validationAddress, modalFlag, selectValidationOption } = this.state;
-    return (
-      <div className={`modal validation_modal fade ${modalFlag == true ? 'show' : ''}`} id="shippingAddressValidationModal" tabindex="-1" role="dialog" aria-labelledby="shippingAddressValidationModal" aria-modal="true" style={{ paddingRight: '16px' }}>
-        <div className="modal-dialog" role="document" style={{ transform: 'none' }}>
-          <div className="modal-content rc-padding-x--lg rc-padding-y--sm rc-padding-y--lg--mobile" style={{ marginTop: '0' }}>
-            <div className="modal-header text-center rc-padding-bottom--xs rc-padding-top--sm rc-padding-y--lg--mobile rc-margin-top--lg--mobile">
-              <h4 className="modal-title sav-modal-title rc-full-width rc-gamma rc-margin-bottom--none">Verify your address</h4>
-            </div>
-            <div className="modal-body rc-padding-top--xs--mobile">
-              <div className="text-center rc-margin-bottom--sm rc-meta">
-                <span className="d-none d-md-block">
-                  We could not verify the address you provided
-                  <br />
-                  Please confirm or edit your address to ensure prompt delivery.
-                </span>
-                <span className="d-block d-md-none">We could not verify the address you providedPlease confirm or edit your address to ensure prompt delivery.</span>
-              </div>
-              <div className="d-flex rc-margin-bottom--sm rc-padding-top--xs row addressSuggestionModal">
-                {/* originalAddress */}
-                <div className={`originalAddress col-12 col-md-6 rc-column rc-padding-top--sm--mobile ${selectValidationOption == 'originalAddress' ? 'addressBorder' : ''}`}>
-                  <div className="rc-input rc-padding-left--xs rc-margin-top--sm--mobile text-break" data-rc-feature-forms-setup="true">
-                    <input type="radio" className="rc-input__radio" name="addressChoice" id="originalAddress" value="originalAddress" defaultChecked={selectValidationOption == 'originalAddress' ? true : false} onChange={(e) => this.chooseValidationAddress(e)} />
-                    <label className="rc-input__label--inline rc-margin-bottom--none text-left" for="originalAddress">
-                      <b>Original Address</b>
-                      <br />
-                      <span className="name"></span>
-                      <span className="address-one">{form.address1},</span>
-                      <span className="address-two">{form.address2}</span>
-                      <span className="city">{form.city},</span>
-                      <span className="state">{form.provinceName},</span>
-                      <span className="postalCode">{form.postCode},</span>
-                      <span className="countryCode">{form.countryName}</span>
-                      <br />
-                      <a className="styled-link" data-dismiss="modal" onClick={() => {
-                        this.setState({
-                          modalFlag: false
-                        });
-                      }}><b>Edit</b></a>
-                    </label>
-                  </div>
-                </div>
-                {/* suggestedAddress */}
-                <div className={`suggestedAddress col-12 col-md-6 rc-column rc-padding-top--sm--mobile rc-margin-top--sm--mobile ${selectValidationOption == 'suggestedAddress' ? 'addressBorder' : ''}`}>
-                  <div className="rc-input rc-margin-top--sm--mobile text-break" data-rc-feature-forms-setup="true">
-                    <input type="radio" className="rc-input__radio" name="addressChoice" id="suggestedAddress" value="suggestedAddress" defaultChecked={selectValidationOption == 'suggestedAddress' ? true : false} onChange={(e) => this.chooseValidationAddress(e)} />
-                    <label className="rc-input__label--inline rc-margin-bottom--none text-left" for="suggestedAddress">
-                      <b>Suggested Address</b>
-                      <br />
-                      <span className="name"></span>
-                      <span className="address-one">{validationAddress.address1},</span>
-                      <span className="address-two">{validationAddress.address2}</span>
-                      <span className="city">{validationAddress.city},</span>
-                      <span className="state">{validationAddress.provinceCode},</span>
-                      <span className="postalCode">{validationAddress.postalCode},</span>
-                      <span className="countryCode">{validationAddress.countryCode}</span>
-                    </label>
-                  </div>
-                </div>
-              </div>
-              <div className="sav-buttons-div text-center rc-padding-top--lg--mobile rc-padding-top--sm">
-                <button className="rc-btn rc-btn--one confirmAddress" onClick={(e) => this.confirmValidationAddress(e)}>Confirm</button>
-              </div>
-              <input type="hidden" name="addressClassification" id="addressClassification" />
-              <input type="hidden" name="fedexValidated" id="fedexValidated" />
-            </div>
-          </div>
-        </div>
-      </div >
-    );
-  };
   render() {
     const { panelStatus } = this;
 
     const { showConfirmBtn } = this.props;
-    const { loading, form, isValid } = this.state;
+    const { loading, form, isValid, validationAddress, modalVisible, selectValidationOption } = this.state;
 
     const _editForm = (
       <EditForm
@@ -338,7 +268,22 @@ class VisitorAddress extends React.Component {
           ) : null
         ) : null}
 
-        {loading ? <Loading positionAbsolute="true" /> : this.modalJSX()}
+        {loading ?
+          <Loading positionFixed="true" /> :
+          <ValidationAddressModal
+            modalVisible={modalVisible}
+            address={form}
+            validationAddress={validationAddress}
+            selectValidationOption={selectValidationOption}
+            handleChooseValidationAddress={(e) => this.chooseValidationAddress(e)}
+            hanldeClickConfirm={() => this.confirmValidationAddress()}
+            close={() => {
+              this.setState({
+                modalVisible: false
+              });
+            }}
+          />
+        }
 
       </>
     );
