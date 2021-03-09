d03d394ca64d90618fac98183891427fdba916d5
ZengKun <kensyang@deloitte.com.cn>
2021-03-09 15:42:33 +0800

    添加地址超过20个错误提示

--- src/views/Payment/index.js
+++ src/views/Payment/index.js
@@ -232,8 +232,8 @@ class Payment extends React.Component {
         {
           needPrescriber: checkoutStore.autoAuditFlag
             ? (this.isLogin ? this.loginCartData : this.cartData).filter(
-                (el) => el.prescriberFlag
-              ).length > 0
+              (el) => el.prescriberFlag
+            ).length > 0
             : checkoutStore.AuditData.length > 0
         },
         () => {
@@ -844,9 +844,9 @@ class Payment extends React.Component {
           const oxxoArgs = oxxoContent.args;
           oxxoPayUrl =
             oxxoArgs &&
-            oxxoArgs.additionalDetails &&
-            oxxoArgs.additionalDetails.object &&
-            oxxoArgs.additionalDetails.object.data[0]
+              oxxoArgs.additionalDetails &&
+              oxxoArgs.additionalDetails.object &&
+              oxxoArgs.additionalDetails.object.data[0]
               ? oxxoArgs.additionalDetails.object.data[0].href
               : '';
           subOrderNumberList = tidList.length
@@ -1526,6 +1526,10 @@ class Payment extends React.Component {
       this.setState({ billingAddress: data });
     }
   };
+  // 抓取异常信息
+  catchAddOrEditAddressErrorMessage = (msg) => {
+    this.showErrorMsg(msg);
+  }
 
   /**
    * 渲染address panel
@@ -1536,15 +1540,14 @@ class Payment extends React.Component {
     return (
       <>
         <div
-          className={`card-panel checkout--padding rc-bg-colour--brand3 rounded mb-3 border ${
-            paymentStore.deliveryAddrPanelStatus.isEdit
+          className={`card-panel checkout--padding rc-bg-colour--brand3 rounded mb-3 border ${paymentStore.deliveryAddrPanelStatus.isEdit
               ? 'border-333'
               : 'border-transparent'
-          }`}
+            }`}
           id="J_checkout_panel_deliveryAddr"
         >
           {this.isLogin ? (
-            <AddressList id="1" updateData={this.updateDeliveryAddrData} />
+            <AddressList id="1" updateData={this.updateDeliveryAddrData} catchErrorMessage={this.catchAddOrEditAddressErrorMessage} />
           ) : (
             <VisitorAddress
               key={1}
@@ -1599,9 +1602,8 @@ class Payment extends React.Component {
                 showOperateBtn={false}
                 visible={!billingChecked}
                 updateData={this.updateBillingAddrData}
-                updateFormValidStatus={this.updateValidStatus.bind(this, {
-                  key: 'billingAddr'
-                })}
+                updateFormValidStatus={this.updateValidStatus.bind(this, { key: 'billingAddr' })}
+                catchErrorMessage={this.catchAddOrEditAddressErrorMessage}
               />
             ) : (
               <VisitorAddress
@@ -1765,11 +1767,10 @@ class Payment extends React.Component {
             {payWayNameArr.map((item, i) => {
               return (
                 <div
-                  className={`rc-input rc-input--inline ${
-                    subForm.buyWay == 'frequency' && item.id == 'adyenPayLater'
+                  className={`rc-input rc-input--inline ${subForm.buyWay == 'frequency' && item.id == 'adyenPayLater'
                       ? 'hidden'
                       : ''
-                  }`}
+                    }`}
                   key={i}
                 >
                   <input
@@ -2005,7 +2006,7 @@ class Payment extends React.Component {
       <div className="ml-custom mr-custom mb-3">
         <div className="row">
           {paymentTypeVal === 'payUCreditCard' ||
-          paymentTypeVal === 'adyenCard' ? (
+            paymentTypeVal === 'adyenCard' ? (
             <div className="col-12 col-md-6">
               <span className="medium">
                 <FormattedMessage id="bankCard" />
@@ -2253,10 +2254,10 @@ class Payment extends React.Component {
     const paymentMethodTitle = paymentMethodPanelStatus.isPrepare
       ? paymentMethodTitleForPrepare
       : paymentMethodPanelStatus.isEdit
-      ? paymentMethodTitleForEdit
-      : paymentMethodPanelStatus.isCompleted
-      ? paymentMethodTitleForCompeleted
-      : null;
+        ? paymentMethodTitleForEdit
+        : paymentMethodPanelStatus.isCompleted
+          ? paymentMethodTitleForCompeleted
+          : null;
 
     return (
       <div>
@@ -2286,9 +2287,8 @@ class Payment extends React.Component {
               <div className="rc-column rc-double-width shipping__address">
                 {/* 错误提示 */}
                 <div
-                  className={`rc-padding-bottom--xs cart-error-messaging cart-error ${
-                    errorMsg ? '' : 'hidden'
-                  }`}
+                  className={`rc-padding-bottom--xs cart-error-messaging cart-error ${errorMsg ? '' : 'hidden'
+                    }`}
                 >
                   <aside
                     className="rc-alert rc-alert--error rc-alert--with-close"
@@ -2338,121 +2338,120 @@ class Payment extends React.Component {
                       </p>
                       {this.isLogin
                         ? checkoutStore.AuditData.map((el, i) => {
-                            return (
-                              <div className="petProduct">
-                                <LazyLoad>
-                                  <img
-                                    className="pull-left"
-                                    alt=""
-                                    src={el.goodsInfoImg}
-                                  />
-                                </LazyLoad>
+                          return (
+                            <div className="petProduct">
+                              <LazyLoad>
+                                <img
+                                  className="pull-left"
+                                  alt=""
+                                  src={el.goodsInfoImg}
+                                />
+                              </LazyLoad>
 
-                                <div
-                                  className="pull-left"
-                                  style={{
-                                    marginTop: '20px',
-                                    marginLeft: '20px'
-                                  }}
-                                >
-                                  <p>
-                                    <span>Pet:</span>
-                                    <span>
-                                      {el.petName ? el.petName : 'required'}
-                                    </span>
-                                  </p>
-                                  <p>
-                                    <span>Qty:</span>
-                                    <span>{el.buyCount}</span>
-                                  </p>
-                                </div>
-                                <div
-                                  className="pull-right"
-                                  style={{
-                                    marginTop: '30px',
-                                    marginLeft: '20px'
-                                  }}
-                                >
-                                  <button
-                                    className="rc-btn rc-btn--sm rc-btn--one"
-                                    onClick={() => {
-                                      this.setState({
-                                        petModalVisible: true,
-                                        currentProIndex: i
-                                      });
-                                    }}
-                                  >
-                                    Select a pet
-                                  </button>
-                                </div>
+                              <div
+                                className="pull-left"
+                                style={{
+                                  marginTop: '20px',
+                                  marginLeft: '20px'
+                                }}
+                              >
+                                <p>
+                                  <span>Pet:</span>
+                                  <span>
+                                    {el.petName ? el.petName : 'required'}
+                                  </span>
+                                </p>
+                                <p>
+                                  <span>Qty:</span>
+                                  <span>{el.buyCount}</span>
+                                </p>
                               </div>
-                            );
-                          })
+                              <div
+                                className="pull-right"
+                                style={{
+                                  marginTop: '30px',
+                                  marginLeft: '20px'
+                                }}
+                              >
+                                <button
+                                  className="rc-btn rc-btn--sm rc-btn--one"
+                                  onClick={() => {
+                                    this.setState({
+                                      petModalVisible: true,
+                                      currentProIndex: i
+                                    });
+                                  }}
+                                >
+                                  Select a pet
+                                  </button>
+                              </div>
+                            </div>
+                          );
+                        })
                         : checkoutStore.AuditData.map((el, i) => {
-                            return (
-                              <div className="petProduct" key={i}>
-                                <LazyLoad>
-                                  <img
-                                    alt=""
-                                    src={
-                                      el.sizeList.filter((el) => el.selected)[0]
-                                        .goodsInfoImg
-                                    }
-                                    className="pull-left"
-                                  />
-                                </LazyLoad>
-                                <div
+                          return (
+                            <div className="petProduct" key={i}>
+                              <LazyLoad>
+                                <img
+                                  alt=""
+                                  src={
+                                    el.sizeList.filter((el) => el.selected)[0]
+                                      .goodsInfoImg
+                                  }
                                   className="pull-left"
-                                  style={{
-                                    marginTop: '20px',
-                                    marginLeft: '20px'
-                                  }}
-                                >
-                                  <p>
-                                    <span>Pet:</span>
-                                    <span>
-                                      {el.petForm
-                                        ? el.petForm.petName
-                                        : 'required'}
-                                    </span>
-                                  </p>
-                                  <p>
-                                    <span>Qty:</span>
-                                    <span>{el.quantity}</span>
-                                  </p>
-                                </div>
-                                <div
-                                  className="pull-right"
-                                  style={{
-                                    marginTop: '30px',
-                                    marginLeft: '20px'
-                                  }}
-                                >
-                                  <button
-                                    id="selectPet"
-                                    className="rc-btn rc-btn--sm rc-btn--one"
-                                    onClick={() => {
-                                      this.setState({
-                                        petModalVisible: true,
-                                        currentProIndex: i
-                                      });
-                                    }}
-                                  >
-                                    Select a pet
-                                  </button>
-                                </div>
+                                />
+                              </LazyLoad>
+                              <div
+                                className="pull-left"
+                                style={{
+                                  marginTop: '20px',
+                                  marginLeft: '20px'
+                                }}
+                              >
+                                <p>
+                                  <span>Pet:</span>
+                                  <span>
+                                    {el.petForm
+                                      ? el.petForm.petName
+                                      : 'required'}
+                                  </span>
+                                </p>
+                                <p>
+                                  <span>Qty:</span>
+                                  <span>{el.quantity}</span>
+                                </p>
                               </div>
-                            );
-                          })}
+                              <div
+                                className="pull-right"
+                                style={{
+                                  marginTop: '30px',
+                                  marginLeft: '20px'
+                                }}
+                              >
+                                <button
+                                  id="selectPet"
+                                  className="rc-btn rc-btn--sm rc-btn--one"
+                                  onClick={() => {
+                                    this.setState({
+                                      petModalVisible: true,
+                                      currentProIndex: i
+                                    });
+                                  }}
+                                >
+                                  Select a pet
+                                  </button>
+                              </div>
+                            </div>
+                          );
+                        })}
                     </h5>
                   </div>
                 )}
                 <div
-                  className={`card-panel checkout--padding rc-bg-colour--brand3 rounded pl-0 pr-0 mb-3 pb-0 border ${
-                    paymentMethodPanelStatus.isEdit
+                  className={`card-panel checkout--padding rc-bg-colour--brand3 rounded pl-0 pr-0 mb-3 pb-0 border ${paymentMethodPanelStatus.isEdit
                       ? 'border-333'
                       : 'border-transparent'
-                  }`}
+                    }`}
                   id="J_checkout_panel_paymentMethod"
                 >
                   <span>{paymentMethodTitle}</span>
@@ -2505,9 +2504,8 @@ class Payment extends React.Component {
           </div>
           <div className="checkout-product-summary rc-bg-colour--brand3 rc-border-all rc-border-colour--brand4 rc-md-down">
             <div
-              className={`order-summary-title align-items-center justify-content-between text-center ${
-                mobileCartVisibleKey === 'less' ? 'd-flex' : 'hidden'
-              }`}
+              className={`order-summary-title align-items-center justify-content-between text-center ${mobileCartVisibleKey === 'less' ? 'd-flex' : 'hidden'
+                }`}
               onClick={this.toggleMobileCart.bind(this, 'more')}
             >
               <span
