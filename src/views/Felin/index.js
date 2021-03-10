cdf17bafb6fd559b2c2e2974093164a2dc884809
yuqi zhu <yuqizhu@yuqideMacBook-Pro.local>
2021-03-10 13:55:36 +0800

    修改bug

--- src/views/Felin/index.js
+++ src/views/Felin/index.js
@@ -368,7 +368,8 @@ export default class Felin extends React.Component {
                   <h1 class="rc-espilon">
                     <LazyLoad>
                       <img
-                        src={`${process.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/felin/cat.jpeg`}
+                        loop="infinite"
+                        src={`${process.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/felin/felin_cat.gif`}
                       />
                     </LazyLoad>
                   </h1>
