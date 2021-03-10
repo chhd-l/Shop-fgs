ee351c13fc472fbc915fae03e8df7c3a3e3525b7
Zr3Lm9Yh <18315151342@163.com>
2021-03-10 18:01:38 +0800

    : (

--- src/views/ClubLandingPage/HowItWorks.js
+++ src/views/ClubLandingPage/HowItWorks.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import LazyLoad from 'react-lazyload';
+import howitworck1 from './ClubImage/howit1.png'
+import howitworck2 from './ClubImage/howit1.png'
+import howitworck3 from './ClubImage/howit1.png'
+import howitworck4 from './ClubImage/howit1.png'
+import workflowicon from './ClubImage/howitworkflow.png'
 
 const HowItWorks=()=>{
   return(
@@ -12,67 +17,51 @@ const HowItWorks=()=>{
         <div className="rc-margin-bottom--md rc-margin-bottom--xl--mobile text-center">
           <h3 className="rc-beta">How it works</h3>
         </div>
-        <div className="flex justify-content-between">
-          <div className="rc-grid">
-            <article className="rc-card rc-card--a">
+        <div style={{display:"flex"}} className='justify-content-between'>
+          <div  >
+            <article >
               <div className="rc-card__body">
-                  <p className="rc-card__meta">Get The RIGHT LAL ARE H UHERE</p>
+                <p className="rc-card__meta"><h5>Get the right diet and ration</h5></p>
               </div>
-              <picture className="rc-card__image">
-                <img src="https://placehold.it/300x150" alt="A Dachshund jumping"/>
-              </picture>
+                <img style={{height:'200px', width:'300px'}} src={howitworck1} alt="A Dachshund jumping"/>
               <div className="rc-card__body">
-                <header>
-                  <p className="rc-card__meta">Meta 1</p>
-                </header>
+                  <p className="rc-card__meta">Answer a few questions to get a tailor-made nutritional recommendation</p>
               </div>
             </article>
           </div>
-          <div style={{alignSelf:'center'}}> > </div>
-          <div className="rc-grid">
-            <article className="rc-card rc-card--a">
+          <div style={{alignSelf:'center',marginLeft:'10px',marginRight:'10px'}} ><article> <img src={workflowicon}/> </article> </div>
+          <div  >
+            <article >
               <div className="rc-card__body">
-                <p className="rc-card__meta">Meta 2</p>
+                <p className="rc-card__meta"><h5>Select the CLUB subscription option</h5></p>
               </div>
-              <picture className="rc-card__image">
-                <img src="https://placehold.it/300x150" alt="A Dachshund jumping"/>
-              </picture>
+              <img style={{height:'200px', width:'300px'}} src={howitworck2} alt="A Dachshund jumping"/>
               <div className="rc-card__body">
-                <header>
-                  <p className="rc-card__meta">Meta 1</p>
-                </header>
+                <p className="rc-card__meta">Proceed to check out to subscribe. You will receive a welcome pack with your first delivery</p>
               </div>
             </article>
           </div>
-          <div style={{alignSelf:'center'}}> > </div>
-          <div className="rc-grid">
-            <article className="rc-card rc-card--a">
+          <div style={{alignSelf:'center',marginLeft:'10px',marginRight:'10px'}} ><article> <img src={workflowicon}/> </article> </div>
+          <div  >
+            <article >
               <div className="rc-card__body">
-                <p className="rc-card__meta">Meta 2</p>
+                <p className="rc-card__meta"><h5>Get the right diet and ration</h5></p>
               </div>
-              <picture className="rc-card__image">
-                <img src="https://placehold.it/300x150" alt="A Dachshund jumping"/>
-              </picture>
+              <img style={{height:'200px', width:'300px'}} src={howitworck3} alt="A Dachshund jumping"/>
               <div className="rc-card__body">
-                <header>
-                  <p className="rc-card__meta">Meta 1</p>
-                </header>
+                <p className="rc-card__meta">Answer a few questions to get a tailor-made nutritional recommendation</p>
               </div>
             </article>
           </div>
-          <div style={{alignSelf:'center'}}> > </div>
-          <div className="rc-grid">
-            <article className="rc-card rc-card--a">
+          <div style={{alignSelf:'center',marginLeft:'10px',marginRight:'10px'}} ><article> <img src={workflowicon}/> </article> </div>
+          <div  >
+            <article >
               <div className="rc-card__body">
-                <p className="rc-card__meta">Meta 2</p>
+                <p className="rc-card__meta"><h5>Enjoy exclusive rewards and services</h5></p>
               </div>
-              <picture className="rc-card__image">
-                <img src="https://placehold.it/300x150" alt="A Dachshund jumping"/>
-              </picture>
+              <img style={{height:'200px', width:'300px'}} src={howitworck4} alt="A Dachshund jumping"/>
               <div className="rc-card__body">
-                <header>
-                  <p className="rc-card__meta">Meta 1</p>
-                </header>
+                <p className="rc-card__meta">Take advantage of subscription offers and services, like a personal pet advisor, vet consultations and more.</p>
               </div>
             </article>
           </div>
