4e8acd53fce75c8d8624658d68519d5067552ac7
minyatang <mytang11@163.com>
2021-03-16 20:08:55 +0800

    样式优化

--- src/components/Header/hub/PromotionPanel.js
+++ src/components/Header/hub/PromotionPanel.js
@@ -24,7 +24,8 @@ export default function PromotionPanel({
         {/* <LazyLoad style={{ flex: 1, width: '100%', height: '100%' }}> */}
         <div style={{ flex: 1, width: '100%', height: '100%' }}>
           <img
-            className="ad-img"
+            className="ad-img111"
+            style={{ margin: '0 auto', width: '100%' }}
             src={cItem.Image.Url}
             alt={cItem.Image.AltText}
             srcSet={cItem.Image.Srcset}
