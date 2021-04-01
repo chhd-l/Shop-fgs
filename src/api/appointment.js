d559816cdfd4f4034e40ecde7e2905c9dae54db0
yuqi zhu <yuqizhu@yuqideMacBook-Pro.local>
2021-04-01 12:00:37 +0800

    修改bug

--- src/api/appointment.js
+++ src/api/appointment.js
@@ -2,7 +2,8 @@ import axios from '@/utils/request';
 
 const api = {
   getTimeOptions: '/appt/findByStoreAndDate',
-  apptSave: '/appt/save'
+  apptSave: '/appt/save',
+  consentList: '/appt/listApptPageConsent'
 };
 
 export default api;
@@ -18,6 +19,14 @@ export function getTimeOptions(parameter) {
 export function apptSave(parameter) {
   return axios({
     url: `${api.apptSave}`,
+    method: 'post',
+    data: parameter
+  });
+}
+
+export function getConsentList(parameter) {
+  return axios({
+    url: `${api.consentList}`,
     method: 'post',
     data: parameter
   });
