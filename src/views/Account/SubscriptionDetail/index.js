2db79cd6d044e4201b8c03e05d1d24ffe6d520db
knifecome <qhx717@qq.com>
2021-04-02 10:37:15 +0800

    update

--- src/views/Account/SubscriptionDetail/index.js
+++ src/views/Account/SubscriptionDetail/index.js
@@ -2534,6 +2534,7 @@ class SubscriptionDetail extends React.Component {
                         };
 
                         this.setState({ loading: true });
+
                         updateDetail(param)
                           .then((res) => {
                             this.getDetail(
