cb8788f0873c8b6c022395fa2f72424f2e732320
knifecome <qhx717@qq.com>
2021-04-07 17:35:26 +0800

    update

--- src/views/Payment/index.js
+++ src/views/Payment/index.js
@@ -1138,6 +1138,9 @@ class Payment extends React.Component {
             oxxoArgs.additionalDetails.object.data[0]
               ? oxxoArgs.additionalDetails.object.data[0].href
               : '';
+          console.log(res);
+          console.log(oxxoPayUrl);
+          debugger;
           subOrderNumberList = tidList.length
             ? tidList
             : oxxoContent && oxxoContent.tidList;
