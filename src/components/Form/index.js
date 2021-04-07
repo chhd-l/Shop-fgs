3d5f00d5e8245efb2463095a53e58a8f7b4f4237
ZengKun <kensyang@deloitte.com.cn>
2021-04-07 18:01:56 +0800

    update form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -63,7 +63,7 @@ class Form extends React.Component {
         province: '',
         stateId: '',
         postCode: '',
-        phoneNumber: '',
+        phoneNumber: 0,
         entrance: '',
         apartment: '',
         comment: '',
