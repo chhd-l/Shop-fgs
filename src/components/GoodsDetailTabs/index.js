f321187c4966e57ff61571014892717903faea74
yuqi zhu <yuqizhu@yuqideMacBook-Pro.local>
2021-04-12 02:36:40 -0400

    修改bug

--- src/components/GoodsDetailTabs/index.js
+++ src/components/GoodsDetailTabs/index.js
@@ -487,7 +487,7 @@ const GoodsDetailTabs = function (props) {
                     </div>
                     <div className="col-12 col-md-6">
                       <div className="rc-video-wrapper">
-                        <img />
+                        <img src={landingBanner} />
                         {/* <iframe
                           src="https://www.youtube.com/embed/FYwO1fiYoa8?enablejsapi=1&amp;origin=https%3A%2F%2Fshop.royalcanin.com"
                           allowfullscreen=""
