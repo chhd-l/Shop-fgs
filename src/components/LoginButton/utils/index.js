be62ed0dc53f64c51c2f1192e7447b0119066bf9
knifecome <qhx717@qq.com>
2021-04-15 17:42:36 +0800

    update

--- src/components/LoginButton/utils/index.js
+++ src/components/LoginButton/utils/index.js
@@ -1,12 +1,12 @@
 import { isDuringDate, transTime } from '@/utils/utils';
 
-const START_TIME = '2021/04/17 08:00'; //限制登录开始时间
-const END_TIME = '2021/04/17 16:00'; //限制登录结束时间
+// const START_TIME = '2021/04/17 08:00'; //限制登录开始时间
+// const END_TIME = '2021/04/17 16:00'; //限制登录结束时间
 
 const TimeZone = 'America/Chicago'; //需要重新转化的时区
 
-// const START_TIME = "2021/04/14 01:00"
-// const END_TIME = "2021/04/17 05:00"
+const START_TIME = '2021/04/15 07:00';
+const END_TIME = '2021/04/15 08:00';
 
 //是否限制登录
 export const isLimitLogin = () => {
