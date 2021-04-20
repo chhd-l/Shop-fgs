b294ef4cc0bbf542640b6abb9107aeb764170a51
knifecome <qhx717@qq.com>
2021-04-20 10:41:12 +0800

    去掉redirectUrl.js文件

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -463,7 +463,6 @@ class Home extends React.Component {
   }
 
   async componentDidMount() {
-    console.log(123);
     if (localItemRoyal.get('login-again')) {
       loginStore.changeLoginModal(true);
       var callOktaCallBack = getOktaCallBackUrl(
