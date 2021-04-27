fc57b44fd19cefe9f285526f448f4cfb7659fbf6
Shi <xiaoshi@deloitte.com.cn>
2021-04-27 18:26:55 +0800

    手机端菜单 图片显示问题 fix

--- src/components/Header/modules/PromotionPanel.js
+++ src/components/Header/modules/PromotionPanel.js
@@ -4,32 +4,34 @@ import LazyLoad from 'react-lazyload';
 export default function PromotionPanel({ item }) {
   return (
     <div className="p-4 pt-sm-0 pb-sm-0 dropdown-nav__ad__card">
-      <div className="border d-flex align-items-center p-4">
-        <div className="container-text">
-          <p className="title-text red">{item.Title}</p>
-          <p className="medium">{item.Subtitle}</p>
-          <a href={item.PrimaryLink.Url} className="rc-btn rc-btn--two red">
-            {item.PrimaryLink.Text}
-          </a>
+      <div className="border  p-4">
+        <div className="d-flex align-items-center">
+          <div className="container-text">
+            <p className="title-text red">{item.Title}</p>
+            <p className="medium">{item.Subtitle}</p>
+          </div>
+          <div
+            // scrollContainer=".rc-nav"
+            style={{
+              flex: 1,
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
+            <img
+              className="ad-img"
+              src={item.Image.Url}
+              alt={item.Image.AltText}
+              srcSet={item.Image.Srcset}
+            />
+          </div>
         </div>
-        <LazyLoad
-          // scrollContainer=".rc-nav"
-          style={{
-            flex: 1,
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center'
-          }}
-        >
-          <img
-            className="ad-img"
-            src={item.Image.Url}
-            alt={item.Image.AltText}
-            srcSet={item.Image.Srcset}
-          />
-        </LazyLoad>
+        <a href={item.PrimaryLink.Url} className="rc-btn rc-btn--two red">
+          {item.PrimaryLink.Text}
+        </a>
       </div>
     </div>
   );
