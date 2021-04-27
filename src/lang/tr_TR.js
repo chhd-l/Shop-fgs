3fb0b510b613fa00d67ae75c4a7795d053358edc
lizongji <kensyang@deloitte.com.cn>
2021-04-27 13:24:25 +0800

    update

--- src/lang/tr_TR.js
+++ src/lang/tr_TR.js
@@ -1147,6 +1147,7 @@ const tr_TR = {
   'subscription.changeNow': 'Değişikliği yap',
   'subscription.thesameProd': 'Değiştirilen ürün, mevcut ürün ile aynıdır',
   'subscription.productRecommendation': 'Ürün öneriniz.',
+  'Your product recommendatio': 'Ürün öneriniz.',
   'subscription.productRecommendationTip':
     'Evcil hayvan profilinize göre, size aşağıdaki ürünleri öneriyoruz. Aboneliğinizi güncellemek için lütfen ürün değişikliğini onaylayın.',
   switchProductTip1: 'Profil bilgilerine göre, şu an',
--- src/views/Account/PetForm/index.js
+++ src/views/Account/PetForm/index.js
@@ -470,6 +470,9 @@ class PetForm extends React.Component {
           ++diffIndex;
         }
       }
+    } else {
+      // 新增的情况下都会改变
+      diffIndex = 1;
     }
 
     try {
@@ -510,7 +513,7 @@ class PetForm extends React.Component {
         currentStep: 'success'
       });
       if (!isEditAlert) {
-        this.gotoNext();
+        this.gotoNext(null, diffIndex);
       }
     } catch (err) {
       this.showErrorMsg(err.message || this.props.intl.messages.saveFailed);
@@ -520,19 +523,21 @@ class PetForm extends React.Component {
     }
   };
 
-  gotoNext(stateText = 'isFromPets') {
+  gotoNext(stateText = 'isFromPets', diffIndex) {
     let isLinkedSub = this.state.subList.find((el) => el.petsId);
     let petsIdLinkedSub = isLinkedSub?.petsId;
     let subscribeId =
       this.props.location.state?.subscribeId || isLinkedSub?.subscribeId;
+    let url = '/account/pets/';
     if (subscribeId || petsIdLinkedSub) {
-      this.props.history.push({
-        pathname: `/account/subscription/order/detail/${subscribeId}`,
-        state: { [stateText]: true }
-      });
-    } else {
-      this.props.history.push('/account/pets/');
+      if (diffIndex) {
+        url = {
+          pathname: `/account/subscription/order/detail/${subscribeId}`,
+          state: { [stateText]: true }
+        };
+      }
     }
+    this.props.history.push(url);
   }
 
   inputNickname = (e) => {
