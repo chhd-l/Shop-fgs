728c9e22b3c578f496efdfec82c2d7b6977faf9f
你好哇 <7967646+hello-wow@user.noreply.gitee.com>
2021-05-19 11:14:21 +0800

    langdingpagenew-desktop（√）

--- src/views/ClubLandingPageNew/Components/GetMoreAd/index.js
+++ src/views/ClubLandingPageNew/Components/GetMoreAd/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import Logo from '../../../../components/Logo';
+import howitworknew1 from '../HowItWorksNew/image/howitworksnew1.png';
+import LazyLoad from 'react-lazyload';
+import logoad from './image/logoad.png';
+import { Link } from 'react-router-dom';
+import logoclubad from './image/CLUBLOGOSUBSCIPTION@4x.png';
 
 const GetMoreAd = () => {
   return (
@@ -24,14 +29,68 @@ const GetMoreAd = () => {
                 >
                   <div
                     style={{
-                      height: '30vh',
-                      marginLeft: '5vh',
-                      marginTop: '10vh'
+                      height: '20vh',
+                      marginRight: '5vh'
                     }}
                   >
-                    <Logo />
+                    <div
+                      style={{
+                        display: 'flex',
+                        marginLeft: '10vh',
+                        marginTop: '10vh'
+                      }}
+                    >
+                      <div>
+                        <LazyLoad>
+                          <img
+                            className="w-60 lazyloaded"
+                            src={logoad}
+                            style={{ width: '100px' }}
+                          />
+                        </LazyLoad>
+                      </div>
+                      <div
+                        style={{
+                          marginLeft: '5vh',
+                          transform: 'translateY(-10px)'
+                        }}
+                      >
+                        <p
+                          style={{
+                            fontSize: '28px',
+                            marginBottom: '0',
+                            fontWeight: 'bolder'
+                          }}
+                        >
+                          SINGLE{' '}
+                        </p>
+                        <p style={{ fontSize: '28px', fontWeight: 'bolder' }}>
+                          Purchase
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                  <div style={{ height: '30vh' }}></div>
+
+                  <div style={{ height: '30vh' }}>
+                    <div className="rc-column">
+                      <div className="rc-padding-y--lg--mobile rc-full-width">
+                        <ul className="rc-list rc-list--blank rc-list--align rc-list--large-icon">
+                          <li className="rc-list__item">
+                            <em className="bingoWhite rc-margin-right--xs"></em>
+                            <a style={{ marginLeft: '10px' }}>
+                              Tailored nutrition
+                            </a>
+                          </li>
+                          <li className="rc-list__item">
+                            <em className="bingoWhite rc-margin-right--xs"></em>
+                            <a style={{ marginLeft: '10px' }}>
+                              Free shipping from $50
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <div
                   style={{
@@ -39,7 +98,124 @@ const GetMoreAd = () => {
                     boxShadow: ' 0vh 0vh 0.3vh 0.1vh #ed001a'
                   }}
                 >
-                  nihaouayayyyyyyyyyyyyyuuuuu
+                  <div
+                    style={{
+                      height: '20vh',
+                      marginRight: '5vh'
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: 'flex',
+                        marginLeft: '25vh',
+                        marginTop: '10vh'
+                      }}
+                    >
+                      <div>
+                        <LazyLoad>
+                          <img
+                            className="w-60 lazyloaded"
+                            src={logoclubad}
+                            style={{ width: '100px' }}
+                          />
+                        </LazyLoad>
+                      </div>
+                      <div
+                        style={{
+                          marginLeft: '5vh',
+                          transform: 'translateY(-10px)'
+                        }}
+                      >
+                        <p
+                          style={{
+                            fontSize: '28px',
+                            marginBottom: '0',
+                            fontWeight: 'bolder',
+                            color: '#E2001A'
+                          }}
+                        >
+                          Club{' '}
+                        </p>
+                        <p
+                          style={{
+                            fontSize: '28px',
+                            fontWeight: 'bolder',
+                            color: '#E2001A'
+                          }}
+                        >
+                          Subscription
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div style={{ height: '30vh', display: 'flex' }}>
+                    <div className="rc-column">
+                      <div className="rc-padding-y--lg--mobile rc-full-width">
+                        <ul className="rc-list rc-list--blank rc-list--align rc-list--large-icon">
+                          <li className="rc-list__item">
+                            <em className="bingoWhite rc-margin-right--xs"></em>
+                            <a style={{ marginLeft: '10px' }}>
+                              Tailored nutrition
+                            </a>
+                          </li>
+                          <li className="rc-list__item">
+                            <em className="bingoWhite rc-margin-right--xs"></em>
+                            <a style={{ marginLeft: '10px' }}>
+                              Free shipping from $50
+                            </a>
+                          </li>
+                          <li className="rc-list__item">
+                            <em className="bingoWhite rc-margin-right--xs"></em>
+                            <a style={{ marginLeft: '10px' }}>
+                              Tailored nutrition
+                            </a>
+                          </li>
+                          <li className="rc-list__item">
+                            <em className="bingoWhite rc-margin-right--xs"></em>
+                            <a style={{ marginLeft: '10px' }}>
+                              Free shipping from $50
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="rc-column">
+                      <div className="rc-padding-y--lg--mobile rc-full-width">
+                        <ul className="rc-list rc-list--blank rc-list--align rc-list--large-icon">
+                          <li className="rc-list__item">
+                            <em className="bingoWhite rc-margin-right--xs"></em>
+                            <a style={{ marginLeft: '10px' }}>
+                              Tailored nutrition
+                            </a>
+                          </li>
+                          <li className="rc-list__item">
+                            <em className="bingoWhite rc-margin-right--xs"></em>
+                            <a style={{ marginLeft: '10px' }}>
+                              Free shipping from $50
+                            </a>
+                          </li>
+                          <li className="rc-list__item">
+                            <em className="bingoWhite rc-margin-right--xs"></em>
+                            <a style={{ marginLeft: '10px' }}>
+                              Free shipping from $50
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <br />
+                  <div className=" rc-btn-group m-0 rc-column rc-padding-x--none">
+                    <Link to="/">
+                      <button
+                        className="rc-btn rc-btn--one rc-margin-right--xs rc-margin-bottom--xs"
+                        style={{ marginLeft: '20vh' }}
+                      >
+                        Get started
+                      </button>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
