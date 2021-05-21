8c6528add0c078ffc22f5b742a2e0117d2affde3
杨升 <kensyang@deloitte.com.cn>
2021-05-21 12:04:59 +0800

    home ru 不要seo

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -539,18 +539,20 @@ class Home extends React.Component {
     if (localItemRoyal.get('login-again')) {
       return null;
     }
-
+    const Ru = process.env.REACT_APP_COUNTRY === 'RU'; //ru-home页不要seo
     return (
       <div>
-        <Helmet>
-          <link rel="canonical" href={pageLink} />
-          <title>{this.state.seoConfig.title}</title>
-          <meta
-            name="description"
-            content={this.state.seoConfig.metaDescription}
-          />
-          <meta name="keywords" content={this.state.seoConfig.metaKeywords} />
-        </Helmet>
+        {!Ru ? (
+          <Helmet>
+            <link rel="canonical" href={pageLink} />
+            <title>{this.state.seoConfig.title}</title>
+            <meta
+              name="description"
+              content={this.state.seoConfig.metaDescription}
+            />
+            <meta name="keywords" content={this.state.seoConfig.metaKeywords} />
+          </Helmet>
+        ) : null}
         <GoogleTagManager
           additionalEvents={event}
           searchEvent={this.state.searchEvent}
