cf737e83937a02b8c21cdd0ef3d93c4ee07436fc
yuqi zhu <yuqizhu@yuqideMacBook-Pro.local>
2021-05-27 17:47:19 +0800

    修改bug

--- src/views/Payment/PaymentMethod/PayUCreditCard/MemberCardList/index.js
+++ src/views/Payment/PaymentMethod/PayUCreditCard/MemberCardList/index.js
@@ -368,8 +368,9 @@ class MemberCardList extends React.Component {
         await this.getPaymentMethodList();
         const tmpSelectedId = addRes.context.id;
         let { creditCardList } = this.state;
-        let tmpItem = creditCardList.filter((c) => c.id === tmpSelectedId);
-        tmpItem.cardCvv = creditCardInfoForm.cardCvv;
+        creditCardList.map((el) => {
+          el.cardCvv = creditCardInfoForm.cardCvv;
+        });
         this.setState({
           creditCardList,
           selectedId: tmpSelectedId,
