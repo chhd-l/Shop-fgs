a73517461ff327a695860e01b97d508925f4111f
ZengKun <kensyang@deloitte.com.cn>
2021-06-10 11:43:50 +0800

    update test

--- src/views/Test/index.js
+++ src/views/Test/index.js
@@ -201,7 +201,7 @@ class Test extends React.Component {
         <h1>0528</h1>
         <br />
         <br />
-        <h1>2021-06-09 10:51:31</h1>
+        <h1>2021-06-10 11:43:31</h1>
         <br />
         <br />
         <h2>{this.state.aaa && this.state.bbb}</h2>
