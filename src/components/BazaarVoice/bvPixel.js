a5136b12d7497b59c7add019054197027c9077d0
zuoqin <qzuo@deloitte.com.cn>
2021-06-22 16:48:47 +0800

    bvPixel参数

--- src/components/BazaarVoice/bvPixel.js
+++ src/components/BazaarVoice/bvPixel.js
@@ -7,20 +7,28 @@ export function transactionPixel(details) {
   console.log(details);
   const items = details.tradeItems.map((item) => {
     return {
-      price: String(item.subscriptionPrice.toFixed(2)),
+      price: item.subscriptionStatus
+        ? String((item.subscriptionPrice * item.num).toFixed(2))
+        : String(item.splitPrice.toFixed(2)),
       quantity: String(item.num),
       productId: item.spuNo,
       name: item.spuName,
       shippingDate: details.tradeState.createTime.split(' ')[0]
     };
   });
+  let totalPrice = 0;
+  details.tradeItems.map(
+    (item) => (totalPrice = totalPrice + item.num * item.price.toFixed(2))
+  );
   const transactionInfo = {
     currency: window.__.env.REACT_APP_CURRENCY,
     orderId: details.id,
     total: String(details.tradePrice.goodsPrice.toFixed(2)),
+    // total: String(totalPrice.toFixed(2)),
     items: items,
     // tax: String(details.tradePrice.taxFeePrice.toFixed(2)),
     shippingDate: details.tradeState.createTime.split(' ')[0],
+    // discount: String(details.tradePrice.promotionDiscountPrice.toFixed(2)),
     email: details.consignee.email,
     locale: 'en_US',
     nickname: details.consignee.name
