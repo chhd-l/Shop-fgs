8231cc66ece63de598354a931892f231340fdd4e
zuoqin <qzuo@deloitte.com.cn>
2021-06-30 11:43:51 +0800

    bug fixed:加入购物车后登录直接跳checkout页面未显示prescriber信息

--- src/lib/login-redirection.js
+++ src/lib/login-redirection.js
@@ -1,4 +1,5 @@
 import { distributeLinktoPrecriberOrPaymentPage } from '@/utils/utils';
+import { getProductPetConfig } from '@/api/payment';
 
 const sessionItemRoyal = window.__.sessionItemRoyal;
 const localItemRoyal = window.__.localItemRoyal;
@@ -14,6 +15,29 @@ const loginRedirection = async ({
     ? localItemRoyal.get('okta-redirectUrl')
     : '/';
   if (tmpUrl === '/prescription' || tmpUrl === '/cart') {
+    let autoAuditFlag = false;
+    let paramData = isLogin
+      ? checkoutStore.loginCartData
+      : checkoutStore.cartData.map((el) => {
+          el.goodsInfoId = el.sizeList.filter(
+            (item) => item.selected
+          )[0].goodsInfoId;
+          return el;
+        });
+    let res = await getProductPetConfig({
+      goodsInfos: paramData
+    });
+    let handledData = paramData.map((el, i) => {
+      el.auditCatFlag = res.context.goodsInfos[i]['auditCatFlag'];
+      el.prescriberFlag = res.context.goodsInfos[i]['prescriberFlag'];
+      return el;
+    });
+    checkoutStore.setLoginCartData(handledData);
+    let AuditData = handledData.filter((el) => el.auditCatFlag);
+    checkoutStore.setAuditData(AuditData);
+    autoAuditFlag = res.context.autoAuditFlag;
+    checkoutStore.setAutoAuditFlag(autoAuditFlag);
+    checkoutStore.setPetFlag(res.context.petFlag);
     const url = await distributeLinktoPrecriberOrPaymentPage({
       configStore,
       checkoutStore,
