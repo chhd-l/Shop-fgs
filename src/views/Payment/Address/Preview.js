fc777795aa6e118a608a4a087bff2880a34099df
ZengKun <kensyang@deloitte.com.cn>
2021-07-06 18:08:38 +0800

    pickup完善

--- src/views/Payment/Address/Preview.js
+++ src/views/Payment/Address/Preview.js
@@ -94,129 +94,144 @@ class AddressPreview extends React.Component {
 
     return form ? (
       <div className="children-nomargin">
-        {form.receiveType == 'PICK_UP' ? <></> : <></>}
-        <p className="preview_pickup_title"></p>
-        <p className="preview_pickup_address">{form.address1}</p>
-        <p className="preview_pickup_worktime">{form.workTime}</p>
-        {/* 是否存在运费 */}
-        {form?.calculation?.deliveryPrice &&
-          form?.calculation?.minDeliveryTime && (
-            <>
-              <p className="preview_delivery_date">
-                {form?.calculation?.minDeliveryTime ==
-                form?.calculation?.maxDeliveryTime ? (
-                  <FormattedMessage
-                    id="payment.deliveryDate2"
-                    values={{
-                      val: form?.calculation?.minDeliveryTime
-                    }}
-                  />
-                ) : (
-                  <FormattedMessage
-                    id="payment.deliveryDate"
-                    values={{
-                      min: form?.calculation?.minDeliveryTime,
-                      max: form?.calculation?.maxDeliveryTime
-                    }}
-                  />
-                )}
-              </p>
-            </>
-          )}
-        ————————————————————————————————————————————————————————————————
-        <p className={`${boldName ? 'medium' : ''}`}>
-          {form.firstName + ' ' + form.lastName}
-        </p>
-        <p className="preview_address">{form.address1}</p>
-        {localAddressForm['address2'] && form.address2 && (
-          <p>{form.address2}</p>
-        )}
-        {/* 俄罗斯计算运费 */}
-        {window.__.env.REACT_APP_COUNTRY == 'ru' ? (
+        {form.receiveType == 'PICK_UP' ? (
           <>
-            <p className="preview_phone_number">
-              {form.phoneNumber || form.consigneeNumber}{' '}
-            </p>
-
             {/* 是否存在运费 */}
-            {form?.calculation?.deliveryPrice &&
-              form?.calculation?.minDeliveryTime && (
-                <>
-                  <p className="preview_delivery_price">
-                    <FormattedMessage id="payment.deliveryFee" />:{' '}
-                    {formatMoney(form?.calculation?.deliveryPrice)}
-                  </p>
-                  {!form.timeSlot && form?.calculation?.minDeliveryTime && (
-                    <p className="preview_delivery_date">
-                      {form?.calculation?.minDeliveryTime ==
-                      form?.calculation?.maxDeliveryTime ? (
-                        <FormattedMessage
-                          id="payment.deliveryDate2"
-                          values={{
-                            val: form?.calculation?.minDeliveryTime
-                          }}
-                        />
-                      ) : (
-                        <FormattedMessage
-                          id="payment.deliveryDate"
-                          values={{
-                            min: form?.calculation?.minDeliveryTime,
-                            max: form?.calculation?.maxDeliveryTime
-                          }}
-                        />
-                      )}
-                    </p>
+            {form?.item?.minDeliveryTime && (
+              <>
+                <p className="preview_delivery_date">
+                  {form.item.minDeliveryTime == form.item.maxDeliveryTime ? (
+                    <FormattedMessage
+                      id="payment.deliveryDate2"
+                      values={{
+                        val: form.item.minDeliveryTime
+                      }}
+                    />
+                  ) : (
+                    <FormattedMessage
+                      id="payment.deliveryDate"
+                      values={{
+                        min: form.item.minDeliveryTime,
+                        max: form.item.maxDeliveryTime
+                      }}
+                    />
                   )}
-                </>
-              )}
-
-            {/* delivery date */}
-            {newDeliveryDate && (
-              <p className="preview_delivery_date">{newDeliveryDate}</p>
+                </p>
+              </>
             )}
-
-            {/* time slot */}
-            {form.timeSlot && (
-              <p className="preview_time_slot">{form.timeSlot}</p>
-            )}
+            <p
+              className="preview_pickup_title"
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                marginTop: '1rem'
+              }}
+            >
+              <span>{form?.pickup.courier}</span>
+              <span style={{ marginRight: '7rem' }}>
+                {formatMoney(form?.pickup.price)}
+              </span>
+            </p>
+            <p className="preview_pickup_address">{form.address1}</p>
+            <p className="preview_pickup_worktime">{form.workTime}</p>
           </>
         ) : (
           <>
-            <p className="preview_infos">
-              {window.__.env.REACT_APP_COUNTRY == 'us' ? null : (
-                <>
-                  <span>
-                    {matchNamefromDict(
-                      this.state.countryList,
-                      form.country || form.countryId
-                    )}{' '}
-                  </span>
-                </>
-              )}
-
-              {/* 城市 */}
-              {localAddressForm['city'] && (
-                <span>
-                  {form.city}
-                  {', '}
-                </span>
-              )}
-
-              {/* 区域 */}
-              {localAddressForm['region'] && (
-                <span>
-                  {form.area}
-                  {', '}
-                </span>
-              )}
-
-              {/* 省份 */}
-              {localAddressForm['state'] && <span>{form.province} </span>}
-
-              {/* 邮编 */}
-              {localAddressForm['postCode'] && <span>{form.postCode}</span>}
+            <p className={`${boldName ? 'medium' : ''}`}>
+              {form.firstName + ' ' + form.lastName}
             </p>
-            <p>{form.phoneNumber || form.consigneeNumber} </p>
+            <p className="preview_address">{form.address1}</p>
+            {localAddressForm['address2'] && form.address2 && (
+              <p>{form.address2}</p>
+            )}
+            {/* 俄罗斯计算运费 */}
+            {window.__.env.REACT_APP_COUNTRY == 'ru' ? (
+              <>
+                <p className="preview_phone_number">
+                  {form.phoneNumber || form.consigneeNumber}{' '}
+                </p>
+
+                {/* 是否存在运费 */}
+                {form?.calculation?.deliveryPrice &&
+                  form?.calculation?.minDeliveryTime && (
+                    <>
+                      <p className="preview_delivery_price">
+                        <FormattedMessage id="payment.deliveryFee" />:{' '}
+                        {formatMoney(form.calculation.deliveryPrice)}
+                      </p>
+                      {!form.timeSlot && form.calculation.minDeliveryTime && (
+                        <p className="preview_delivery_date">
+                          {form.calculation.minDeliveryTime ==
+                          form?.calculation?.maxDeliveryTime ? (
+                            <FormattedMessage
+                              id="payment.deliveryDate2"
+                              values={{
+                                val: form.calculation.minDeliveryTime
+                              }}
+                            />
+                          ) : (
+                            <FormattedMessage
+                              id="payment.deliveryDate"
+                              values={{
+                                min: form.calculation.minDeliveryTime,
+                                max: form.calculation.maxDeliveryTime
+                              }}
+                            />
+                          )}
+                        </p>
+                      )}
+                    </>
+                  )}
+
+                {/* delivery date */}
+                {newDeliveryDate && (
+                  <p className="preview_delivery_date">{newDeliveryDate}</p>
+                )}
+
+                {/* time slot */}
+                {form.timeSlot && (
+                  <p className="preview_time_slot">{form.timeSlot}</p>
+                )}
+              </>
+            ) : (
+              <>
+                <p className="preview_infos">
+                  {window.__.env.REACT_APP_COUNTRY == 'us' ? null : (
+                    <>
+                      <span>
+                        {matchNamefromDict(
+                          this.state.countryList,
+                          form.country || form.countryId
+                        )}{' '}
+                      </span>
+                    </>
+                  )}
+
+                  {/* 城市 */}
+                  {localAddressForm['city'] && (
+                    <span>
+                      {form.city}
+                      {', '}
+                    </span>
+                  )}
+
+                  {/* 区域 */}
+                  {localAddressForm['region'] && (
+                    <span>
+                      {form.area}
+                      {', '}
+                    </span>
+                  )}
+
+                  {/* 省份 */}
+                  {localAddressForm['state'] && <span>{form.province} </span>}
+
+                  {/* 邮编 */}
+                  {localAddressForm['postCode'] && <span>{form.postCode}</span>}
+                </p>
+                <p>{form.phoneNumber || form.consigneeNumber} </p>
+              </>
+            )}
           </>
         )}
       </div>
