74d92c09a566aa8b852beb3446d921208699c64f
曾琨 <1099709342@qq.com>
2021-07-27 20:46:54 +0800

    update test

--- src/views/Test/index.js
+++ src/views/Test/index.js
@@ -189,7 +189,7 @@ class Test extends React.Component {
         <h1>0513_pickup</h1>
         <br />
         <br />
-        <h1>2021-07-27 10:00:18</h1>
+        <h1>2021-07-27 21:00:18</h1>
         <br />
         <br />
         <h2>{this.state.aaa && this.state.bbb}</h2>
