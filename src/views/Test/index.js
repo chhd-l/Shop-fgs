4c66ab5df24be223212ac444a3349d13182e81a1
ZengKun <kensyang@deloitte.com.cn>
2021-07-29 18:36:42 +0800

    update test

--- src/views/Test/index.js
+++ src/views/Test/index.js
@@ -189,7 +189,7 @@ class Test extends React.Component {
         <h1>0513_pickup</h1>
         <br />
         <br />
-        <h1>2021-07-29 18:00:18</h1>
+        <h1>2021-07-29 17:00:18</h1>
         <br />
         <br />
         <h2>{this.state.aaa && this.state.bbb}</h2>
