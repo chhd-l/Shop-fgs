a58d0c6cb8d0be374a04503444f6cfeca3fac7c6
minyatang <mytang11@163.com>
2021-09-14 18:29:00 +0800

    同上一注释

--- src/lang/tr_TR.js
+++ src/lang/tr_TR.js
@@ -1318,7 +1318,7 @@ const tr_TR = {
   'ClubLP.GetMoreAd.Single.tip2': 'Ücretsiz kargo',
   'ClubLP.GetMoreAd.Club.tip1': 'Özel beslenme',
   'ClubLP.GetMoreAd.Club.tip2': 'Ücretsiz kargo',
-  'ClubLP.GetMoreAd.Club.tip3': 'Her siparişinizde %5 indirim',
+  'ClubLP.GetMoreAd.Club.tip3': 'Her siparişinizde %10 indirim',
   'ClubLP.GetMoreAd.Club.tip4': 'Kapınıza gelen otomatik sipariş',
   'ClubLP.GetMoreAd.Club.tip5': 'Kişiselleştirilmiş Haber Bültenleri',
   'ClubLP.GetMoreAd.Club.tip6': 'Hoş geldin paketi ',
