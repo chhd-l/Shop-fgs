ff1222c298b2e7a31e6ec6172655b1e920a76f92
lizongji <kensyang@deloitte.com.cn>
2021-09-24 09:57:07 +0800

    cancel nextPageIsReco

--- src/views/PreciseRecommendation/index.js
+++ src/views/PreciseRecommendation/index.js
@@ -83,7 +83,7 @@ class PreciseRecommendation extends React.Component {
   async getProductInfo() {
     let productString = sessionItemRoyal.get('nutrition-recommendation-filter');
     let res = productString && JSON.parse(productString);
-    if (res.nextPageIsReco) {
+    if (res) {
       let productId = res.goodsInfo.goodsInfoNo;
       let productShowInfo = productList[productId];
       let recommData = res;
