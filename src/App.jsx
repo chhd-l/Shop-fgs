8f2a9b85e7d3a3dbc17d32e186b06cac29589d62
jinglv <lvjing520cxc>
2021-10-14 18:20:56 +0800

    felin

--- src/App.jsx
+++ src/App.jsx
@@ -189,9 +189,9 @@ const ClubLandingPageDeVet = loadable(() =>
 const DedicatedLandingPage = loadable(() =>
   import('@/views/DedicatedLandingPage')
 );
-// const Felin = loadable(() =>
-//   import('@/views/Felin')
-// );
+const Felin = loadable(() =>
+  import('@/views/Felin2')
+);
 // const FelinRecommendation = loadable(() =>
 //   import('@/views/FelinRecommendation')
 // );
@@ -715,11 +715,11 @@ const App = () => {
                   )}
                 />
                 <Route exact sensitive path="/FAQ" component={Exception} />
-                {/* <Route
-                  path="/FelinRecommendation/:id"
-                  component={FelinRecommendation}
-                />
-                <Route path="/latelier/felin" component={Felin} /> */}
+                {/* <Route*/}
+                {/*  path="/FelinRecommendation/:id"*/}
+                {/*  component={FelinRecommendation}*/}
+                {/*/>*/}
+                <Route path="/felin" component={Felin} />
                 <Route
                   path="/"
                   render={(props) => {
