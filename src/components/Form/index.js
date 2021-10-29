107e08f5224c038e2f68d11d14445439db52100c
ZengKun <kensyang@deloitte.com.cn>
2021-10-29 16:04:10 +0800

    英国电话号码格式校验

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -578,6 +578,9 @@ class Form extends React.Component {
           if (window.__.env.REACT_APP_COUNTRY == 'fr') {
             // 法国
             regExp = /^\(\+[3][3]\)[\s](([0][1-9])|[1-9])[\s][0-9]{2}[\s][0-9]{2}[\s][0-9]{2}[\s][0-9]{2}$/;
+          } else if (window.__.env.REACT_APP_COUNTRY == 'uk') {
+            // 英国
+            regExp = /^\+[4][4][0-9]{4}[\s][0-9]{6}$/;
           } else if (window.__.env.REACT_APP_COUNTRY == 'us') {
             // 美国
             regExp = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
