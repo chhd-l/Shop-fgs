5807f1d26addda2b7b1e92bdd999947ad167e217
ZengKun <kensyang@deloitte.com.cn>
2021-11-02 16:41:44 +0800

    订单和地址展示county

--- src/views/Test/index.js
+++ src/views/Test/index.js
@@ -189,7 +189,7 @@ class Test extends React.Component {
         <h1>1014_sprint5</h1>
         <br />
         <br />
-        <h1>2021-11-02 15:30:18</h1>
+        <h1>2021-11-02 16:50:18</h1>
         <br />
         <br />
         <h2>{this.state.aaa && this.state.bbb}</h2>
