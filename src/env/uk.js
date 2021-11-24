0318128ef8bcc0094687e3b8b555f767414eb8db
tyyyyou <mytang11@163.com>
2021-11-24 17:07:53 +0800

    增加uk prod hub配置

--- src/env/uk.js
+++ src/env/uk.js
@@ -232,6 +232,48 @@ const env = {
     REACT_APP_DELETE_My_ACCOUNT_URL:
       'https://account.royalcanin.com/en-us/manager/settings'
   },
+  productionHub: {
+    // #okta
+    REACT_APP_ACCESS_PATH: 'https://www.royalcanin.com/uk/shop',
+    REACT_APP_CLIENT_ID: '0oamkkuay3Bdq8tHc416',
+    REACT_APP_ISSUER: 'https://signin.royalcanin.com/oauth2/default',
+    REACT_APP_RedirectURL:
+      'https://www.royalcanin.com/uk/shop/implicit/callback',
+    REACT_APP_RegisterPrefix: 'https://welcome.royalcanin.com/?redirect_uri=',
+    REACT_APP_RegisterCallback: 'https://www.royalcanin.com/uk/shop',
+    REACT_APP_FaceBook_IDP: '0oa75jv7oSeLBnnkt416',
+    REACT_APP_Google_IDP: '0oa75klujN7PkgedM416',
+
+    // #接口配置
+    REACT_APP_BASEURL: 'https://www.royalcanin.com/uk/shop/api',
+
+    // #Adyen
+    REACT_APP_AdyenOriginKEY:
+      'pub.v2.4116377247202506.aHR0cHM6Ly93d3cucm95YWxjYW5pbi5jb20.dQWAy2uSu5M80k2hy9aZfeCAAAfIyB61SE_-2DhqRX4',
+    REACT_APP_Adyen_ENV: 'live',
+
+    // #GA环境配置
+    REACT_APP_GA_ENV: 'prd',
+
+    // #onetrust
+    REACT_APP_ONTRUST_DOMAIN_SCRIPT: '8923767a-c9c8-4c36-ae5f-97af6af42bdb',
+
+    // #设置二级子目录访问页面
+    REACT_APP_HOMEPAGE: '/uk/shop',
+
+    // #HUB_GA
+    REACT_APP_HUB_GA: '1',
+
+    // #根节点自定义样式名
+    REACT_APP_ROOT_CLS: 'ui-custom-hub',
+
+    REACT_APP_HUB: '1',
+
+    // #delete接口前缀
+    REACT_APP_HUB_APIURL: 'https://www.royalcanin.com/uk/api',
+
+    REACT_APP_HUB_URLPREFIX: 'https://www.royalcanin.com/uk'
+  },
   stgwedding: {
     // #okta
     REACT_APP_ACCESS_PATH: 'https://stgwedding.royalcanin.com/uk/shop/',
