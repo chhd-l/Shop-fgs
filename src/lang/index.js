1f1c5531ed626dadd41fe44e5ef5b93df8aff195
tyyyyou <mytang11@163.com>
2021-11-29 18:32:06 +0800

    删除项目中管理的多语言文件

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,19 +1,19 @@
-const locales = {
-  us: require('@/lang/en_US'),
-  uk: require('@/lang/en_GB'),
-  mx: require('@/lang/es_ES'),
-  de: require('@/lang/de_DE'),
-  fr: require('@/lang/fr_FR'),
-  ru: require('@/lang/ru_RU'),
-  tr: require('@/lang/tr_TR'),
-  ca: require('@/lang/en_US'),
-  se: require('@/lang/sv_SE'),
-  core: require('@/lang/en_US')
-};
+// const locales = {
+//   us: require('@/lang/en_US'),
+//   uk: require('@/lang/en_GB'),
+//   mx: require('@/lang/es_ES'),
+//   de: require('@/lang/de_DE'),
+//   fr: require('@/lang/fr_FR'),
+//   ru: require('@/lang/ru_RU'),
+//   tr: require('@/lang/tr_TR'),
+//   ca: require('@/lang/en_US'),
+//   se: require('@/lang/sv_SE'),
+//   core: require('@/lang/en_US')
+// };
 // export default locales[window.__.env.REACT_APP_COUNTRY].default;
 
 //Phrase翻译集成
-const localLanguage = locales[window.__.env.REACT_APP_COUNTRY].default;
+// const localLanguage = locales[window.__.env.REACT_APP_COUNTRY].default;
 
 //拼接本地语言&phrase平台的语言用到的函数
 function assignObj(obj, source) {
@@ -65,4 +65,4 @@ async function getDynamicLanguage() {
   return language;
 }
 
-export { localLanguage, getDynamicLanguage };
+export { getDynamicLanguage };
