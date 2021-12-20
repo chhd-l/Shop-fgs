c073df440a228b21ec4f334e6265cc869912c280
kevin <qhx717@qq.com>
2021-12-20 18:27:01 +0800

    update

--- src/views/Home/modules/SalesCategory.js
+++ src/views/Home/modules/SalesCategory.js
@@ -75,7 +75,7 @@ class SalesCategory extends Component {
                   className="rc-beta rc-margin--none rc-padding--xs rc-padding--lg--mobile text-center rc-padding-top--none"
                   onClick={() => {
                     window.location =
-                      'swish://paymentrequest?token=c28a4061470f4af48973bd2a4642b4fa&callbackurl=merchant%253A%252F%252F';
+                      'swish://paymentrequest?callbackurl=http%3A%2F%2Flocalhost%3A3000%2FPayResult%3Fpp%3D3JHH83rBmeXC6daYRvK%252BlT6Mj0WITloV9iTaJHtE3%252F%252B%252FH07ByZKWclJ8Rb1TEstUbG8BU1HBb2RnY9XiiWCP1Nkr%252Bl9YOgueV7W4Ve%252BUMTA0XKzSKQKhlByh8fmR0NwZtCU3dxA37NxRYYRoNX6xbQ%253D%253D&token=19f06b44c8ff4ae58b74c744523bbd60';
                   }}
                 >
                   <FormattedMessage id="home.productsCategory" />
