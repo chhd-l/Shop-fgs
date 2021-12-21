e7212b64ca9ea5681af774de0307a5990f9c9459
xiaoli <kensyang@deloitte.com.cn>
2021-12-21 08:47:16 +0800

    pdp 折扣金额百分比显示添加

--- src/views/Details/components/ClubBuyMethod/index.tsx
+++ src/views/Details/components/ClubBuyMethod/index.tsx
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl-phraseapp';
 import ConfirmTooltip from '@/components/ConfirmTooltip';
 import FrequencySelection from '@/components/FrequencySelection/index.tsx';
 import { formatMoney } from '@/utils/utils';
+import {Decimal} from 'decimal.js';
 const De = window.__.env.REACT_APP_COUNTRY === 'de';
 
 interface Props {
@@ -32,6 +33,10 @@ const ClubBuyMethod = ({
   const handleToClubTab = () => {
     toClubTab();
   };
+  const discountAmount = new Decimal(currentUnitPrice).sub(new Decimal(currentSubscriptionPrice)).toNumber()
+  const discountAmountUnit = formatMoney(discountAmount)
+  console.log(currentUnitPrice,currentSubscriptionPrice,discountAmount,discountAmountUnit,'discountAmount')
+  console.log(configStore,configStore.discountDisplayTypeInfo,'discountDisplayTypeInfo==')
   return (
     <div
       className={`buyMethod rc-margin-bottom--xs d-flex row align-items-center 3 ml-0 mr-0 ui-cursor-pointer-pure ${
@@ -77,12 +82,14 @@ const ClubBuyMethod = ({
         </div>
         <br />
         <div className="discountBox" style={{ background: '#3ab41d' }}>
-          <FormattedMessage
+          {configStore.discountDisplayTypeInfo == "Percentage"?
+          <span>{formatMoney(discountAmountUnit)}</span>
+          :<FormattedMessage
             id="saveExtra"
             values={{
               val: selectedSpecItem?.subscriptionPercentage
             }}
-          />
+          />}
         </div>
         <br />
         <div className="freeshippingBox">
