b724f1c978cb65e30594f6d09b47c99ab026d06f
xiaoli <kensyang@deloitte.com.cn>
2021-12-30 16:42:48 +0800

    pdt显示更改baby判断

--- src/views/Details/index.js
+++ src/views/Details/index.js
@@ -208,8 +208,14 @@ class Details extends React.Component {
     return this.props.checkoutStore;
   }
   get btnStatus() {
-    const { details, quantity, instockStatus, initing, loading, form } =
-      this.state;
+    const {
+      details,
+      quantity,
+      instockStatus,
+      initing,
+      loading,
+      form
+    } = this.state;
     const { sizeList } = details;
     let selectedSpecItem = details.sizeList.filter((el) => el.selected)[0];
     let addedFlag = 1;
@@ -531,11 +537,8 @@ class Details extends React.Component {
               if (mixFeeding) {
                 mixFeeding.quantity = 1;
               }
-              let {
-                goodsImg = '',
-                goodsName = '',
-                goodsNo = ''
-              } = mixFeeding?.goods || {};
+              let { goodsImg = '', goodsName = '', goodsNo = '' } =
+                mixFeeding?.goods || {};
               let _hiddenMixFeedingBanner = false;
               let mixFeedingSelected = mixFeeding?.sizeList?.filter(
                 (el) => el.selected
@@ -913,8 +916,13 @@ class Details extends React.Component {
     try {
       !type && this.setState({ addToCartLoading: true });
       const { checkoutStore } = this.props;
-      const { currentUnitPrice, quantity, form, details, questionParams } =
-        this.state;
+      const {
+        currentUnitPrice,
+        quantity,
+        form,
+        details,
+        questionParams
+      } = this.state;
       hubGAAToCar(quantity, form);
       let cartItem = Object.assign({}, details, {
         selected: true,
@@ -1095,11 +1103,12 @@ class Details extends React.Component {
     );
     let isTechnology = ['dry', 'wet'].includes(details?.wsTechnologyCode);
     let reg = new RegExp('baby', 'i');
-    let isBaby = goodsAttributesValueRelList.find(
-      (item) =>
-        item.goodsAttributeName === 'Lifestages' &&
-        reg.test(item.goodsAttributeValue)
+    let LifestagesAttr = goodsAttributesValueRelList.filter(
+      (item) => item.goodsAttributeName === 'Lifestages'
     );
+    let isBaby =
+      LifestagesAttr.length === 1 &&
+      reg.test(LifestagesAttr?.[0]?.goodsAttributeValue);
 
     /**
      *  是否显示计算工具
