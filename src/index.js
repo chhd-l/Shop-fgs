41e93148c74a05c2ef21e8befd607c20b8c36f13
lizongji <kensyang@deloitte.com.cn>
2022-01-04 19:10:09 +0800

    删除不需要的

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import '@/assets/css/global.css';
 import '@/utils/global';
 import './index.css';
 import '@/env/init';
-import '@/assets/css/royal-canin/royal-canin.styles.forms.min.css';
+// import '@/assets/css/royal-canin/royal-canin.styles.forms.min.css';
 import '@/assets/css/royal-canin/royal-canin.styles.icons.min.css';
-import '@/assets/css/royal-canin/royal-canin.styles.progress.min.css';
+// import '@/assets/css/royal-canin/royal-canin.styles.progress.min.css';
 import '@/assets/css/royal-canin/royal-canin.styles.rc_type-medium.min.css';
 import '@/assets/css/royal-canin/royal-canin.styles.rc_type-regular.min.css';
-import '@/assets/css/royal-canin/royal-canin.styles.selects.min.css';
-import '@/assets/css/royal-canin/royal-canin.styles.tooltip.min.css';
+// import '@/assets/css/royal-canin/royal-canin.styles.selects.min.css';
+// import '@/assets/css/royal-canin/royal-canin.styles.tooltip.min.css';
 
 // import App from './App.jsx';
 
