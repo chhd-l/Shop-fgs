6aef6fe5c92d68db6017a2f57f5df48c8be85416
tyyyyou <mytang11@163.com>
2022-01-04 14:32:43 +0800

    容错

--- src/lang/index.js
+++ src/lang/index.js
@@ -43,16 +43,16 @@ async function getDynamicLanguage() {
     phraseRet = JSON.parse(phraseSession);
   } else {
     const url = `https://api.phrase.com/v2/projects/8f0d7f6b0396b8af7f08bf9f36d81259/locales/${key}/download?access_token=31950e3e49b165b8b2c604b65574e6cf279d9ea395e3718ce52b1ec335bef6e5&include_empty_translations=true&file_format=node_json`;
-    const resJson = await fetch(url, {
-      method: 'get',
-      mode: 'cors',
-      cache: 'no-store',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      }
-    });
     try {
+      const resJson = await fetch(url, {
+        method: 'get',
+        mode: 'cors',
+        cache: 'no-store',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
       const res = await resJson.json();
       sessionItemRoyal.set('PHRASE_LANGUAGE', JSON.stringify(res));
       phraseRet = res;
