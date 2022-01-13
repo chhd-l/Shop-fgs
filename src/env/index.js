6b6d8496036ba4ba2da7f19f210360dcb3104484
tyyyyou <mytang11@163.com>
2022-01-13 13:31:11 +0800

    获取各国家基础配置信息

--- src/env/index.js
+++ src/env/index.js
@@ -1,27 +1,5 @@
 import { fetchDynamicConfig } from '@/framework/common';
-import ca from '@/env/ca';
-import core from '@/env/core';
-import de from '@/env/de';
-import fr from '@/env/fr';
-import mx from '@/env/mx';
-import ru from '@/env/ru';
-import se from '@/env/se';
-import tr from '@/env/tr';
-import uk from '@/env/uk';
-import us from '@/env/us';
 
 export const initShopConfig = async () => {
   return await fetchDynamicConfig();
-};
-export const baseStoreConfigByCountry = {
-  ca,
-  core,
-  de,
-  fr,
-  mx,
-  ru,
-  se,
-  tr,
-  uk,
-  us
 };
