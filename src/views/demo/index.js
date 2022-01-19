bb0dad70df9a0cf5f2a944f7a3cebc8da7d44a71
lizongji <lizj@belink.com>
2022-01-19 13:49:12 +0800

    update image

--- src/views/demo/index.js
+++ src/views/demo/index.js
@@ -1,222 +1,247 @@
-import React, { Component } from 'react';
+import React, { useRef, useState } from 'react';
 import Slider from 'react-slick';
 import { SideBySideMagnifier } from 'react-image-magnifiers';
 import './index.less';
-export default class AsNavFor extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataList: [
-        {
-          imageId: 381892,
-          goodsId: '2c918085768f3a4101768f3e1e9a0029',
-          imageType: 'master',
-          goodsInfoId: null,
-          artworkUrl:
-            'https://d2cstgstorage.z13.web.core.windows.net/FR_129171_master.jpg',
-          middleUrl: null,
-          thumbUrl: null,
-          bigUrl: null,
-          createTime: '2022-01-13 08:26:01.000',
-          updateTime: '2022-01-13 08:26:01.000',
-          delFlag: 0
-        },
-        {
-          imageId: 381893,
-          goodsId: '2c918085768f3a4101768f3e1e9a0029',
-          imageType: 'other',
-          goodsInfoId: null,
-          artworkUrl:
-            'https://d2cstgstorage.z13.web.core.windows.net/FR_129171_other_328049.jpg',
-          middleUrl: null,
-          thumbUrl: null,
-          bigUrl: null,
-          createTime: '2022-01-13 08:26:01.000',
-          updateTime: '2022-01-13 08:26:01.000',
-          delFlag: 0
-        },
-        {
-          imageId: 381894,
-          goodsId: '2c918085768f3a4101768f3e1e9a0029',
-          imageType: 'hero',
-          goodsInfoId: null,
-          artworkUrl:
-            'https://d2cstgstorage.z13.web.core.windows.net/FR_129171_hero_508324.jpg',
-          middleUrl: null,
-          thumbUrl: null,
-          bigUrl: null,
-          createTime: '2022-01-13 08:26:01.000',
-          updateTime: '2022-01-13 08:26:01.000',
-          delFlag: 0
-        },
-        {
-          imageId: 381895,
-          goodsId: '2c918085768f3a4101768f3e1e9a0029',
-          imageType: 'other',
-          goodsInfoId: null,
-          artworkUrl:
-            'https://d2cstgstorage.z13.web.core.windows.net/FR_129171_other_508353.jpg',
-          middleUrl: null,
-          thumbUrl: null,
-          bigUrl: null,
-          createTime: '2022-01-13 08:26:01.000',
-          updateTime: '2022-01-13 08:26:01.000',
-          delFlag: 0
-        },
-        {
-          imageId: 381896,
-          goodsId: '2c918085768f3a4101768f3e1e9a0029',
-          imageType: 'bag',
-          goodsInfoId: null,
-          artworkUrl:
-            'https://d2cstgstorage.z13.web.core.windows.net/FR_129171_bag_498159.jpg',
-          middleUrl: null,
-          thumbUrl: null,
-          bigUrl: null,
-          createTime: '2022-01-13 08:26:01.000',
-          updateTime: '2022-01-13 08:26:01.000',
-          delFlag: 0
-        },
-        {
-          imageId: 381897,
-          goodsId: '2c918085768f3a4101768f3e1e9a0029',
-          imageType: 'other',
-          goodsInfoId: null,
-          artworkUrl:
-            'https://d2cstgstorage.z13.web.core.windows.net/FR_129171_other_633479.jpg',
-          middleUrl: null,
-          thumbUrl: null,
-          bigUrl: null,
-          createTime: '2022-01-13 08:26:01.000',
-          updateTime: '2022-01-13 08:26:01.000',
-          delFlag: 0
-        },
-        {
-          imageId: 381898,
-          goodsId: '2c918085768f3a4101768f3e1e9a0029',
-          imageType: 'kibble',
-          goodsInfoId: null,
-          artworkUrl:
-            'https://d2cstgstorage.z13.web.core.windows.net/FR_129171_kibble_328050.jpg',
-          middleUrl: null,
-          thumbUrl: null,
-          bigUrl: null,
-          createTime: '2022-01-13 08:26:01.000',
-          updateTime: '2022-01-13 08:26:01.000',
-          delFlag: 0
-        }
-      ]
-    };
-  }
+import ImageMagnifier from './test';
 
-  componentDidMount() {
-    this.setState({});
-  }
-  navToSlide = (idx) => {
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={`${className} d-none d-md-block rc-carousel__direction rc-carousel__direction--next iconfont font-weight-bold icon-direction ui-cursor-pointer`}
+      style={{
+        ...style,
+        right: '-5%',
+        zIndex: 1,
+        top: '50%',
+        position: 'absolute',
+        transform: 'translateY(-50%)'
+      }}
+      onClick={onClick}
+    >
+      <span className="iconjiantouyou1 iconfont rc-text-colour--text font-weight-bold ui-cursor-pointer" />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={`${className} d-none d-md-block rc-carousel__direction rc-carousel__direction--prev icon-direction ui-cursor-pointer`}
+      style={{
+        ...style,
+        left: '-5%',
+        zIndex: 1,
+        top: '50%',
+        position: 'absolute',
+        transform: 'translateY(-50%)'
+      }}
+      onClick={onClick}
+    >
+      <span className="iconjiantouzuo1 iconfont rc-text-colour--text font-weight-bold ui-cursor-pointer" />
+    </div>
+  );
+}
+const AsNavFor = (props) => {
+  const dataList = [
+    {
+      imageId: 381892,
+      goodsId: '2c918085768f3a4101768f3e1e9a0029',
+      imageType: 'master',
+      goodsInfoId: null,
+      artworkUrl:
+        'https://d2cstgstorage.z13.web.core.windows.net/FR_129171_master.jpg',
+      middleUrl: null,
+      thumbUrl: null,
+      bigUrl: null,
+      createTime: '2022-01-13 08:26:01.000',
+      updateTime: '2022-01-13 08:26:01.000',
+      delFlag: 0
+    },
+    {
+      imageId: 381893,
+      goodsId: '2c918085768f3a4101768f3e1e9a0029',
+      imageType: 'other',
+      goodsInfoId: null,
+      artworkUrl:
+        'https://d2cstgstorage.z13.web.core.windows.net/FR_129171_other_328049.jpg',
+      middleUrl: null,
+      thumbUrl: null,
+      bigUrl: null,
+      createTime: '2022-01-13 08:26:01.000',
+      updateTime: '2022-01-13 08:26:01.000',
+      delFlag: 0
+    },
+    {
+      imageId: 381894,
+      goodsId: '2c918085768f3a4101768f3e1e9a0029',
+      imageType: 'hero',
+      goodsInfoId: null,
+      artworkUrl:
+        'https://d2cstgstorage.z13.web.core.windows.net/FR_129171_hero_508324.jpg',
+      middleUrl: null,
+      thumbUrl: null,
+      bigUrl: null,
+      createTime: '2022-01-13 08:26:01.000',
+      updateTime: '2022-01-13 08:26:01.000',
+      delFlag: 0
+    },
+    {
+      imageId: 381895,
+      goodsId: '2c918085768f3a4101768f3e1e9a0029',
+      imageType: 'other',
+      goodsInfoId: null,
+      artworkUrl:
+        'https://d2cstgstorage.z13.web.core.windows.net/FR_129171_other_508353.jpg',
+      middleUrl: null,
+      thumbUrl: null,
+      bigUrl: null,
+      createTime: '2022-01-13 08:26:01.000',
+      updateTime: '2022-01-13 08:26:01.000',
+      delFlag: 0
+    },
+    {
+      imageId: 381896,
+      goodsId: '2c918085768f3a4101768f3e1e9a0029',
+      imageType: 'bag',
+      goodsInfoId: null,
+      artworkUrl:
+        'https://d2cstgstorage.z13.web.core.windows.net/FR_129171_bag_498159.jpg',
+      middleUrl: null,
+      thumbUrl: null,
+      bigUrl: null,
+      createTime: '2022-01-13 08:26:01.000',
+      updateTime: '2022-01-13 08:26:01.000',
+      delFlag: 0
+    },
+    {
+      imageId: 381897,
+      goodsId: '2c918085768f3a4101768f3e1e9a0029',
+      imageType: 'other',
+      goodsInfoId: null,
+      artworkUrl:
+        'https://d2cstgstorage.z13.web.core.windows.net/FR_129171_other_633479.jpg',
+      middleUrl: null,
+      thumbUrl: null,
+      bigUrl: null,
+      createTime: '2022-01-13 08:26:01.000',
+      updateTime: '2022-01-13 08:26:01.000',
+      delFlag: 0
+    },
+    {
+      imageId: 381898,
+      goodsId: '2c918085768f3a4101768f3e1e9a0029',
+      imageType: 'kibble',
+      goodsInfoId: null,
+      artworkUrl:
+        'https://d2cstgstorage.z13.web.core.windows.net/FR_129171_kibble_328050.jpg',
+      middleUrl: null,
+      thumbUrl: null,
+      bigUrl: null,
+      createTime: '2022-01-13 08:26:01.000',
+      updateTime: '2022-01-13 08:26:01.000',
+      delFlag: 0
+    }
+  ];
+  const [currentSliderSize, setCurrentSliderSize] = useState({});
+  const [idx, setIdx] = useState(0);
+  const switchSides = false;
+  const sliderImageMagnifierBox = useRef({});
+  const sliderRef = useRef({});
+  const navToSlide = (idx) => {
     console.info('idx', idx);
-    this.slider.slickGoTo(idx);
+    setIdx(idx);
+    sliderRef.current.slickGoTo(idx);
+    let currentSlider = document.querySelectorAll('.slick-list img')[idx];
+    let width = currentSlider.clientWidth;
+    let height = currentSlider.clientHeight;
+    setCurrentSliderSize({ width, height });
   };
-
-  render() {
-    let switchSides = false;
-    return (
+  return (
+    <div ref={sliderImageMagnifierBox}>
       <div className="demo-css">
-        <Slider
-          ref={(slider) => (this.slider = slider)}
-          swipeToSlide={false}
-          arrows={false}
-          touchMove={false}
-          swipe={false}
-          infinite={false}
-        >
-          {this.state.dataList.map((item, idx) => (
-            <div>
-              <h3
-                className="current-img flex items-center justify-center"
-                onMouseOver={() => {
-                  console.info('........');
-                }}
-              >
-                <SideBySideMagnifier
-                  className="input-position"
-                  style={{ order: switchSides ? '1' : '0' }}
-                  imageSrc={item.artworkUrl}
-                  // imageSrc='https://d2cstgstorage.z13.web.core.windows.net/FR_129171_master.jpg'
-                  // largeImageSrc='https://d2cstgstorage.z13.web.core.windows.net/FR_129171_master.jpg'
-                  alwaysInPlace={false}
-                  overlayBackgroundColor="transparent"
-                  overlayBoxOpacity={0.7}
-                  overlayBoxColor="#000"
-                  switchSides={switchSides}
-                  zoomPosition="left"
-                  overlayBoxImageSize=""
-                  inPlaceMinBreakpoint={400}
-                  fillAvailableSpace={false}
-                  fillAlignTop={false}
-                  fillGapTop={10}
-                  fillGapRight={10}
-                  fillGapBottom={10}
-                  fillGapLeft={0}
-                  zoomContainerBorder="1px solid #ccc"
-                  zoomContainerBoxShadow="0 4px 8px rgba(0,0,0,.5)"
-                />
-                {/* <img src={item.artworkUrl} /> */}
-              </h3>
-            </div>
-          ))}
-        </Slider>
+        <div className="relative">
+          <Slider
+            ref={(slider) => (sliderRef.current = slider)}
+            swipeToSlide={false}
+            arrows={false}
+            touchMove={false}
+            swipe={false}
+            infinite={false}
+          >
+            {dataList.map((item, index) => (
+              <img src={item.artworkUrl} />
+            ))}
+          </Slider>
+          <div
+            className="absolute"
+            style={{
+              top: 0,
+              marginLeft: '50%',
+              transform: 'translate(-50%)',
+              zIndex: 999
+            }}
+          >
+            <ImageMagnifier
+              minImg={dataList[idx].artworkUrl}
+              magnifierContainerLeft={
+                sliderImageMagnifierBox.current.clientWidth
+              }
+              currentSliderSize={currentSliderSize}
+              maxImg={dataList[idx].artworkUrl}
+            />
+          </div>
+        </div>
         <Slider
           slidesToShow={5}
           swipeToSlide={false}
           arrows={true}
           infinite={false}
+          nextArrow={<SampleNextArrow />}
+          prevArrow={<SamplePrevArrow />}
         >
-          {this.state.dataList.map((item, idx) => (
+          {dataList.map((item, index) => (
             <div
-              onClick={() => {
-                this.navToSlide(idx);
+              className={`slider-item`}
+              onMouseOver={() => {
+                navToSlide(index);
               }}
             >
-              <h3
-                className="pic-item"
-                style={{
-                  height: 0,
-                  paddingTop: '100%',
-                  backgroundImage: `url('${item.artworkUrl}')`,
-                  backgroundRepeat: 'no-repeat',
-                  backgroundSize: '100%'
-                }}
-              >
-                {/* <img src={item.artworkUrl}/> */}
-              </h3>
+              <div className={`pic-item-box ${index == idx ? 'active' : ''}`}>
+                <div
+                  className={`pic-item`}
+                  style={{
+                    height: 0,
+                    paddingTop: '100%',
+                    backgroundImage: `url('${item.artworkUrl}')`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: '100%'
+                  }}
+                ></div>
+              </div>
             </div>
           ))}
         </Slider>
-
-        <br />
-        <SideBySideMagnifier
-          className="input-position"
-          style={{ order: switchSides ? '1' : '0' }}
-          imageSrc="https://d2cstgstorage.z13.web.core.windows.net/FR_129171_master.jpg"
-          // largeImageSrc='https://d2cstgstorage.z13.web.core.windows.net/FR_129171_master.jpg'
-          alwaysInPlace={false}
-          overlayBackgroundColor="transparent"
-          overlayBoxOpacity={0.7}
-          overlayBoxColor="#000"
-          switchSides={switchSides}
-          zoomPosition="left"
-          overlayBoxImageSize=""
-          inPlaceMinBreakpoint={400}
-          fillAvailableSpace={false}
-          fillAlignTop={false}
-          fillGapTop={10}
-          fillGapRight={10}
-          fillGapBottom={10}
-          fillGapLeft={0}
-          zoomContainerBorder="1px solid #ccc"
-          zoomContainerBoxShadow="0 4px 8px rgba(0,0,0,.5)"
-        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+export default AsNavFor;
+// export default class AsNavFor extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.setState({});
+//   }
+
+//   render() {
+//     let switchSides = false;
+
+//   }
+// }
