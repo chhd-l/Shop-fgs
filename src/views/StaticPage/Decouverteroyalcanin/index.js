07f60f469100f86285a1c1c1cc1e0a6159d9b888
lizongji <lizj@belink.com>
2022-01-20 01:25:07 +0800

    landing page

--- src/views/StaticPage/Decouverteroyalcanin/index.js
+++ src/views/StaticPage/Decouverteroyalcanin/index.js
@@ -4,6 +4,13 @@ import GoogleTagManager from '@/components/GoogleTagManager';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { setSeoConfig } from '@/utils/utils';
+import LazyLoad from 'react-lazyload';
+import pic1 from './images/pic-1.png';
+import pic2 from './images/pic-2.png';
+import pic3 from './images/pic-3.png';
+import pic4 from './images/pic-4.png';
+import pic5 from './images/pic-5.png';
+import pic6 from './images/pic-6.png';
 import './index.less';
 const pageLink = window.location.href;
 const HrLine = () => (
@@ -28,6 +35,9 @@ const Decouverteroyalcanin = (props) => {
       setSeoinfo(res);
     });
   });
+  const redirectLink = (link) => {
+    location.href = link;
+  };
   return (
     <div>
       <GoogleTagManager additionalEvents={event} />
@@ -39,10 +49,13 @@ const Decouverteroyalcanin = (props) => {
       </Helmet>
       <Header {...props} showMiniIcons={true} showUserIcon={true} />
       <div className="rc-content--fixed-header rc-bg-colour--brand3 decouverteroyalcanin">
-        <div className="rc-layout-container rc-two-column  rc-max-width--xl rc-padding-left--md items-center">
+        <div
+          className="rc-layout-container rc-two-column  rc-max-width--xl rc-padding-left--md
+        rc-padding-top--md rc-padding-bottom--xl items-center"
+        >
           <div className="rc-column">
             <div style={{ maxWidth: '504px' }} className="m-auto">
-              <h1 className="xs: text-3xl lg:text-5xl  rc-padding-bottom--md">
+              <h1 className="xs: text-3xl lg:text-5xl  rc-padding-bottom--sm">
                 Venez à la découverte de Royal Canin
               </h1>
               <p className="xs:text-base lg:text-xl">
@@ -55,64 +68,71 @@ const Decouverteroyalcanin = (props) => {
             </div>
           </div>
           <div className="rc-column">
-            <img
-              src={`${window.__.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/static-page/decouverteroyalcanin/pic1.png`}
-            />
+            <LazyLoad>
+              <img
+                src={pic1}
+                // src={`${window.__.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/static-page/decouverteroyalcanin/pic1.png`}
+              />
+            </LazyLoad>
           </div>
         </div>
 
         <div className="rc-layout-container rc-two-column rc-padding-x--xl  rc-max-width--xl items-center">
           <div className="rc-column">
-            <h2 className="xs:text-xl lg:text-3xl rc-padding-bottom--md">
+            <h2 className="xs:text-xl lg:text-3xl rc-padding-bottom--sm">
               Expériences incroyables
             </h2>
             <p className="rc-padding-bottom--sm">
-              Incroyable Aventure : venez découvrir l’histoire et le patrimoine
-              de Royal Canin avec une frise chronologique numérique innovante et
-              interactive.{' '}
+              <span className="font-semibold">Incroyable Aventure </span>: venez
+              découvrir l’histoire et le patrimoine de Royal Canin avec une
+              frise chronologique numérique innovante et interactive.{' '}
             </p>
-            <p className="rc-padding-bottom--xs rc-padding-top--xs">
-              Incroyables Amis : participez à un quizz ludique pour mieux
-              comprendre le comportement de vos chats et chiens.
+            <p className="rc-padding-bottom--sm">
+              <span className="font-semibold">Incroyables Amis</span> :
+              participez à un quizz ludique pour mieux comprendre le
+              comportement de vos chats et chiens.
             </p>
-            <p className="rc-padding-bottom--xs rc-padding-top--xs">
-              Incroyable Sens : découvrez comment vos animaux perçoivent le
-              monde, comment ils le sentent, le voient et l’entendent.
-              Plongez-vous dans une pièce immersive qui avec des écrans incurvés
-              nouvelle génération, des bancs chauffants et vibrants, vous
-              permettra de ressentir ce que ressentent vos animaux.
+            <p>
+              <span className="font-semibold">Incroyable Sens</span> : découvrez
+              comment vos animaux perçoivent le monde, comment ils le sentent,
+              le voient et l’entendent. Plongez-vous dans une pièce immersive
+              qui avec des écrans incurvés nouvelle génération, des bancs
+              chauffants et vibrants, vous permettra de ressentir ce que
+              ressentent vos animaux.
             </p>
           </div>
           <div className="rc-column">
-            <img
-              src={`${window.__.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/static-page/decouverteroyalcanin/pic2.png`}
-            />
+            <LazyLoad>
+              <img
+                src={pic2}
+
+                // src={`${window.__.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/static-page/decouverteroyalcanin/pic2.png`}
+              />
+            </LazyLoad>
           </div>
         </div>
-        <HrLine />
-
-        <div className="rc-layout-container rc-one-column rc-max-width--xl items-center rc-padding-y--xl  rc-padding-x--xl">
-          <div class="rc-column">
-            <p>
-              Incroyable Aventure : venez découvrir l’histoire et le patrimoine
-              de Royal Canin avec une frise chronologique numérique innovante et
-              interactive.
+        <div className="rc-layout-container rc-one-column rc-max-width--xl items-center rc-padding-bottom--lg  rc-padding-x--xl">
+          <div className="rc-column">
+            <p className="rc-padding-bottom--sm">
+              <span className="font-semibold">Incroyable Aventure</span> : venez
+              découvrir l’histoire et le patrimoine de Royal Canin avec une
+              frise chronologique numérique innovante et interactive.
             </p>
-            <br />
-            <p>
-              Incroyables Amis : participez à un quizz ludique pour mieux
-              comprendre le comportement de vos chats et chiens.
+            <p className="rc-padding-bottom--sm">
+              <span className="font-semibold">Incroyables Amis </span>:
+              participez à un quizz ludique pour mieux comprendre le
+              comportement de vos chats et chiens.
             </p>
-            <br />
-            <p>
-              Incroyable Sens : découvrez comment vos animaux perçoivent le
-              monde, comment ils le sentent, le voient et l’entendent.
-              Plongez-vous dans une pièce immersive qui avec des écrans incurvés
-              nouvelle génération, des bancs chauffants et vibrants, vous
-              permettra de ressentir ce que ressentent vos animaux.
+            <p className="rc-padding-bottom--xs">
+              <span className="font-semibold">Incroyable Sens</span> : découvrez
+              comment vos animaux perçoivent le monde, comment ils le sentent,
+              le voient et l’entendent. Plongez-vous dans une pièce immersive
+              qui avec des écrans incurvés nouvelle génération, des bancs
+              chauffants et vibrants, vous permettra de ressentir ce que
+              ressentent vos animaux.
             </p>
             <p className="rc-padding-top--md rc-padding-bottom--xs">
-              <button class="rc-btn rc-btn--two">Nous rendre visite</button>
+              <button className="rc-btn rc-btn--two">Nous rendre visite</button>
             </p>
             <p>
               Ouverture le 24 Janvier 2022
@@ -125,15 +145,19 @@ const Decouverteroyalcanin = (props) => {
         </div>
         <HrLine />
 
-        <div className="rc-layout-container rc-two-column  rc-max-width--xl  rc-padding-x--xl itmes-center rc-padding-y--xl">
+        <div className="rc-layout-container rc-two-column  rc-max-width--xl  rc-padding-x--xl items-center rc-padding-y--xl">
           <div className="rc-column">
-            <img
-              src={`${window.__.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/static-page/decouverteroyalcanin/pic3.png`}
-            />
+            <LazyLoad>
+              <img
+                src={pic3}
+
+                // src={`${window.__.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/static-page/decouverteroyalcanin/pic3.png`}
+              />
+            </LazyLoad>
           </div>
           <div className="rc-column">
             <div style={{ maxWidth: '450px' }} className="m-auto">
-              <h2 className="xs:text-xl lg:text-3xl rc-padding-bottom--md">
+              <h2 className="xs:text-xl lg:text-3xl rc-padding-bottom--sm">
                 A la découverte des expériences Royal Canin
               </h2>
               <p>
@@ -148,70 +172,99 @@ const Decouverteroyalcanin = (props) => {
         </div>
         <HrLine />
 
-        <div className="rc-layout-container rc-two-column  rc-max-width--xl  rc-padding-x--xl itmes-center rc-padding-y--xl">
+        <div className="rc-layout-container rc-two-column  rc-max-width--xl  rc-padding-x--xl items-center rc-padding-y--xl">
           <div className="rc-column">
             <div style={{ maxWidth: '450px' }}>
-              <h2 className="xs:text-xl lg:text-3xl rc-padding-bottom--md">
+              <h2 className="xs:text-xl lg:text-3xl rc-padding-bottom--sm">
                 Venez écouter et échanger avec nos experts sur tous les sujets
                 autour des chiens et chats
               </h2>
-              <p className="rc-padding-bottom--md">
+              <p className="rc-padding-bottom--sm">
                 Royal Canin a toujours quelque chose de différent et
                 d’instructif à proposer. Des ateliers indoor, des activités en
                 extérieur, des rencontres pilotées par des experts et bien plus
                 encore.
               </p>
-              <button class="rc-btn rc-btn--two">Réservez vite</button>
+              <button
+                className="rc-btn rc-btn--two"
+                onClick={() =>
+                  redirectLink(
+                    'https://www.eventbrite.com/cc/incroyables-experts-85279?utm-campaign=social&utm-content=creatorshare&utm-medium=discovery&utm-term=odclsxcollection&utm-source=cp&aff=escb'
+                  )
+                }
+              >
+                Réservez vite
+              </button>
             </div>
           </div>
           <div className="rc-column">
-            <img
-              src={`${window.__.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/static-page/decouverteroyalcanin/pic4.png`}
-            />
+            <LazyLoad>
+              <img
+                src={pic4}
+
+                // src={`${window.__.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/static-page/decouverteroyalcanin/pic4.png`}
+              />
+            </LazyLoad>
           </div>
         </div>
         <HrLine />
 
-        <div className="rc-layout-container rc-two-column  rc-max-width--xl rc-padding-x--xl rc-padding-y--xl itmes-center">
+        <div className="rc-layout-container rc-two-column  rc-max-width--xl rc-padding-x--xl rc-padding-top--xl rc-padding-bottom--lg itmes-center">
           <div className="rc-column">
-            <img
-              src={`${window.__.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/static-page/decouverteroyalcanin/pic5.png`}
-            />
+            <LazyLoad>
+              <img
+                src={pic5}
+
+                // src={`${window.__.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/static-page/decouverteroyalcanin/pic5.png`}
+              />
+            </LazyLoad>
           </div>
           <div className="rc-column">
             <div style={{ maxWidth: '450px' }}>
-              <h2 className="xs:text-xl lg:text-3xl rc-padding-bottom--md">
+              <h2 className="xs:text-xl lg:text-3xl rc-padding-bottom--sm">
                 Incroyables moments : une séance photo professionnelle avec
                 votre animal
               </h2>
-              <p className="rc-padding-bottom--md">
+              <p className="rc-padding-bottom--sm">
                 Offrez vous une séance photo avec un photographe professionnel
                 pour immortaliser votre relation avec votre animal.
               </p>
-              <button class="rc-btn rc-btn--two">Inscrivez-vous </button>
+              <button
+                className="rc-btn rc-btn--two"
+                onClick={() =>
+                  redirectLink(
+                    'https://www.eventbrite.com/cc/incroyables-moments-85499?utm-campaign=social&utm-content=creatorshare&utm-medium=discovery&utm-term=odclsxcollection&utm-source=cp&aff=escb'
+                  )
+                }
+              >
+                Inscrivez-vous{' '}
+              </button>
             </div>
           </div>
         </div>
         <HrLine />
 
-        <div className="rc-layout-container rc-one-column  rc-max-width--xl  rc-padding-top--md  rc-padding-bottom--xl   rc-padding-x--xl justify-center flex-col">
-          <h2 className="xs:text-xl lg:text-3xl rc-padding-bottom--md">
-            Venez à la découverte de Royal Canin
-          </h2>
-          <br />
-          <h4 className="xs:text-base lg:text-xl rc-padding-bottom--md">
-            Où nous trouver
-          </h4>
-          <br />
+        <div className="rc-layout-container rc-one-column  rc-max-width--xl  rc-padding-top--md  rc-padding-bottom--xl   rc-padding-x--xl ">
+          <div className="text-center m-auto">
+            <h2 className="xs:text-xl lg:text-3xl rc-padding-y--xs">
+              Venez à la découverte de Royal Canin
+            </h2>
+            <h4 className="xs:text-base lg:text-xl rc-padding-y--xs">
+              Où nous trouver
+            </h4>
+            <p className="rc-padding-top--xs">Ouverture le 24 janvier 2022</p>
 
-          <p>Ouverture le 24 janvier 2022</p>
+            <p>126, rue de Turenne, 75003 Paris</p>
 
-          <p>126, rue de Turenne, 75003 Paris</p>
+            <p className="rc-padding-bottom--sm">Métro : Filles du Calvaire</p>
+            <LazyLoad>
+              <img
+                src={pic6}
 
-          <p>Métro : Filles du Calvaire</p>
-          <img
-            src={`${window.__.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/static-page/decouverteroyalcanin/pic6.png`}
-          />
+                // src={`${window.__.env.REACT_APP_EXTERNAL_ASSETS_PREFIX}/img/static-page/decouverteroyalcanin/pic6.png`}
+              />
+            </LazyLoad>
+          </div>
         </div>
       </div>
       <Footer />
