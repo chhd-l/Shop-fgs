388aa36cac9390b4dcc0a0183acb990c2a6cef97
lizongji <lizj@belink.com>
2022-01-24 16:24:05 +0800

    'adoptions'

--- src/views/Adoptions/index.js
+++ src/views/Adoptions/index.js
@@ -20,15 +20,17 @@ import { getShelterList } from '@/api/recommendation';
 import { getDetails, getLoginDetails } from '@/api/details';
 import { getFrequencyDict } from '@/utils/utils';
 let goodsInfoNosObj = {
-  8172118196590126: 'Kitten <br/> (3-12 months)',
-  8172256907894156: 'Adult Cat<br/> (1+ years)',
-  493013: 'Small Puppy <br/> (3-12 months)',
-  493817: 'Medium Puppy <br/>(3-12 months)',
-  492818: 'Large Puppy <br/>(3-12 months)',
-  512514: 'Small Adult Dog<br/> (1+ years)',
-  517417: 'Medium Adult Dog<br/> (1+ years)',
-  517906: 'Large Adult Dog <br/>(1+ years)'
+  8172118196590126: ['Kitten <br/>> (3-12 months)'],
+  8172256907894156: ['Adult Cat<br/> (1+ years)'],
+  493013: ['Small Puppy <br/> (3-12 months)'],
+  493817: ['Medium Puppy<br/> (3-12 months)'],
+  492818: ['Large Puppy<br/> (3-12 months)'],
+  512514: ['Small Adult Dog<br/> (1+ years)', '9 to 22lbs'],
+  517417: ['Medium Adult Dog<br/> (1+ years)', '22 to 55lbs'],
+  517906: ['Large Adult Dog<br/> (1+ years)', '56 to 100lbs']
 };
+const sessionItemRoyal = window.__.sessionItemRoyal;
+
 const Adoptions = (props) => {
   const { loginStore, paymentStore, checkoutStore, configStore } =
     useLocalStore(() => stores);
@@ -54,9 +56,11 @@ const Adoptions = (props) => {
         };
       });
     setShelterList(list);
+    let choosedShelter = sessionItemRoyal.get('handled-shelter');
+    let data = list.find((el) => el.value == choosedShelter);
+    data && setShelter(data);
   };
   const addCart = async (product) => {
-    console.info('product', product);
     if (
       !shelter.value ||
       !product.goodsInfo?.goodsInfoId ||
@@ -198,23 +202,26 @@ const Adoptions = (props) => {
       goodsInfoNos
     });
     let goodsLists = res.context.esGoodsPage?.content;
+    let sortList = [];
     // 找出sku并放到goodsInfo上
     goodsInfoNos.forEach((id) => {
       goodsLists.forEach((el) => {
         let goodsInfo = el.goodsInfos.find((info) => info.goodsInfoNo == id);
         if (goodsInfo) {
-          el.goodsNameStr = goodsInfoNosObj[id];
+          el.goodsNameStr = goodsInfoNosObj[id][0];
+          el.weightInfo = goodsInfoNosObj[id][1] || '';
           el.goodsInfo = goodsInfo;
+          sortList.push(el);
         }
       });
     });
     // 查出的其他数据不应该被展示
-    let list = goodsLists.filter((el) => el.goodsInfo);
-    console.info('list', list);
+    let list = sortList.filter((el) => el.goodsInfo);
     setGoodsList(list);
   };
   const handleSelectChange = (data) => {
     setShelter(data);
+    sessionItemRoyal.set('handled-shelter', data.value);
   };
   const GAShelterLPdropdownClick = () => {
     dataLayer.push({
@@ -304,7 +311,7 @@ const Adoptions = (props) => {
                     style={{
                       color: '#444444',
                       fontSize: '14px',
-                      fontWeight: '500'
+                      fontWeight: 'bold'
                     }}
                   >
                     Select your shelter
@@ -317,6 +324,7 @@ const Adoptions = (props) => {
                     selectedItemData={{
                       value: shelter.value
                     }}
+                    key={shelter.value}
                     placeholder="Please select..."
                   />
                   <p
@@ -367,6 +375,12 @@ const Adoptions = (props) => {
                         __html: item.goodsNameStr
                       }}
                     ></div>
+                    <p
+                      style={{ height: '40px', marginBottom: '0.5rem' }}
+                      className="flex items-center justify-center"
+                    >
+                      {item.weightInfo}
+                    </p>
                     <button
                       onClick={() => addCart(item)}
                       class={`rc-btn rc-btn--two ${
