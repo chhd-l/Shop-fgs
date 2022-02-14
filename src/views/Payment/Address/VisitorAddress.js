6b633d1ca1420c7efde8522dec4fc162b4527a9d
tutu <1515112287@qq.com>
2022-02-14 11:31:19 +0800

    improve the analytics of the checkout page for RU

--- src/views/Payment/Address/VisitorAddress.js
+++ src/views/Payment/Address/VisitorAddress.js
@@ -619,6 +619,7 @@ class VisitorAddress extends React.Component {
         getFormAddressValidFlag={this.getFormAddressValidFlag}
         updateData={this.updateDeliveryAddress}
         calculateFreight={this.calculateFreight}
+        {...this.props}
       />
     );
     const _title = panelStatus.isPrepare
--- src/views/Payment/index.js
+++ src/views/Payment/index.js
@@ -2765,6 +2765,24 @@ class Payment extends React.Component {
               calculateFreight={this.calculateFreight}
               cartData={this.computedCartData}
               isLogin={false}
+              onSearchSelectionChange={() =>
+                window.__.env.REACT_APP_COUNTRY === 'ru' &&
+                window?.dataLayer?.push({
+                  event: 'suggestedAdressInteraction',
+                  suggestedAdress: {
+                    action: 'suggestionClick'
+                  }
+                })
+              }
+              onSearchSelectionFocus={() =>
+                window.__.env.REACT_APP_COUNTRY === 'ru' &&
+                window?.dataLayer?.push({
+                  event: 'suggestedAdressInteraction',
+                  suggestedAdress: {
+                    action: 'fieldClick'
+                  }
+                })
+              }
             />
           )}
         </div>
