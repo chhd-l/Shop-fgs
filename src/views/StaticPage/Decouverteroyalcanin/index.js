4a14b419324c992871646b41522c3d4f5c5b3d13
zuoqin <qzuo@deloitte.com.cn>
2022-03-09 11:26:30 +0800

    css update

--- src/views/StaticPage/Decouverteroyalcanin/index.js
+++ src/views/StaticPage/Decouverteroyalcanin/index.js
@@ -11,7 +11,6 @@ import pic3 from './images/pic-03.jpg';
 import pic4 from './images/pic-04.jpg';
 import pic5 from './images/pic-05.jpg';
 import pic6 from './images/pic-06.jpg';
-// import './index.less';
 const pageLink = window.location.href;
 
 const HrLine = () => (
