d0cd8a567289228f414f76e25054b7d222d12ecd
kevin <qhx717@qq.com>
2022-03-11 16:40:58 +0800

    去掉 订阅订单的delivery date

--- src/components/Address/AddressPreview/AddressPreview.jsx
+++ src/components/Address/AddressPreview/AddressPreview.jsx
@@ -47,7 +47,8 @@ const AddressPreview = ({ configStore, data, nameCls, pickupNameCls }) => {
     consigneeName,
     firstNameKatakana,
     lastNameKatakana,
-    consigneeNumber
+    consigneeNumber,
+    showDeliveryDateAndTimeSlot = true
   } = data;
 
   /**
@@ -117,7 +118,7 @@ const AddressPreview = ({ configStore, data, nameCls, pickupNameCls }) => {
           {/* 是否存在运费 */}
           {minDeliveryTime && (
             <>
-              <p className="preview_delivery_date">
+              <p className="preview_delivery_date pickup1">
                 {minDeliveryTime == maxDeliveryTime ? (
                   <FormattedMessage
                     id="payment.deliveryDate2"
@@ -158,7 +159,7 @@ const AddressPreview = ({ configStore, data, nameCls, pickupNameCls }) => {
                     {formatMoney(calculation.deliveryPrice)}
                   </p>
                   {!timeSlot && calculation.minDeliveryTime && (
-                    <p className="preview_delivery_date">
+                    <p className="preview_delivery_date ru1">
                       {calculation.minDeliveryTime ==
                       calculation?.maxDeliveryTime ? (
                         <FormattedMessage
@@ -183,7 +184,7 @@ const AddressPreview = ({ configStore, data, nameCls, pickupNameCls }) => {
 
               {/* delivery date */}
               {newDeliveryDate && (
-                <p className="preview_delivery_date">{newDeliveryDate}</p>
+                <p className="preview_delivery_date ru2">{newDeliveryDate}</p>
               )}
 
               {/* time slot */}
@@ -201,21 +202,26 @@ const AddressPreview = ({ configStore, data, nameCls, pickupNameCls }) => {
               <span>{COUNTRY == 'jp' ? '〒' + postCode : postCode}</span>
               <p>{[province, city, area, address1].join(', ')}</p>
               <p>{consigneeNumber}</p>
-              {/* delivery date */}
-              {newDeliveryDate && (
-                <p className="preview_delivery_date">{newDeliveryDate}</p>
-              )}
-
-              {/* time slot */}
-              {timeSlot && (
-                <p className="preview_time_slot">
-                  {timeSlot == 'Unspecified' ? (
-                    <FormattedMessage id="Unspecified" />
-                  ) : (
-                    timeSlot
-                  )}
-                </p>
-              )}
+              <p className={`${showDeliveryDateAndTimeSlot ? '' : 'hidden'}`}>
+                {deliveryDate && timeSlot ? (
+                  <>
+                    {/* 格式化 delivery date 格式: 星期, 15 月份 */}
+                    {formatDate({
+                      date: deliveryDate,
+                      formatOption: {
+                        weekday: 'long',
+                        day: '2-digit',
+                        month: 'long'
+                      }
+                    })}{' '}
+                    {timeSlot == 'Unspecified' ? (
+                      <FormattedMessage id="Unspecified" />
+                    ) : (
+                      timeSlot
+                    )}
+                  </>
+                ) : null}
+              </p>
             </div>
           ) : (
             <>
