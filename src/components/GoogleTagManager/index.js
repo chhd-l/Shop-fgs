b0ccc28b6debde4bf196db7fcf6051a428676512
tutu <1515112287@qq.com>
2022-03-21 16:14:43 +0800

    bug fix:hubEvent.user not logged in

--- src/components/GoogleTagManager/index.js
+++ src/components/GoogleTagManager/index.js
@@ -157,7 +157,7 @@ class GoogleTagManager extends React.Component {
       hubEvent.user = {
         // segment: 'Not Authenticated',
         // country: window.__.env.REACT_APP_GA_COUNTRY,
-        id: ''
+        id: 'not logged in'
       };
     }
     event.user.country = window.__.env.REACT_APP_GA_COUNTRY;
