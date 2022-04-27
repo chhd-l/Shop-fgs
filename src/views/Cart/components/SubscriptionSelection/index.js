0d62ecc8454f692a4e7ab6cee64b1f95dd8f167e
leesher <784852885@qq.com>
2022-04-27 15:34:42 +0800

    upd: Change Request D2C Journey - Discounts

--- src/views/Cart/components/SubscriptionSelection/index.js
+++ src/views/Cart/components/SubscriptionSelection/index.js
@@ -14,6 +14,93 @@ const SubscriptionSelection = function (props) {
     pitem.form.frequencyId,
     toJS(pitem)
   );
+
+  // 价格有关的内容
+  let priceContent = (
+    <div className="price text-nowrap">
+      <div
+        style={{
+          fontSize: '.9375rem',
+          display: `${isGift ? 'none' : 'initial'}`
+        }}
+        className="line-through"
+      >
+        {!isLogin
+          ? formatMoney(
+              pitem.quantity *
+                pitem.sizeList.filter((el) => el.selected)[0].salePrice
+            )
+          : formatMoney(pitem.buyCount * pitem.salePrice)}
+      </div>
+
+      <div style={{ color: '#ec001a' }}>
+        {!isLogin
+          ? formatMoney(
+              pitem.quantity *
+                pitem.sizeList.filter((el) => el.selected)[0].subscriptionPrice
+            )
+          : formatMoney(
+              isGift
+                ? pitem.buyCount * pitem.settingPrice
+                : pitem.buyCount * pitem.subscriptionPrice
+            )}
+      </div>
+    </div>
+  );
+  let saveExtraMoney = (
+    <span
+      style={{
+        display: `${isGift ? 'none' : 'initial'}`
+      }}
+    >
+      <FormattedMessage
+        id="saveExtraMoney"
+        values={{
+          val: (
+            <span className="product-pricing__card__head__price red rc-padding-y--none medium">
+              {!isLogin
+                ? formatMoney(
+                    pitem.quantity *
+                      pitem.sizeList.filter((el) => el.selected)[0].salePrice -
+                      pitem.quantity *
+                        pitem.sizeList.filter((el) => el.selected)[0]
+                          .subscriptionPrice
+                  )
+                : formatMoney(
+                    pitem.buyCount * pitem.salePrice -
+                      pitem.buyCount * pitem.subscriptionPrice
+                  )}
+            </span>
+          )
+        }}
+      />
+    </span>
+  );
+  // 如果是日本 没有折扣 不显示折扣价
+  if (
+    window.__.env.REACT_APP_COUNTRY === 'jp' &&
+    pitem.salePrice === pitem.subscriptionPrice
+  ) {
+    priceContent = (
+      <div className="price text-nowrap flex flex-col justify-end">
+        <div>
+          {!isLogin
+            ? formatMoney(
+                pitem.quantity *
+                  pitem.sizeList.filter((el) => el.selected)[0]
+                    .subscriptionPrice
+              )
+            : formatMoney(
+                isGift
+                  ? pitem.buyCount * pitem.settingPrice
+                  : pitem.buyCount * pitem.subscriptionPrice
+              )}
+        </div>
+      </div>
+    );
+    saveExtraMoney = null;
+  }
+
   return (
     <div
       className="buyMethod rc-margin-bottom--xs cursor-pointer"
@@ -73,64 +160,9 @@ const SubscriptionSelection = function (props) {
             />
           </span>
           <br />
-          <span
-            style={{
-              display: `${isGift ? 'none' : 'initial'}`
-            }}
-          >
-            <FormattedMessage
-              id="saveExtraMoney"
-              values={{
-                val: (
-                  <span className="product-pricing__card__head__price red rc-padding-y--none medium">
-                    {!isLogin
-                      ? formatMoney(
-                          pitem.quantity *
-                            pitem.sizeList.filter((el) => el.selected)[0]
-                              .salePrice -
-                            pitem.quantity *
-                              pitem.sizeList.filter((el) => el.selected)[0]
-                                .subscriptionPrice
-                        )
-                      : formatMoney(
-                          pitem.buyCount * pitem.salePrice -
-                            pitem.buyCount * pitem.subscriptionPrice
-                        )}
-                  </span>
-                )
-              }}
-            />
-          </span>
+          {saveExtraMoney}
         </div>
-        <div className="price text-nowrap">
-          <div
-            style={{
-              fontSize: '.9375rem',
-              display: `${isGift ? 'none' : 'initial'}`
-            }}
-            className="line-through"
-          >
-            {!isLogin
-              ? formatMoney(
-                  pitem.quantity *
-                    pitem.sizeList.filter((el) => el.selected)[0].salePrice
-                )
-              : formatMoney(pitem.buyCount * pitem.salePrice)}
-          </div>
-          <div style={{ color: '#ec001a' }}>
-            {!isLogin
-              ? formatMoney(
-                  pitem.quantity *
-                    pitem.sizeList.filter((el) => el.selected)[0]
-                      .subscriptionPrice
-                )
-              : formatMoney(
-                  isGift
-                    ? pitem.buyCount * pitem.settingPrice
-                    : pitem.buyCount * pitem.subscriptionPrice
-                )}
-          </div>
-        </div>
+        {priceContent}
       </div>
       <FrequencySelection
         frequencyType={
