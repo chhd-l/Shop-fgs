333828d20ee7d2101f5dccc0b776497bdf89eefc
chenshuai <394988171@qq.com>
2022-05-24 15:43:26 +0800

    Revert "upd: jp menu home跳转失败buf"
    This reverts commit 2a173eba5e58b0347daf4589986a394e3d781348.

--- src/components/Header/modules/NavItem.js
+++ src/components/Header/modules/NavItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function NavItem({ item = {}, className, style = {}, children, onClick }) {
-  const isLocalHost = window.location.hostname == 'localhost';
+  console.log('NavItem', item);
   return (
     <>
       {item.href ? (
@@ -16,37 +16,26 @@ function NavItem({ item = {}, className, style = {}, children, onClick }) {
           {children}
         </a>
       ) : item.link ? (
-        item.link.pathname == '/home' && item.storeId == 123457919 ? (
-          isLocalHost ? (
-            <a
-              href={`${item.link.pathname}`}
-              className={className}
-              style={{ ...style }}
-              onClick={onClick}
-            >
-              {children}
-            </a>
-          ) : (
-            <a
-              href={`/jp${item.link.pathname}`}
-              className={className}
-              style={{ ...style }}
-              onClick={onClick}
-            >
-              {children}
-            </a>
-          )
-        ) : (
-          <Link
-            onClick={onClick}
-            to={item.link}
-            className={className}
-            style={{ ...style }}
-          >
-            {children}
-          </Link>
-        )
+        // item.link.pathname == '/home' && item.storeId== 123457919 ? (
+        //   <a
+        //     href={`/jp${item.link.pathname}`}
+        //     className={className}
+        //     style={{ ...style }}
+        //     onClick={onClick}
+        //   >
+        //     {children}
+        //   </a>
+        // ) : (
+        <Link
+          onClick={onClick}
+          to={item.link}
+          className={className}
+          style={{ ...style }}
+        >
+          {children}
+        </Link>
       ) : (
+        // )
         <span onClick={onClick} className={className} style={{ ...style }}>
           {children}
         </span>
