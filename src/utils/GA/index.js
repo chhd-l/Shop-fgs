ddba8e6b691f129647af8d38d759f18a84d3da3c
chenshuai <394988171@qq.com>
2022-05-30 15:28:08 +0800

    upd: 日本confirmation页面GA 运费取值

--- src/utils/GA/index.js
+++ src/utils/GA/index.js
@@ -608,7 +608,9 @@ export const orderConfirmationPushEvent = (details) => {
       currency: window.__.env.REACT_APP_GA_CURRENCY_CODE, //cf. https://support.google.com/analytics/answer/6205902?hl=en for complete list
       amount: details.tradePrice.totalPrice, //Transaction amount without taxes and shipping, US number format, for local currency
       taxes: details.tradePrice.taxFeePrice, //Taxes amount, US number format, local currency
-      shipping: details.tradePrice.deliveryPrice, //Shipping amount, US number format, local currency
+      shipping:
+        details.tradePrice?.deliveryPrice -
+        details.tradePrice?.freeShippingDiscountPrice, //Shipping amount, US number format, local currency
       paymentMethod: details?.paymentMethodNickName
         ? details?.paymentMethodNickName
         : 'Credit Card', //String : Payment method used for the transaction : "Credit Card", "Paypal", "Swish" (SE), "Cash on delivery" (JP), "Convenience Store" (JP)
