9c7d99a4347723f8a1e2c0c55b9bf7dd03eebb09
xiaoli <kensyang@deloitte.com.cn>
2022-06-30 09:55:40 +0800

    us 420432, Interface fault tolerance

--- src/views/Account/SubscriptionDetail/components/ChangeProduct/ChooseSKU.js
+++ src/views/Account/SubscriptionDetail/components/ChangeProduct/ChooseSKU.js
@@ -95,7 +95,7 @@ const ChooseSKU = ({ intl, configStore, ...restProps }) => {
       if (res.code === 'K-000000') {
         const { stockNotice, email } = res.context;
         setAlreadyNotice(stockNotice);
-        setUserEmail(email);
+        email && setUserEmail(email);
       }
     }
   };
