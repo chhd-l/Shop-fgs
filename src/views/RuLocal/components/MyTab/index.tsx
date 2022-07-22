d1247b4e38ec5efec95260050c033ae2221315ee
tutu <1515112287@qq.com>
2022-07-22 15:09:11 +0800

    bug fixed

--- src/views/RuLocal/components/MyTab/index.tsx
+++ src/views/RuLocal/components/MyTab/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './index.less';
 type MyTabProps = {
   tabList: {
     tab: string;
@@ -11,7 +10,7 @@ const MyTab = ({ tabList }: MyTabProps) => {
     <div>
       <nav className="rc-tabs__controller  rc-fade--x" data-toggle-group="">
         <ul
-          className="rc-scroll--x rc-list rc-list--inline rc-list--align rc-list--blank md:justify-center padding-inline-start"
+          className="rc-scroll--x rc-list rc-list--inline rc-list--align rc-list--blank md:justify-center "
           role="tablist"
         >
           {tabList.map((item, index) => (
