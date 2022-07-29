daaa4b732dcdd608335b26d8188441e802f6879a
minya <mytang11@163.com>
2022-07-29 09:31:41 +0800

    us:429622 [JP][My page]_ Account B displayed information from Account A

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -67,7 +67,11 @@ const LoginButton = ({
   // 拿到userinfo信息后，执行传入该组件的init方法
   useEffect(() => {
     // if (isGetUserInfoDown) {
-      console.log('getUserInfoDownCallback', isGetUserInfoDown, getUserInfoDownCallback);
+    console.log(
+      'getUserInfoDownCallback',
+      isGetUserInfoDown,
+      getUserInfoDownCallback
+    );
     // }
     if (isGetUserInfoDown && getUserInfoDownCallback) {
       getUserInfoDownCallback();
@@ -126,7 +130,7 @@ const LoginButton = ({
               customerId
             })
               .then(() => {
-                console.log('setIsGetUserInfoDown22')
+                console.log('setIsGetUserInfoDown22');
                 setIsGetUserInfoDown(true);
                 loginStore.changeLoginModal(false);
               })
@@ -186,7 +190,7 @@ const LoginButton = ({
                   //     customerId
                   //   });
                   // }
-                  console.log('setIsGetUserInfoDown33')
+                  console.log('setIsGetUserInfoDown33');
                   setIsGetUserInfoDown(true);
                 })
                 .catch((e) => {
@@ -195,7 +199,7 @@ const LoginButton = ({
                 });
             } else {
               loginStore.changeLoginModal(false);
-              console.log('setIsGetUserInfoDown11')
+              console.log('setIsGetUserInfoDown11');
               setIsGetUserInfoDown(true);
             }
           }
@@ -213,16 +217,23 @@ const LoginButton = ({
     // debugger;
     // }
     // 当打开多个tab时，同步登录登出状态
-    if (
-      e.key ===
-      `${localStorage.getItem('country-code-current-operated')}-rc-token`
-    ) {
+    if (e.key === `${window.__.env.REACT_APP_COUNTRY}-rc-token`) {
       // debugger;
       // 该token的旧值不存在，新值存在，表示登录
       if (!e.oldValue && e.newValue) {
         login();
       }
     }
+    // if (
+    //   e.key ===
+    //   `${localStorage.getItem('country-code-current-operated')}-rc-token`
+    // ) {
+    //   // debugger;
+    //   // 该token的旧值不存在，新值存在，表示登录
+    //   if (!e.oldValue && e.newValue) {
+    //     login();
+    //   }
+    // }
   };
 
   const login = async () => {
