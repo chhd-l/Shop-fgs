854dc9ba9e4cbe23f1b4d68c8a7a9741a1633b9f
minya <mytang11@163.com>
2022-07-29 14:14:03 +0800

    us:429622

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -55,14 +55,14 @@ const LoginButton = ({
   const { oktaAuth } = useOktaAuth();
   const { authState } = useOktaAuth();
 
-  useEffect(() => {
-    if (window.__.env.SYNCHRONIZE_LOGIN_STATUS) {
-      window.addEventListener('storage', storageHandler);
-      return () => {
-        window.removeEventListener('storage', storageHandler);
-      };
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (window.__.env.SYNCHRONIZE_LOGIN_STATUS) {
+  //     window.addEventListener('storage', storageHandler);
+  //     return () => {
+  //       window.removeEventListener('storage', storageHandler);
+  //     };
+  //   }
+  // }, []);
 
   // 拿到userinfo信息后，执行传入该组件的init方法
   useEffect(() => {
