337f57120986cfcb7db903a5e672e8d615c91397
minya <mytang11@163.com>
2022-08-03 19:03:19 +0800

    Temporarily remove Checkout folder to avoid sonar's repetition rate scanning

--- src/views/Checkout/Common/index.js
+++
@@ -1,3 +0,0 @@
-export { default as PanelContainer } from './PanelContainer';
-export { default as PayInfoPreview } from './PayInfoPreview';
-export { default as PaymentPanelInfoPreview } from './PaymentPanelInfoPreview';
--- src/views/Checkout/PaymentMethod/Cash/index.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import { FormattedMessage } from 'react-intl-phraseapp';
-import { handleEmailShow } from '@/utils/utils';
-
-const Cash = ({ billingJSX }) => {
-  return <>{billingJSX}</>;
-};
-
-export default Cash;
