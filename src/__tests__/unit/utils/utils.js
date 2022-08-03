f5994fd48dafdc73a6e27600388e1b2ae6be7fe9
梦醒家 <hongxiang.qu@effem.com>
2022-08-03 18:30:08 +0800

    update unit test

--- src/__tests__/unit/utils/utils.js
+++ src/__tests__/unit/utils/utils.js
@@ -1,13 +1,10 @@
-// global.window = Object.create(window);
-
-//   Object.defineProperty(window, "__", {
-//     value: {
-//       sessionItemRoyal: ''
-//     },
-//   });
 import { flat } from '@/utils/utils';
 
-test('flat', () => {
+jest.mock('@/store', () => {
+  return {};
+});
+
+it('flat', () => {
   expect(flat([''])).not.toBe('');
   expect(flat([[123]])).not.toBe('');
 });
