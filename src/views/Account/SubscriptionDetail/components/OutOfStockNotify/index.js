baf24d013628340b38734de303700a0844b0106e
xiaoli <xiaoli.shi@effem.com>
2022-08-25 14:55:41 +0800

    us 437866 70%

---
+++ src/views/Account/SubscriptionDetail/components/OutOfStockNotify/index.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import { FormattedMessage } from 'react-intl-phraseapp';
+import { Link } from 'react-router-dom';
+import cn from 'classnames';
+
+const OngoingOrder = ({ subDetail }) => {
+  useEffect(() => {}, []);
+
+  return <></>;
+};
+export default OngoingOrder;
