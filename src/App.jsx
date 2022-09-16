d155d3f445eb612b1d5d5f383dc0d734837b4b09
梦醒家 <hongxiang.qu@effem.com>
2022-09-16 18:47:35 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,7 @@ const Cart = loadable(() => import('@/views/Cart'));
 const CartFRBreeder = loadable(() => import('@/views/CartFRBreeder'));
 // const CartInStock = loadable(() => import('@/views/CartInStock'));
 const Payment = loadable(() => import('@/views/Payment'));
-const Checkout = loadable(() => import('@/views/Checkout'));
+// const Checkout = loadable(() => import('@/views/Checkout'));
 const demo = loadable(() => import('@/views/demo'));
 const Confirmation = loadable(() => import('@/views/Confirmation'));
 const AccountAppointments = loadable(() =>
