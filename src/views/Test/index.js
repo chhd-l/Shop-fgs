3985741a8fa281c60ea7ae65e891cc0de277cb00
lizongji <fuqinag.luo@effem.com>
2022-09-19 10:16:53 +0800

    test apple pay

--- src/views/Test/index.js
+++ src/views/Test/index.js
@@ -12,19 +12,19 @@ class Test extends React.Component {
     return (
       <div className="rc-content--fixed-header rc-bg-colour--brand4 p-40 text-4xl">
         <h1 className="mt-32">feature_0401_sprint12</h1>
-        {/* <div>
-        <AdyenEditForm
-          showSetAsDefaultCheckobx={window.__.env.REACT_APP_COUNTRY === 'uk'}
-          showCancelBtn={true}
-          // queryList={this.props.refreshList}
-          // updateFormVisible={this.handleCancel}
-          // updateInitStatus={this.updateInitStatus}
-          enableStoreDetails={true}
-          mustSaveForFutherPayments={true}
-          // showErrorMsg={this.showErrorMsg}
-          {...this.props}
-        />
-      </div> */}
+        <div>
+          <AdyenEditForm
+            showSetAsDefaultCheckobx={window.__.env.REACT_APP_COUNTRY === 'uk'}
+            showCancelBtn={true}
+            // queryList={this.props.refreshList}
+            // updateFormVisible={this.handleCancel}
+            // updateInitStatus={this.updateInitStatus}
+            enableStoreDetails={true}
+            mustSaveForFutherPayments={true}
+            // showErrorMsg={this.showErrorMsg}
+            {...this.props}
+          />
+        </div>
       </div>
     );
   }
