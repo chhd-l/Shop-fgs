79690d40f3690777a8276c079c72819e62b9e758
fuluo <fuluo@deloitte.com.cn>
2022-09-20 17:24:38 +0800

    upd: new search function

--- src/components/Header/Search/components/Search.js
+++ src/components/Header/Search/components/Search.js
@@ -7,6 +7,8 @@ import SearchResult from './SearchResult';
 import SearchEmpty from './SearchEmpty';
 import * as api from './api';
 
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './index.less';
 
 const Search = () => {
